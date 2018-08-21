bd7ba1f5bd8fc651f965e0bb83c9f32b3ab17135
jirik <jirik@users.noreply.github.com>
2018-08-21 12:05:09 +0200

    Use gspld-test-client

--- start.sh
+++ start.sh
@@ -1,4 +1,12 @@
 #!/bin/bash
 
 bash src/clear-python-cache.sh
+
+if [ ! -d src/layman/static/test-client ]; then
+    cd src/layman
+    rm -rf static/test-client
+    curl -L https://github.com/jirik/gspld-test-client/releases/download/v0.1/release.tar.gz | tar xvz
+    cd ../../
+fi
+
 python3 src/prepare_layman.py && flask run --host=0.0.0.0 --port=8000
