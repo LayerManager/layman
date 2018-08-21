f8f81d6db3ebdf2b2311a7aca09a64b5c3cb97a9
jirik <jirik@users.noreply.github.com>
2018-08-21 14:08:23 +0200

    Fix downloading test-client

--- src/ensure-test-client.sh
+++ src/ensure-test-client.sh
@@ -2,7 +2,7 @@
 
 if [ ! -d src/layman/static/test-client ]; then
     mkdir -p src/layman/static/test-client
-    cd mkdir -p src/layman/static/test-client
+    cd src/layman/static/test-client
     curl -L https://github.com/jirik/gspld-test-client/releases/download/v0.1/release.tar.gz | tar xvz
     cd ../../../../
 fi
