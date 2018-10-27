f45e66448384e67c6f00eadd251082f950185100
jirik <jirik@users.noreply.github.com>
2018-10-27 11:15:59 +0200

    Set version to 0.2.0

--- src/setup.py
+++ src/setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name='layman',
-    version='0.1.0',
+    version='0.2.0',
     packages=['layman'],
     include_package_data=True,
     install_requires=[
