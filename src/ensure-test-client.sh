b46a31d3ff193b412959c2f05108159056d552a2
jirik <jirik@users.noreply.github.com>
2019-04-11 14:02:38 +0200

    Update test client to v0.7.0

--- src/ensure-test-client.sh
+++ src/ensure-test-client.sh
@@ -3,6 +3,6 @@
 if [ ! -d src/layman/static/test-client ]; then
     mkdir -p src/layman/static/test-client
     cd src/layman/static/test-client
-    curl -L https://github.com/jirik/gspld-test-client/releases/download/v0.6.0/release.tar.gz | tar xvz
+    curl -L https://github.com/jirik/gspld-test-client/releases/download/v0.7.0/release.tar.gz | tar xvz
     cd ../../../../
 fi
