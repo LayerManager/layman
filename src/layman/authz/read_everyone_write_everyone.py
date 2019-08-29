ddd320f9724b09ecbeea20603df3a4cffb868005
jirik <jirik@users.noreply.github.com>
2019-08-29 12:05:10 +0200

    Treat unexpected HTTP method with authorization system

--- src/layman/authz/read_everyone_write_everyone.py
+++ src/layman/authz/read_everyone_write_everyone.py
@@ -1,6 +1,11 @@
+from flask import g, request
+
+from layman import LaymanError
 
 
 def authorize():
+    if request.method not in ['GET', 'POST', 'PUT', 'PATCH', 'DELETE']:
+        raise LaymanError(31, {'method': request.method})
     pass
 
 
