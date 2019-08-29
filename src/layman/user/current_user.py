aeb26b189fd8db0c9425ce0d2da6af9a3c2814ab
jirik <jirik@users.noreply.github.com>
2019-08-29 14:47:37 +0200

    Implement current-user REST endpoint

---
+++ src/layman/user/current_user.py
@@ -0,0 +1,23 @@
+from flask import Blueprint, jsonify, g, current_app as app
+
+from layman.authn import authenticate
+from layman.authz import authorize
+from .util import get_user_profile
+
+
+bp = Blueprint('rest_current_user', __name__)
+
+@bp.before_request
+@authenticate
+@authorize
+def before_request():
+    pass
+
+
+@bp.route('', methods=['GET'])
+def get():
+    app.logger.info(f"GET Current User, user={g.user and g.user['name']}")
+
+    user_profile = get_user_profile(g.user)
+
+    return jsonify(user_profile), 200
