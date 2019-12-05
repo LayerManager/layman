b7a6326e6fbec60a884c8b99421bbc5410a15980
jirik <jirik@users.noreply.github.com>
2019-12-05 15:34:38 +0100

    Clean metadata util

--- src/layman/metadata/util_test.py
+++ src/layman/metadata/util_test.py
@@ -1,11 +1,3 @@
-from celery import chain
-import importlib
-import time
-from celery.contrib.abortable import AbortableAsyncResult
-import pytest
-
-from xml.dom import minidom
-
 import sys
 del sys.modules['layman']
 
@@ -19,8 +11,3 @@ def test_get_default_values_dict():
 
 def test_fill_template():
     xmlstr = util.fill_template('src/layman/metadata/INSPIRE2-min.template.xml', filled_path='src/layman/metadata/INSPIRE2-min.xml')
-
-    # dom = minidom.parseString(xmlstr)
-    # pretty_xml_str = dom.toprettyxml(indent="", newl="")
-    # with open('src/layman/metadata/INSPIRE2-min.xml', "w") as wml_file:
-    #     wml_file.write(pretty_xml_str)
