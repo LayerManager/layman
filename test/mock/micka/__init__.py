498c3fa07aa28f9ce6a433f7e2fe387136c8a3a8
jirik <jirik@users.noreply.github.com>
2020-02-19 10:03:30 +0100

    Better handle Micka's HTTP errors

---
+++ test/mock/micka/__init__.py
@@ -0,0 +1,14 @@
+def run(env_vars, app_config, host, port, debug, load_dotenv, options):
+    import os
+    import sys
+    # os.makedirs("/code/tmp/mock/micka-out", exist_ok=True)
+    # sys.stdout = open(f"/code/tmp/mock/micka-out/{str(os.getpid())}.out", "w")
+    # sys.stderr = open(f"/code/tmp/mock/micka-out/{str(os.getpid())}.err", "w")
+    for k, v in env_vars.items():
+        os.environ[k] = v
+
+    from .app import create_app
+    app = create_app(app_config)
+    app.run(host, port, debug, load_dotenv, **options)
+
+
