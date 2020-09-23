932488baa496c7d8fdabb81c00f296b9101ee06a
index-git <sileny.index@gmail.com>
2020-09-23 13:00:18 +0200

    WIP

--- src/layman/common/util.py
+++ src/layman/common/util.py
@@ -1,2 +1,14 @@
 def get_names_from_infos(infos):
     return list(set(sorted([info for info in infos])))
+
+
+def merge_infos(infos):
+    result_infos = {}
+    # TODO maybe, those two cycles can be done at once
+    for source in infos:
+        for (name, info) in source.items():
+            if result_infos.get(name) is None:
+                result_infos[name] = info
+            else:
+                result_infos[name].update(info)
+    return result_infos
