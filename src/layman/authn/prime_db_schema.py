96475cedf20edcdb2844a8f4190eed9e30e20ad6
index-git <66255344+index-git@users.noreply.github.com>
2020-10-27 13:53:08 +0100

    Create get.users endpoint (#121)
    Part of #28

---
+++ src/layman/authn/prime_db_schema.py
@@ -0,0 +1,9 @@
+from layman.common.prime_db_schema import ensure_whole_user
+
+
+def save_username_reservation(username, iss_id, sub, claims):
+    userinfo = {"iss_id": iss_id,
+                "sub": sub,
+                "claims": claims,
+                }
+    ensure_whole_user(username, userinfo)
--- src/layman/util_test.py
+++ src/layman/util_test.py
@@ -1,3 +1,7 @@
+import pytest
+
+from . import app as app, LaymanError, settings
+from . import util
 from .util import slugify
 
 
@@ -8,3 +12,12 @@ def test_slugify():
     assert slugify(' ?:"+  @') == ''
     assert slugify('01 Stanice vodních toků 26.4.2017 (voda)') == \
         '01_stanice_vodnich_toku_26_4_2017_voda'
+
+
+def test_check_reserved_workspace_names():
+    with app.app_context():
+        for username in settings.RESERVED_WORKSPACE_NAMES:
+            with pytest.raises(LaymanError) as exc_info:
+                util.check_reserved_workspace_names(username)
+            assert exc_info.value.code == 35
+            assert exc_info.value.data['reserved_by'] == 'RESERVED_WORKSPACE_NAMES'
