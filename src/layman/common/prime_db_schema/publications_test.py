ad08e50d3d4417cb4c9c70f3a46cd5f1fe743bd0
index-git <sileny.index@gmail.com>
2020-11-09 13:55:12 +0100

    Access rights are tested against axioms.

--- src/layman/common/prime_db_schema/publications_test.py
+++ src/layman/common/prime_db_schema/publications_test.py
@@ -114,6 +114,138 @@ def test_select_publications():
         pubs = publications.get_publication_infos(username)
         assert len(pubs) == 0, pubs
 
+
+def test_only_valid_names():
+    workspace_name = 'test_only_valid_names_workspace'
+    username = 'test_only_valid_names_user'
+
+    with app.app_context():
+        workspaces.ensure_workspace(workspace_name)
+        id_workspace_user = workspaces.ensure_workspace(username)
+        users.ensure_user(id_workspace_user, userinfo)
+
+        publications.only_valid_names(set())
+        publications.only_valid_names({username, })
+        publications.only_valid_names({settings.RIGHTS_EVERYONE_ROLE, })
+        publications.only_valid_names({settings.RIGHTS_EVERYONE_ROLE, username, })
+        publications.only_valid_names({username, settings.RIGHTS_EVERYONE_ROLE, })
+
+        with pytest.raises(LaymanError) as exc_info:
+            publications.only_valid_names({username, workspace_name})
+            assert exc_info.value.code == 43
+
+        with pytest.raises(LaymanError) as exc_info:
+            publications.only_valid_names({workspace_name, username})
+            assert exc_info.value.code == 43
+
+        with pytest.raises(LaymanError) as exc_info:
+            publications.only_valid_names({workspace_name, settings.RIGHTS_EVERYONE_ROLE, })
+            assert exc_info.value.code == 43
+
+        with pytest.raises(LaymanError) as exc_info:
+            publications.only_valid_names({settings.RIGHTS_EVERYONE_ROLE, 'skaljgdalskfglshfgd', })
+            assert exc_info.value.code == 43
+
+
+def test_at_least_one_can_write():
+    workspace_name = 'test_at_least_one_can_write_workspace'
+    username = 'test_at_least_one_can_write_user'
+
+    publications.at_least_one_can_write({username, })
+    publications.at_least_one_can_write({settings.RIGHTS_EVERYONE_ROLE, })
+    publications.at_least_one_can_write({username, settings.RIGHTS_EVERYONE_ROLE, })
+    publications.at_least_one_can_write({workspace_name, })
+    publications.at_least_one_can_write({'lusfjdiaurghalskug', })
+
+    with pytest.raises(LaymanError) as exc_info:
+        publications.at_least_one_can_write(set())
+        assert exc_info.value.code == 43
+
+
+def test_who_can_write_can_read():
+    workspace_name = 'test_who_can_write_can_read_workspace'
+    username = 'test_who_can_write_can_read_user'
+
+    publications.who_can_write_can_read(set(), set())
+    publications.who_can_write_can_read({username, }, {username, })
+    publications.who_can_write_can_read({username, workspace_name}, {username, })
+    publications.who_can_write_can_read({username, settings.RIGHTS_EVERYONE_ROLE}, {username, })
+    publications.who_can_write_can_read({username, settings.RIGHTS_EVERYONE_ROLE}, {username, settings.RIGHTS_EVERYONE_ROLE, })
+    publications.who_can_write_can_read({settings.RIGHTS_EVERYONE_ROLE, }, {settings.RIGHTS_EVERYONE_ROLE, })
+    publications.who_can_write_can_read({settings.RIGHTS_EVERYONE_ROLE, }, {settings.RIGHTS_EVERYONE_ROLE, username, })
+    publications.who_can_write_can_read({settings.RIGHTS_EVERYONE_ROLE, }, {settings.RIGHTS_EVERYONE_ROLE, workspace_name, })
+    publications.who_can_write_can_read({settings.RIGHTS_EVERYONE_ROLE, username, }, {settings.RIGHTS_EVERYONE_ROLE, })
+    publications.who_can_write_can_read({settings.RIGHTS_EVERYONE_ROLE, username, }, set())
+    publications.who_can_write_can_read({workspace_name, }, {workspace_name, })
+
+    with pytest.raises(LaymanError) as exc_info:
+        publications.who_can_write_can_read(set(), {workspace_name, })
+        assert exc_info.value.code == 43
+
+    with pytest.raises(LaymanError) as exc_info:
+        publications.who_can_write_can_read(set(), {username, })
+        assert exc_info.value.code == 43
+
+    with pytest.raises(LaymanError) as exc_info:
+        publications.who_can_write_can_read(set(), {settings.RIGHTS_EVERYONE_ROLE, })
+        assert exc_info.value.code == 43
+
+    with pytest.raises(LaymanError) as exc_info:
+        publications.who_can_write_can_read(username, {settings.RIGHTS_EVERYONE_ROLE, })
+        assert exc_info.value.code == 43
+
+    with pytest.raises(LaymanError) as exc_info:
+        publications.who_can_write_can_read(username, {workspace_name, })
+        assert exc_info.value.code == 43
+
+
+def test_i_can_still_write():
+    workspace_name = 'test_i_can_still_write_workspace'
+    username = 'test_who_can_write_can_read_user'
+
+    publications.i_can_still_write(None, {settings.RIGHTS_EVERYONE_ROLE, })
+    publications.i_can_still_write(None, {username, settings.RIGHTS_EVERYONE_ROLE, })
+    publications.i_can_still_write(username, {settings.RIGHTS_EVERYONE_ROLE, })
+    publications.i_can_still_write(username, {workspace_name, settings.RIGHTS_EVERYONE_ROLE, })
+    publications.i_can_still_write(username, {workspace_name, username, })
+
+    with pytest.raises(LaymanError) as exc_info:
+        publications.i_can_still_write(None, set())
+        assert exc_info.value.code == 43
+
+    with pytest.raises(LaymanError) as exc_info:
+        publications.i_can_still_write(None, {workspace_name, })
+        assert exc_info.value.code == 43
+
+    with pytest.raises(LaymanError) as exc_info:
+        publications.i_can_still_write(username, set())
+        assert exc_info.value.code == 43
+
+    with pytest.raises(LaymanError) as exc_info:
+        publications.i_can_still_write(username, {workspace_name, })
+        assert exc_info.value.code == 43
+
+
+def test_owner_can_still_write():
+    workspace_name = 'test_owner_can_still_write_workspace'
+    username = 'test_owner_can_still_write_user'
+
+    publications.owner_can_still_write(None, set())
+    publications.owner_can_still_write(None, {settings.RIGHTS_EVERYONE_ROLE, })
+    publications.owner_can_still_write(None, {username, })
+    publications.owner_can_still_write(username, {settings.RIGHTS_EVERYONE_ROLE, })
+    publications.owner_can_still_write(username, {username, })
+    publications.owner_can_still_write(username, {username, workspace_name, })
+
+    with pytest.raises(LaymanError) as exc_info:
+        publications.owner_can_still_write(username, set())
+        assert exc_info.value.code == 43
+
+    with pytest.raises(LaymanError) as exc_info:
+        publications.owner_can_still_write(username, {workspace_name, })
+        assert exc_info.value.code == 43
+
+
 def test_clear_roles():
     workspace_name = 'test_clear_roles_workspace'
     username = 'test_clear_roles_user'
