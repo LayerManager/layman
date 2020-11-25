06b23e20d4422f6280bfded13c67ac7a71d2c9d9
index-git <sileny.index@gmail.com>
2020-11-25 10:40:52 +0100

    Rewrite rest_layer_style_test.py into ussing Layman in process

--- src/layman/layer/rest_layer_style_test.py
+++ src/layman/layer/rest_layer_style_test.py
@@ -1,25 +1,28 @@
 import requests
+import pytest
 from lxml import etree as ET
 
 from layman import app
 from layman.util import url_for
-from test import flask_client as client_util
+from test import process, process_client
 
-client = client_util.client
+ensure_layman = process.ensure_layman
 
 
-def test_get_layer_style(client):
+@pytest.mark.usefixtures('ensure_layman')
+def test_get_layer_style():
     username = 'test_get_layer_style_user'
     layername = 'test_get_layer_style_layer'
 
-    client_util.publish_layer(username,
-                              layername,
-                              client,)
+    process_client.publish_layer(username,
+                                 layername,
+                                 )
 
     with app.app_context():
         rest_url = url_for('rest_layer_style.get', username=username, layername=layername)
-        rv = client.get(rest_url)
+        rv = requests.get(rest_url)
         assert rv.status_code == 200, rv.text
-        xml_tree = ET.fromstring(rv.get_data())
+        # lxml does not support importing from utf8 string
+        xml_tree = ET.fromstring(bytes(rv.text, encoding='utf8'))
         assert ET.QName(xml_tree) == "{http://www.opengis.net/sld}StyledLayerDescriptor", rv.text
-    client_util.delete_layer(username, layername, client)
+    process_client.delete_layer(username, layername)
