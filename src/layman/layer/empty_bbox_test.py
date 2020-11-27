e5abc4df2c91273d68c4e76434f6024e635f66f7
Jiri Kozel <jirik@users.noreply.github.com>
2020-11-27 17:56:15 +0100

    Set world bounding box on GeoServer layer if DB table is empty
    Fix #40
    
    Related to #159

---
+++ src/layman/layer/empty_bbox_test.py
@@ -0,0 +1,79 @@
+import os
+import pytest
+import requests
+from owslib.wms import WebMapService
+from layman import settings
+from layman.layer.geoserver import wms as gs_wms
+from test import process, process_client
+from test.data import wfs as wfs_data_util
+
+
+def get_shp_file_paths(shp_file_path):
+    extensions = ['dbf', 'prj', 'qpj', 'shx']
+    root, shp_ext = os.path.splitext(shp_file_path)
+    result = [f"{root}.{ext}" for ext in extensions]
+    result.append(shp_file_path)
+    return result
+
+
+def assert_non_empty_bbox(bbox):
+    assert bbox[0] < bbox[2] and bbox[1] < bbox[3]
+
+
+def assert_wms_layer(workspace, layername, exp_title):
+    wms = WebMapService(gs_wms.get_wms_url(workspace), gs_wms.VERSION)
+    assert layername in wms.contents
+    wms_layer = wms[layername]
+    assert wms_layer.title == exp_title
+    assert_non_empty_bbox(wms_layer.boundingBox)
+    assert_non_empty_bbox(wms_layer.boundingBoxWGS84)
+    return wms_layer
+
+
+def wfs_t_insert_point(workspace, layername):
+    wfs_t_data = wfs_data_util.get_wfs20_insert_points(workspace, layername)
+    wfs_t_url = f"http://{settings.LAYMAN_SERVER_NAME}/geoserver/{workspace}/wfs?request=Transaction"
+    wfs_t_headers = {
+        'Accept': 'text/xml',
+        'Content-type': 'text/xml',
+    }
+    r = requests.post(wfs_t_url,
+                      data=wfs_t_data,
+                      headers=wfs_t_headers)
+    assert r.status_code == 200
+
+
+@pytest.fixture(scope="module")
+def ensure_layman():
+    layman_process = process.start_layman()
+    yield
+    process.stop_process(layman_process)
+
+
+@pytest.mark.parametrize('layername, file_paths', [
+    ('empty', get_shp_file_paths('sample/layman.layer/empty.shp')),
+    ('single_point', get_shp_file_paths('sample/layman.layer/single_point.shp')),
+])
+@pytest.mark.usefixtures('ensure_layman')
+def test_empty_shapefile(layername, file_paths):
+    workspace = 'test_empty_bbox_workspace'
+    title = layername
+
+    process_client.publish_layer(workspace, layername, file_paths=file_paths)
+
+    wms_layer = assert_wms_layer(workspace, layername, title)
+    native_bbox = wms_layer.boundingBox
+    wgs_bbox = wms_layer.boundingBoxWGS84
+
+    title = 'new title'
+    process_client.patch_layer(workspace, layername, title=title)
+    wms_layer = assert_wms_layer(workspace, layername, title)
+    assert wms_layer.boundingBox == native_bbox
+    assert wms_layer.boundingBoxWGS84 == wgs_bbox
+
+    wfs_t_insert_point(workspace, layername)
+    wms_layer = assert_wms_layer(workspace, layername, title)
+    assert wms_layer.boundingBox == native_bbox
+    assert wms_layer.boundingBoxWGS84 == wgs_bbox
+
+    process_client.delete_layer(workspace, layername)
--- src/layman/layer/geoserver/__init__.py
+++ src/layman/layer/geoserver/__init__.py
@@ -8,6 +8,7 @@ from flask import g, current_app as app
 from layman.http import LaymanError
 from layman import settings
 from layman.common import geoserver as common
+from layman.layer import db as db_source
 
 FLASK_WORKSPACES_KEY = f"{__name__}:WORKSPACES"
 FLASK_RULES_KEY = f"{__name__}:RULES"
@@ -64,28 +65,38 @@ def publish_layer_from_db(username, layername, description, title):
         title
     ]
     keywords = list(set(keywords))
+    feature_type_def = {
+        "name": layername,
+        "title": title,
+        "abstract": description,
+        "keywords": {
+            "string": keywords
+        },
+        "srs": "EPSG:3857",
+        "projectionPolicy": "FORCE_DECLARED",
+        "enabled": True,
+        "store": {
+            "@class": "dataStore",
+            "name": username + ":postgresql",
+        },
+    }
+    db_bbox = db_source.get_bbox(username, layername)
+    if db_bbox is None:
+        # world
+        native_bbox = {
+            "minx": -20026376.39,
+            "miny": -20048966.10,
+            "maxx": 20026376.39,
+            "maxy": 20048966.10,
+            "crs": "EPSG:3857",
+        }
+        feature_type_def['nativeBoundingBox'] = native_bbox
     r = requests.post(
         urljoin(settings.LAYMAN_GS_REST_WORKSPACES,
                 username + '/datastores/postgresql/featuretypes/'),
-        data=json.dumps(
-            {
-                "featureType": {
-                    "name": layername,
-                    "title": title,
-                    "abstract": description,
-                    "keywords": {
-                        "string": keywords
-                    },
-                    "srs": "EPSG:3857",
-                    "projectionPolicy": "FORCE_DECLARED",
-                    "enabled": True,
-                    "store": {
-                        "@class": "dataStore",
-                        "name": username + ":postgresql",
-                    },
-                }
-            }
-        ),
+        data=json.dumps({
+            "featureType": feature_type_def
+        }),
         headers=headers_json,
         auth=settings.LAYMAN_GS_AUTH
     )
