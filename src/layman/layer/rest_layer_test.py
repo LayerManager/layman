ed3d638245b555502a03b4505e52bd7d7154b389
index-git <sileny.index@gmail.com>
2020-12-04 11:11:32 +0100

    Rewrite rest_layer_test::test_sld_value into process.

--- src/layman/layer/rest_layer_test.py
+++ src/layman/layer/rest_layer_test.py
@@ -1,41 +1,42 @@
 import sys
+import pytest
+import requests
+import json
 
 del sys.modules['layman']
 
-from layman import app, settings
-from test import flask_client
+from layman import app
+from test import process, process_client
 from layman.util import url_for
 
+ensure_layman = process.ensure_layman
 
-client = flask_client.client
 
-
-def test_sld_value(client):
+@pytest.mark.usefixtures('ensure_layman')
+def test_sld_value():
     username = 'test_layer_sld_user'
     layername = 'test_layer_sld_layer'
 
-    flask_client.publish_layer(username, layername, client)
+    process_client.publish_layer(username, layername)
 
     with app.app_context():
         layer_url = url_for('rest_layer.get', username=username, layername=layername)
         style_url = url_for('rest_layer_style.get', username=username, layername=layername)
-        r = client.get(layer_url)
-    assert r.status_code == 200, r.get_json()
-    resp_json = r.get_json()
+    r = requests.get(layer_url)
+    assert r.status_code == 200, r.text
+    resp_json = json.loads(r.text)
 
-    assert "sld" in resp_json, r.get_json()
-    assert "url" in resp_json["sld"], r.get_json()
-    assert "status" not in resp_json["sld"], r.get_json()
+    assert "sld" in resp_json, r.text
+    assert "url" in resp_json["sld"], r.text
+    assert "status" not in resp_json["sld"], r.text
 
     sld_url = resp_json["sld"]["url"]
-    assert sld_url == style_url, (r.get_json(), sld_url)
+    assert sld_url == style_url, (r.text, sld_url)
 
-    with app.app_context():
-        r_get = client.get(sld_url)
-    assert r_get.status_code == 200, (r_get.get_json(), sld_url)
+    r_get = requests.get(sld_url)
+    assert r_get.status_code == 200, (r_get.text, sld_url)
 
-    with app.app_context():
-        r_del = client.delete(sld_url)
-    assert r_del.status_code >= 400, (r_del.get_json(), sld_url)
+    r_del = requests.delete(sld_url)
+    assert r_del.status_code >= 400, (r_del.text, sld_url)
 
-    flask_client.delete_layer(username, layername, client)
+    process_client.delete_layer(username, layername)
