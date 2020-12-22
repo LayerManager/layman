e3269d2ba46825722f18c4d7c813ccced3fcbe24
index-git <sileny.index@gmail.com>
2020-12-22 11:53:34 +0100

    Use general publication methods in tests.

--- src/layman/rest_multipublication_test.py
+++ src/layman/rest_multipublication_test.py
@@ -1,9 +1,4 @@
 import pytest
-import json
-import requests
-
-from layman import app
-from layman.util import url_for
 from test import process, process_client
 
 ensure_layman = process.ensure_layman
@@ -16,62 +11,49 @@ class TestDeletePublicationsClass:
     authn_headers_owner = process_client.get_authz_headers(owner)
     authn_headers_deleter = process_client.get_authz_headers(deleter)
 
-    @pytest.fixture(scope="class")
+    @pytest.fixture()
     def provide_data(self):
         process_client.ensure_reserved_username(self.owner, self.authn_headers_owner)
         process_client.ensure_reserved_username(self.deleter, self.authn_headers_deleter)
         yield
 
-    @pytest.mark.parametrize("publication_prefix, publish_method, get_url_string, delete_url_string", [
-        ('test_delete_publications_layer', process_client.publish_layer, 'rest_layers.get', 'rest_layers.delete', ),
-        ('test_delete_publications_map', process_client.publish_map, 'rest_maps.get', 'rest_maps.delete',),
-    ])
+    @pytest.mark.parametrize('publ_type', process_client.PUBLICATION_TYPES)
     @pytest.mark.usefixtures('liferay_mock', 'ensure_layman', 'provide_data')
     def test_delete_publications(self,
-                                 publication_prefix,
-                                 publish_method,
-                                 get_url_string,
-                                 delete_url_string):
+                                 publ_type):
+        def check_delete(headers,
+                         after_delete_publications,
+                         remaining_publications):
+            delete_json = process_client.delete_publications(publ_type, owner, headers=headers)
+            publication_set = {publication['name'] for publication in delete_json}
+            assert after_delete_publications == publication_set
+
+            get_json = process_client.get_publications(publ_type, workspace=owner, headers=authn_headers_owner)
+            publication_set = {publication['name'] for publication in get_json}
+            assert remaining_publications == publication_set
+
         owner = self.owner
         authn_headers_owner = self.authn_headers_owner
         authn_headers_deleter = self.authn_headers_deleter
 
-        publication_a = publication_prefix + '_a'
-        publication_b = publication_prefix + '_b'
+        publication_a = 'test_delete_publications_publication_a'
+        publication_b = 'test_delete_publications_publication_b'
         publications = [(publication_a, {'read': 'EVERYONE', 'write': owner}),
                         (publication_b, {'read': 'EVERYONE', 'write': 'EVERYONE'}),
                         ]
 
         for (name, access_rights) in publications:
-            publish_method(owner, name, access_rights=access_rights, headers=authn_headers_owner)
+            process_client.publish_publication(publ_type, owner, name, access_rights=access_rights, headers=authn_headers_owner)
 
-        with app.app_context():
-            url_get = url_for(get_url_string, username=owner)
-            url_del = url_for(delete_url_string, username=owner)
-
-        rv = requests.get(url_get, headers=authn_headers_owner)
-        assert rv.status_code == 200, rv.text
-        assert len(rv.json()) == len(publications)
+        rv = process_client.get_publications(publ_type, workspace=owner, headers=authn_headers_owner)
+        assert len(rv) == len(publications)
 
         # Delete by other user with rights only for one layer
-        rv = requests.delete(url_del, headers=authn_headers_deleter)
-        assert rv.status_code == 200, rv.text
-        publications = {publication['name'] for publication in json.loads(rv.text)}
-        assert {publication_b, } == publications
+        check_delete(authn_headers_deleter,
+                     {publication_b, },
+                     {publication_a, })
 
-        rv = requests.get(url_get, headers=authn_headers_owner)
-        assert rv.status_code == 200, rv.text
-        publications = {publication['name'] for publication in json.loads(rv.text)}
-        assert {publication_a, } == publications
-
-        # Delete by owner
-        rv = requests.delete(url_del, headers=authn_headers_owner)
-        assert rv.status_code == 200, rv.text
-        publications = {publication['name'] for publication in json.loads(rv.text)}
-        assert {publication_a, } == publications
-
-        # Check, that everything is deleted
-        rv = requests.get(url_get, headers=authn_headers_owner)
-        assert rv.status_code == 200, rv.text
-        publications = {publication['name'] for publication in json.loads(rv.text)}
-        assert set() == publications
+        # Delete by owner, everything is deleted
+        check_delete(authn_headers_owner,
+                     {publication_a, },
+                     set())
--- src/layman/rest_publication_test.py
+++ src/layman/rest_publication_test.py
@@ -8,14 +8,9 @@ ensure_layman = process.ensure_layman
 liferay_mock = process.liferay_mock
 
 
-@pytest.mark.parametrize("publish_method, patch_method, delete_method", [
-    (process_client.publish_layer, process_client.patch_layer, process_client.delete_layer,),
-    (process_client.publish_map, process_client.patch_map, process_client.delete_map,),
-])
+@pytest.mark.parametrize('publ_type', process_client.PUBLICATION_TYPES)
 @pytest.mark.usefixtures('ensure_layman')
-def test_wrong_post(publish_method,
-                    patch_method,
-                    delete_method):
+def test_wrong_post(publ_type):
     def check_response(resp):
         assert resp.status_code == 400, (resp.status_code, r.text)
         resp_json = json.loads(resp.text)
@@ -25,29 +20,29 @@ def test_wrong_post(publish_method,
     workspace = 'test_wrong_post_workspace'
     publication = 'test_wrong_post_publication'
 
-    r = publish_method(workspace, publication, access_rights={'read': 'EVRBODY'}, assert_status=False)
+    r = process_client.publish_publication(publ_type, workspace, publication, access_rights={'read': 'EVRBODY'}, assert_status=False)
     check_response(r)
 
-    r = publish_method(workspace, publication, access_rights={'write': 'EVRBODY'}, assert_status=False)
+    r = process_client.publish_publication(publ_type, workspace, publication, access_rights={'write': 'EVRBODY'}, assert_status=False)
     check_response(r)
 
-    r = publish_method(workspace, publication, access_rights={'read': 'EVRBODY', 'write': 'EVRBODY'}, assert_status=False)
+    r = process_client.publish_publication(publ_type, workspace, publication, access_rights={'read': 'EVRBODY', 'write': 'EVRBODY'}, assert_status=False)
     check_response(r)
 
-    publish_method(workspace, publication)
+    process_client.publish_publication(publ_type, workspace, publication)
 
-    r = patch_method(workspace, publication, access_rights={'read': 'EVRBODY'}, assert_status=False)
+    r = process_client.patch_publication(publ_type, workspace, publication, access_rights={'read': 'EVRBODY'}, assert_status=False)
     check_response(r)
 
-    r = patch_method(workspace, publication, access_rights={'write': 'EVRBODY'}, assert_status=False)
+    r = process_client.patch_publication(publ_type, workspace, publication, access_rights={'write': 'EVRBODY'}, assert_status=False)
     check_response(r)
 
-    r = patch_method(workspace, publication, access_rights={'read': 'EVRBODY', 'write': 'EVRBODY'}, assert_status=False)
+    r = process_client.patch_publication(publ_type, workspace, publication, access_rights={'read': 'EVRBODY', 'write': 'EVRBODY'}, assert_status=False)
     check_response(r)
 
-    r = patch_method(workspace, publication)
+    r = process_client.patch_publication(publ_type, workspace, publication)
 
-    delete_method(workspace, publication)
+    process_client.delete_publication(publ_type, workspace, publication)
 
 
 class TestSoapClass:
@@ -58,9 +53,9 @@ class TestSoapClass:
     access_rights_rewo = {'read': f"{username},EVERYONE", 'write': f"{username}"}
     access_rights_rewe = {'read': f"{username},EVERYONE", 'write': f"{username},EVERYONE"}
 
-    @pytest.fixture(scope='class')
+    @pytest.fixture()
     def reserve_username(self):
-        process_client.reserve_username(self.username, headers=self.authz_headers)
+        process_client.ensure_reserved_username(self.username, headers=self.authz_headers)
         yield
 
     @pytest.mark.usefixtures('liferay_mock', 'ensure_layman', 'reserve_username')
@@ -76,27 +71,27 @@ class TestSoapClass:
         publ_name_prefix = self.publ_name_prefix
         authz_headers = self.authz_headers
 
-        post_method = process_client.get_post_publications_method(publ_type)
-        patch_method = process_client.get_patch_publication_method(publ_type)
+        post_method = process_client.publish_publication
+        patch_method = process_client.patch_publication
         publ_name = f"{publ_name_prefix}{publ_type.split('.')[-1]}"
-        get_method = process_client.get_get_publication_method(publ_type)
-        delete_method = process_client.get_delete_publication_method(publ_type)
 
         for idx, (access_rights, anonymous_visibility) in enumerate(params_and_expected_list):
             write_method = patch_method if idx > 0 else post_method
 
-            write_method(username,
+            write_method(publ_type,
+                         username,
                          publ_name,
                          headers=authz_headers,
                          access_rights=access_rights)
 
-            publ_uuid = get_method(username, publ_name, headers=authz_headers)['uuid']
+            publ_uuid = process_client.get_publication(publ_type, username, publ_name, headers=authz_headers)['uuid']
             publ_muuid = f"m-{publ_uuid}"
             assert micka_util.get_number_of_records(publ_muuid, use_authn=True) > 0
             anon_number_of_records = micka_util.get_number_of_records(publ_muuid, use_authn=False)
             assert bool(anon_number_of_records) == anonymous_visibility, \
                 f"muuid={publ_muuid}, access_rights={access_rights}, number_of_records={anon_number_of_records}"
 
-        delete_method(username,
-                      publ_name,
-                      headers=authz_headers)
+        process_client.delete_publication(publ_type,
+                                          username,
+                                          publ_name,
+                                          headers=authz_headers)
