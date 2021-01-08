9d9b8bad5cc85382b48e864bb2b99268bc5c0d27
Jiri Kozel <jirik@users.noreply.github.com>
2021-01-08 10:48:10 +0100

    Assert that output SRS list affects also existing layers

--- src/layman/common/geoserver/output_srs_list_test.py
+++ src/layman/common/geoserver/output_srs_list_test.py
@@ -21,17 +21,28 @@ def delete_layer_after_test():
         process_client.delete_layer(workspace, layername)
 
 
+@pytest.mark.timeout(20)
 def test_custom_srs_list(delete_layer_after_test):
     workspace = 'test_custom_srs_list_workspace'
-    layername = 'test_custom_srs_list_layer'
+    layername1 = 'test_custom_srs_list_layer1'
+    layername2 = 'test_custom_srs_list_layer2'
     output_srs_list = [4326, 3857, 32633, 32634, 3059]
+    assert settings.LAYMAN_OUTPUT_SRS_LIST != output_srs_list
+
+    process.ensure_layman_function(process.LAYMAN_DEFAULT_SETTINGS)
+    process_client.publish_layer(workspace, layername1)
+    delete_layer_after_test(workspace, layername1)
+    assert_wms_output_srs_list(workspace, layername1, settings.LAYMAN_OUTPUT_SRS_LIST)
+    assert_wfs_output_srs_list(workspace, layername1, settings.LAYMAN_OUTPUT_SRS_LIST)
+
     process.ensure_layman_function({
         'LAYMAN_OUTPUT_SRS_LIST': ','.join([str(code) for code in output_srs_list])
     })
-    process_client.publish_layer(workspace, layername)
-    delete_layer_after_test(workspace, layername)
-    assert_wms_output_srs_list(workspace, layername, output_srs_list)
-    assert_wfs_output_srs_list(workspace, layername, output_srs_list)
+    process_client.publish_layer(workspace, layername2)
+    delete_layer_after_test(workspace, layername2)
+    for layername in [layername1, layername2]:
+        assert_wms_output_srs_list(workspace, layername, output_srs_list)
+        assert_wfs_output_srs_list(workspace, layername, output_srs_list)
 
 
 def assert_wms_output_srs_list(workspace, layername, expected_output_srs_list):
