2e578a63822ef41dea9ef51290a13c0c85da06d9
index-git <sileny.index@gmail.com>
2021-01-08 11:58:14 +0100

    Propagate OUTPUT_SRS_LIST to GeoServer epsg.properties

---
+++ src/geoserver/epsg_properties.py
@@ -0,0 +1,95 @@
+import logging
+import os
+import pathlib
+import re
+import requests
+
+logger = logging.getLogger(__name__)
+
+EPSG_PROPERTIES_DEFAULT = {
+    3059: '3059=PROJCS["LKS92 / Latvia TM",GEOGCS["LKS92",DATUM["Latvia_1992",SPHEROID["GRS 1980",6378137,298.257222101,'
+          'AUTHORITY["EPSG","7019"]],TOWGS84[0,0,0,0,0,0,0],AUTHORITY["EPSG","6661"]],PRIMEM["Greenwich",0,AUTHORITY["EPSG","8901"]],'
+          'UNIT["degree",0.0174532925199433,AUTHORITY["EPSG","9122"]],AUTHORITY["EPSG","4661"]],PROJECTION["Transverse_Mercator"],'
+          'PARAMETER["latitude_of_origin",0],PARAMETER["central_meridian",24],PARAMETER["scale_factor",0.9996],'
+          'PARAMETER["false_easting",500000],PARAMETER["false_northing",-6000000],UNIT["metre",1,AUTHORITY["EPSG","9001"]],'
+          'AUTHORITY["EPSG","3059"]]',
+    3857: '3857=PROJCS["WGS 84 / Pseudo-Mercator",GEOGCS["WGS 84",DATUM["WGS_1984",SPHEROID["WGS 84",6378137,298.257223563,'
+          'AUTHORITY["EPSG","7030"]],AUTHORITY["EPSG","6326"]],PRIMEM["Greenwich",0,AUTHORITY["EPSG","8901"]],UNIT["degree",'
+          '0.0174532925199433,AUTHORITY["EPSG","9122"]],AUTHORITY["EPSG","4326"]],PROJECTION["Mercator_1SP"],'
+          'PARAMETER["central_meridian",0],PARAMETER["scale_factor",1],PARAMETER["false_easting",0],PARAMETER["false_northing",0],'
+          'UNIT["metre",1,AUTHORITY["EPSG","9001"]],AXIS["X",EAST],AXIS["Y",NORTH],EXTENSION["PROJ4","+proj=merc +a=6378137 +b=6378137 '
+          '+lat_ts=0.0 +lon_0=0.0 +x_0=0.0 +y_0=0 +k=1.0 +units=m +nadgrids=@null +wktext  +no_defs"],AUTHORITY["EPSG","3857"]]',
+    4326: '4326=GEOGCS["WGS 84",DATUM["WGS_1984",SPHEROID["WGS 84",6378137,298.257223563,AUTHORITY["EPSG","7030"]],AUTHORITY["EPSG",'
+          '"6326"]],PRIMEM["Greenwich",0,AUTHORITY["EPSG","8901"]],UNIT["degree",0.0174532925199433,AUTHORITY["EPSG","9122"]],'
+          'AUTHORITY["EPSG","4326"]]',
+    5514: '5514=PROJCS["S-JTSK / Krovak East North",GEOGCS["S-JTSK",DATUM["System_Jednotne_Trigonometricke_Site_Katastralni",'
+          'SPHEROID["Bessel 1841",6377397.155,299.1528128,AUTHORITY["EPSG","7004"]],TOWGS84[589,76,480,0,0,0,0],AUTHORITY["EPSG",'
+          '"6156"]],PRIMEM["Greenwich",0,AUTHORITY["EPSG","8901"]],UNIT["degree",0.0174532925199433,AUTHORITY["EPSG","9122"]],'
+          'AUTHORITY["EPSG","4156"]],PROJECTION["Krovak"],PARAMETER["latitude_of_center",49.5],PARAMETER["longitude_of_center",'
+          '24.83333333333333],PARAMETER["azimuth",30.28813972222222],PARAMETER["pseudo_standard_parallel_1",78.5],'
+          'PARAMETER["scale_factor",0.9999],PARAMETER["false_easting",0],PARAMETER["false_northing",0],UNIT["metre",1,'
+          'AUTHORITY["EPSG","9001"]],AXIS["X",EAST],AXIS["Y",NORTH],AUTHORITY["EPSG","5514"]]',
+    32634: '32634=PROJCS["WGS 84 / UTM zone 34N",GEOGCS["WGS 84",DATUM["WGS_1984",SPHEROID["WGS 84",6378137,298.257223563,'
+           'AUTHORITY["EPSG","7030"]],AUTHORITY["EPSG","6326"]],PRIMEM["Greenwich",0,AUTHORITY["EPSG","8901"]],UNIT["degree",'
+           '0.0174532925199433,AUTHORITY["EPSG","9122"]],AUTHORITY["EPSG","4326"]],PROJECTION["Transverse_Mercator"],'
+           'PARAMETER["latitude_of_origin",0],PARAMETER["central_meridian",21],PARAMETER["scale_factor",0.9996],'
+           'PARAMETER["false_easting",500000],PARAMETER["false_northing",0],UNIT["metre",1,AUTHORITY["EPSG","9001"]],AXIS["Easting",'
+           'EAST],AXIS["Northing",NORTH],AUTHORITY["EPSG","32634"]]',
+    32633: '32633=PROJCS["WGS 84 / UTM zone 33N",GEOGCS["WGS 84",DATUM["WGS_1984",SPHEROID["WGS 84",6378137,298.257223563,'
+           'AUTHORITY["EPSG","7030"]],AUTHORITY["EPSG","6326"]],PRIMEM["Greenwich",0,AUTHORITY["EPSG","8901"]],UNIT["degree",'
+           '0.0174532925199433,AUTHORITY["EPSG","9122"]],AUTHORITY["EPSG","4326"]],PROJECTION["Transverse_Mercator"],'
+           'PARAMETER["latitude_of_origin",0],PARAMETER["central_meridian",15],PARAMETER["scale_factor",0.9996],'
+           'PARAMETER["false_easting",500000],PARAMETER["false_northing",0],UNIT["metre",1,AUTHORITY["EPSG","9001"]],AXIS["Easting",'
+           'EAST],AXIS["Northing",NORTH],AUTHORITY["EPSG","32633"]]',
+    102067: '102067=PROJCS["S-JTSK_Krovak_East_North",GEOGCS["GCS_S_JTSK",DATUM["Jednotne_Trigonometricke_Site_Katastralni",'
+            'SPHEROID["Bessel_1841",6377397.155,299.1528128]],PRIMEM["Greenwich",0],UNIT["Degree",0.017453292519943295]],'
+            'PROJECTION["Krovak"],PARAMETER["False_Easting",0],PARAMETER["False_Northing",0],PARAMETER["Pseudo_Standard_Parallel_1",'
+            '78.5],PARAMETER["Scale_Factor",0.9999],PARAMETER["Azimuth",30.28813975277778],PARAMETER["Longitude_Of_Center",'
+            '24.83333333333333],PARAMETER["Latitude_Of_Center",49.5],PARAMETER["X_Scale",-1],PARAMETER["Y_Scale",1],'
+            'PARAMETER["XY_Plane_Rotation",90],UNIT["Meter",1],AUTHORITY["EPSG","102067"]]',
+}
+
+
+def get_srs_list_from_epsg_file(file_path):
+    result = set()
+    if os.path.exists(file_path):
+        with open(file_path, 'r') as f:
+            epsg_props = f.read()
+        epsg_defs = re.finditer(r'^([1-9][0-9]+)=', epsg_props, re.MULTILINE)
+        result = {int(epsg_def.group(1)) for epsg_def in epsg_defs}
+    return result
+
+
+def setup_epsg(data_dir, srs_list):
+
+    file_name = 'epsg.properties'
+
+    file_path = os.path.join(data_dir, 'user_projections', file_name)
+    pathlib.Path(os.path.dirname(file_path)).mkdir(parents=True, exist_ok=True)
+    old_srs_list = get_srs_list_from_epsg_file(file_path)
+    missing_srs = srs_list.difference(old_srs_list)
+    new_epsg = {code: definition for code, definition in EPSG_PROPERTIES_DEFAULT.items()
+                if code in missing_srs}
+
+    srs_to_download = missing_srs.difference(new_epsg.keys())
+    if new_epsg or srs_to_download:
+        logger.info(f"Ensuring GeoServer EPSG definition for SRS list: '{srs_list}'")
+        logger.info(f"  already in {file_name}: {old_srs_list}")
+        logger.info(f"  found in Layman internal definition: {set(new_epsg.keys())}")
+        logger.info(f"  to download from epsg.io: {srs_to_download}")
+    else:
+        logger.info(f"No change in GeoServer EPSG definition for SRS list: '{srs_list}'")
+    for code in srs_to_download:
+        try:
+            url = f'http://epsg.io/{code}.geoserver'
+            res = requests.get(url,
+                               timeout=5)
+            res.raise_for_status()
+            new_epsg[code] = res.text
+        except BaseException as ex:
+            logger.warning(f'Not able to download EPSG definition from epsg.id for code={code}. Reason={ex}')
+
+    with open(file_path, "a") as f:
+        for epsg_definition in new_epsg.values():
+            f.write(epsg_definition)
+            f.write('\n')
