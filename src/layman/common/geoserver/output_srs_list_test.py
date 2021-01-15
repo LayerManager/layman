020b5b2dff9f43e4ba94c8c482c88a76059f3c75
Jiri Kozel <jirik@users.noreply.github.com>
2021-01-15 14:25:22 +0100

    Test spatial precision of EPSG 5514 and some others

--- src/layman/common/geoserver/output_srs_list_test.py
+++ src/layman/common/geoserver/output_srs_list_test.py
@@ -6,8 +6,11 @@ from test import process, process_client, geoserver_client
 LAYERS_TO_DELETE_AFTER_TEST = []
 
 
+OUTPUT_SRS_LIST = [4326, 3857, 32633, 32634, 3059, 5514]
+
+
 def test_default_srs_list():
-    assert settings.LAYMAN_OUTPUT_SRS_LIST == [4326, 3857]
+    assert settings.LAYMAN_OUTPUT_SRS_LIST == [4326, 3857, 5514]
 
 
 @pytest.fixture(scope="module")
@@ -19,28 +22,34 @@ def delete_layer_after_test():
         process_client.delete_layer(workspace, layername)
 
 
+@pytest.fixture(scope="module")
+def ensure_layer(delete_layer_after_test):
+    def ensure_layer_internal(workspace, layername, file_paths=None):
+        if (workspace, layername) not in LAYERS_TO_DELETE_AFTER_TEST:
+            process_client.publish_layer(workspace, layername, file_paths=file_paths)
+            delete_layer_after_test(workspace, layername)
+    yield ensure_layer_internal
+
+
 @pytest.mark.timeout(20)
-def test_custom_srs_list(delete_layer_after_test):
+def test_custom_srs_list(ensure_layer):
     workspace = 'test_custom_srs_list_workspace'
     layername1 = 'test_custom_srs_list_layer1'
     layername2 = 'test_custom_srs_list_layer2'
-    output_srs_list = [4326, 3857, 32633, 32634, 3059]
-    assert settings.LAYMAN_OUTPUT_SRS_LIST != output_srs_list
+    assert settings.LAYMAN_OUTPUT_SRS_LIST != OUTPUT_SRS_LIST
 
     process.ensure_layman_function(process.LAYMAN_DEFAULT_SETTINGS)
-    process_client.publish_layer(workspace, layername1)
-    delete_layer_after_test(workspace, layername1)
+    ensure_layer(workspace, layername1)
     assert_wms_output_srs_list(workspace, layername1, settings.LAYMAN_OUTPUT_SRS_LIST)
     assert_wfs_output_srs_list(workspace, layername1, settings.LAYMAN_OUTPUT_SRS_LIST)
 
     process.ensure_layman_function({
-        'LAYMAN_OUTPUT_SRS_LIST': ','.join([str(code) for code in output_srs_list])
+        'LAYMAN_OUTPUT_SRS_LIST': ','.join([str(code) for code in OUTPUT_SRS_LIST])
     })
-    process_client.publish_layer(workspace, layername2)
-    delete_layer_after_test(workspace, layername2)
+    ensure_layer(workspace, layername2)
     for layername in [layername1, layername2]:
-        assert_wms_output_srs_list(workspace, layername, output_srs_list)
-        assert_wfs_output_srs_list(workspace, layername, output_srs_list)
+        assert_wms_output_srs_list(workspace, layername, OUTPUT_SRS_LIST)
+        assert_wfs_output_srs_list(workspace, layername, OUTPUT_SRS_LIST)
 
 
 def assert_wms_output_srs_list(workspace, layername, expected_output_srs_list):
@@ -59,3 +68,28 @@ def assert_wfs_output_srs_list(workspace, layername, expected_output_srs_list):
     crs_names = [str(crs) for crs in wfs_layer.crsOptions]
     for expected_output_srs in expected_output_srs_list:
         assert f"urn:ogc:def:crs:EPSG::{expected_output_srs}" in crs_names
+
+
+# expected coordinates manually copied from QGIS 3.16.2 in given EPSG
+# point_id 1: northernmost vertex of fountain at Moravske namesti, Brno
+@pytest.mark.parametrize('point_id, epsg_code, exp_coordinates, precision', [
+    (1, 3857, (1848649.486, 6308703.297), 0.2),
+    # ~5 meters! By default, GeoServer limits WFS output to 4 decimal places, about 10 m accuracy
+    (1, 4326, (16.60669976, 49.19904767), 0.00005),
+    (1, 32633, (617046.8503, 5450825.7990), 0.1),
+    (1, 32634, (179991.0748, 5458879.0878), 0.1),
+    (1, 5514, (-598208.8093, -1160307.4484), 0.1),
+])
+def test_spatial_precision(ensure_layer, point_id, epsg_code, exp_coordinates, precision):
+    process.ensure_layman_function({
+        'LAYMAN_OUTPUT_SRS_LIST': ','.join([str(code) for code in OUTPUT_SRS_LIST])
+    })
+    workspace = 'test_coordinate_precision_workspace'
+    layername = 'test_coordinate_precision_layer'
+
+    ensure_layer(workspace, layername, file_paths=['sample/layman.layer/sample_point_cz.geojson'])
+
+    feature_collection = geoserver_client.get_features(workspace, layername, epsg_code=epsg_code)
+    feature = next(f for f in feature_collection['features'] if f['properties']['point_id'] == point_id)
+    for idx, coordinate in enumerate(feature['geometry']['coordinates']):
+        assert abs(coordinate - exp_coordinates[idx]) <= precision, f"EPSG:{epsg_code}: expected coordinates={exp_coordinates}, found coordinates={feature['geometry']['coordinates']}"
--- test/geoserver_client.py
+++ test/geoserver_client.py
@@ -1,6 +1,8 @@
+import requests
 from layman import app
 from layman.util import url_for
 from layman.layer.geoserver.util import wfs_direct, wms_direct
+from layman.layer.geoserver import wfs
 
 
 def get_url(workspace, service_endpoint):
@@ -13,6 +15,10 @@ def get_wms_url(workspace, service_endpoint='ows'):
     return get_url(workspace, service_endpoint)
 
 
+def get_wfs_url(workspace, service_endpoint='wfs'):
+    return get_url(workspace, service_endpoint)
+
+
 def get_wfs_capabilities(workspace=None, service_endpoint='wfs', headers=None):
     wfs_url = get_url(workspace, service_endpoint)
     return wfs_direct(wfs_url, headers=headers)
@@ -21,3 +27,20 @@ def get_wfs_capabilities(workspace=None, service_endpoint='wfs', headers=None):
 def get_wms_capabilities(workspace=None, service_endpoint='ows', headers=None):
     wms_url = get_url(workspace, service_endpoint)
     return wms_direct(wms_url, headers=headers)
+
+
+def get_features(workspace, feature_type, epsg_code=3857):
+    wfs_url = get_wfs_url(workspace)
+    epsg_name = f"urn:ogc:def:crs:EPSG::{epsg_code}"
+    r = requests.get(wfs_url, params={
+        'service': 'WFS',
+        'request': 'GetFeature',
+        'version': wfs.VERSION,
+        'typeNames': f"{workspace}:{feature_type}",
+        'outputFormat': f"application/json",
+        'srsName': epsg_name,
+    })
+    r.raise_for_status()
+    result = r.json()
+    assert result['crs']['properties']['name'] == epsg_name
+    return result
