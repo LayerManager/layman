dbcf9fbc8a0d01b4425ba6f8d676c4a3c994f8e3
Jiri Kozel <jirik@users.noreply.github.com>
2021-02-02 15:56:30 +0100

    Add test that SLD style is applied to WMS
    Using image comparing

--- src/layman/layer/geoserver/sld_test.py
+++ src/layman/layer/geoserver/sld_test.py
@@ -1,3 +1,4 @@
+import os
 import pytest
 import requests
 
@@ -37,3 +38,36 @@ def test_sld_style_in_wms_workspace():
                                 layer)
 
 
+@pytest.mark.usefixtures('ensure_layman')
+@pytest.mark.xfail(reason='Not implemented yet')
+def test_sld_style_applied_in_wms():
+    workspace = 'test_sld_style_wms_workspace'
+    layer = 'test_sld_style_wms_layer'
+    geojson_file = ['/code/tmp/naturalearth/110m/cultural/ne_110m_admin_0_countries.geojson']
+    style_file = 'sample/style/generic-blue.xml'
+    expected_file = 'sample/style/countries_wms_blue.png'
+    obtained_file = 'tmp/artifacts/test_sld_style_applied_in_wms.png'
+
+    process_client.publish_layer(workspace,
+                                 layer,
+                                 file_paths=geojson_file,
+                                 style_file=style_file)
+
+    url = f"http://{settings.LAYMAN_SERVER_NAME}/geoserver/{workspace}_wms/wms?SERVICE=WMS&VERSION=1.1.1&REQUEST=GetMap&FORMAT=image/png&TRANSPARENT=true&STYLES=&LAYERS={workspace}:{layer}&SRS=EPSG:3857&WIDTH=768&HEIGHT=752&BBOX=-30022616.05686392,-30569903.32873383,30022616.05686392,28224386.44929134"
+
+    r = requests.get(url,
+                     timeout=5,
+                     stream=True,
+                     )
+    r.raise_for_status()
+    with open(obtained_file, 'wb') as f:
+        for chunk in r:
+            f.write(chunk)
+
+    diffs = util.compare_images(expected_file, obtained_file)
+
+    assert diffs < 2000
+
+    os.remove(obtained_file)
+    process_client.delete_layer(workspace,
+                                layer)
