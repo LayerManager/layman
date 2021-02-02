56d2966caa99c6bf576137a8a8a422ad13fcc21d
Jiri Kozel <jirik@users.noreply.github.com>
2021-02-02 15:40:50 +0100

    Add util.compare_images to test

--- test/util.py
+++ test/util.py
@@ -1,6 +1,7 @@
 import requests
 import time
 from requests.exceptions import ConnectionError
+from PIL import Image, ImageChops
 
 
 # utils
@@ -18,3 +19,21 @@ def wait_for_url(url, max_attempts, sleeping_time):
                 raise e
             attempt += 1
         time.sleep(sleeping_time)
+
+
+def compare_images(image1, image2):
+    expected_image = Image.open(image1)
+    current_image = Image.open(image2)
+
+    diff_image = ImageChops.difference(expected_image, current_image)
+
+    diffs = 0
+
+    for x in range(diff_image.width):
+        for y in range(diff_image.height):
+            pixel_diff = diff_image.getpixel((x, y))
+            if pixel_diff != (0, 0, 0, 0) and \
+                    (expected_image.getpixel((x, y))[3] > 0 or current_image.getpixel((x, y))[3] > 0):
+                diffs += 1
+
+    return diffs
