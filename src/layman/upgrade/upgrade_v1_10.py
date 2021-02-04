cbff0784e2bc5d3105fe420b2561c268383d691a
index-git <sileny.index@gmail.com>
2021-02-04 15:14:56 +0100

    Migrate layers into geoserver WMS workspace in v1.10.0

--- src/layman/upgrade/upgrade_v1_10.py
+++ src/layman/upgrade/upgrade_v1_10.py
@@ -1,6 +1,12 @@
+import io
 from layman import settings
 from layman.http import LaymanError
 from layman.common import prime_db_schema
+from layman.common import geoserver as gs_common
+from layman import util
+from layman.layer import LAYER_TYPE
+from layman.layer import geoserver
+from layman.layer.geoserver import wms
 
 
 def check_usernames_for_wms_suffix():
@@ -14,3 +20,26 @@ def check_usernames_for_wms_suffix():
                               data={'workspace': workspace,
                                     }
                               )
+
+
+def migrate_layers_to_wms_workspace(workspace=None):
+    infos = util.get_publication_infos(publ_type=LAYER_TYPE, workspace=workspace)
+    for (workspace, publication_type, layer) in infos.keys():
+        info = util.get_publication_info(workspace, publication_type, layer)
+        geoserver_workspace = wms.get_geoserver_workspace(workspace)
+        geoserver.ensure_workspace(workspace)
+
+        geoserver.publish_layer_from_db(workspace,
+                                        layer,
+                                        info.get('description'),
+                                        info.get('title'),
+                                        info.get('access_rights'),
+                                        geoserver_workspace=geoserver_workspace)
+        wms.clear_cache(workspace)
+
+        sld_r = gs_common.get_workspace_style_response(workspace, layer, auth=settings.LAYMAN_GS_AUTH)
+        sld_stream = io.BytesIO(sld_r.content)
+        gs_common.post_workspace_sld_style(geoserver_workspace, layer, sld_stream)
+        wms.clear_cache(workspace)
+
+        gs_common.delete_workspace_style(workspace, layer, auth=settings.LAYMAN_GS_AUTH)
