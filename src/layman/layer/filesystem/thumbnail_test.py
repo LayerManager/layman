1317ecedff3e4b030dac8a78900e7c628f7937f0
Jiri Kozel <jirik@users.noreply.github.com>
2021-03-08 11:36:04 +0100

    If layer thumbnail rendering fails, do not save file
    And report exception
    
    Fix #281

--- src/layman/layer/filesystem/thumbnail_test.py
+++ src/layman/layer/filesystem/thumbnail_test.py
@@ -32,3 +32,37 @@ def test_sld_style_applied_in_thumbnail():
 
     process_client.delete_layer(workspace,
                                 layer)
+
+
+@pytest.mark.usefixtures('ensure_layman')
+def test_wrong_sld_causes_no_thumbnail():
+    workspace = 'test_wrong_sld_causes_no_thumbnail_workspace'
+    layer = 'test_wrong_sld_causes_no_thumbnail_layer'
+    geojson_file = ['/code/sample/layman.layer/sample_point_cz.geojson']
+    style_file = '/code/sample/layman.layer/sample_point_cz_wrong_literal.sld'
+
+    def wait_for_thumbnail_error(response):
+        ok_keys = ['db_table', 'wms', 'wfs', 'file']
+        if response.status_code == 200:
+            r_json = response.json()
+            return response.status_code == 200 and all(
+                'status' not in r_json[k] for k in ok_keys
+            ) and 'status' in r_json['thumbnail'] and r_json['thumbnail']['status'] in ['FAILURE']
+        else:
+            return False
+
+    process_client.publish_layer(workspace,
+                                 layer,
+                                 file_paths=geojson_file,
+                                 style_file=style_file,
+                                 check_response_fn=wait_for_thumbnail_error,
+                                 )
+
+    layer_info = process_client.get_layer(workspace, layer)
+
+    assert 'error' in layer_info['thumbnail']
+    assert layer_info['thumbnail']['error']['message'] == 'Thumbnail rendering failed'
+    assert layer_info['thumbnail']['error']['code'] == -1
+
+    process_client.delete_layer(workspace,
+                                layer)
