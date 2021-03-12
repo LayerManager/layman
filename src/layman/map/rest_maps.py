f12ed9da4463470816f646efe15eef2f398ae8c8
Jiri Kozel <jirik@users.noreply.github.com>
2021-03-12 16:25:17 +0100

    Extract GET Layers and GET Maps logic into get_publications method

--- src/layman/map/rest_maps.py
+++ src/layman/map/rest_maps.py
@@ -1,11 +1,11 @@
 from flask import Blueprint, jsonify, g
 from flask import current_app as app
 
-from layman.util import url_for
-from layman import util as layman_util, settings
+from layman import settings
 from . import MAP_TYPE, MAP_REST_PATH_NAME
 from layman.authn import authenticate
 from layman.authz import authorize_publications_decorator
+from layman.common import rest as rest_common
 
 bp = Blueprint('rest_maps', __name__)
 
@@ -22,19 +22,4 @@ def get():
     app.logger.info(f"GET Maps, user={g.user}")
 
     user = g.user.get('username') if g.user else settings.ANONYM_USER
-    map_infos_whole = layman_util.get_publication_infos(publ_type=MAP_TYPE, context={'actor_name': user,
-                                                                                     'access_type': 'read',
-                                                                                     })
-
-    infos = [
-        {
-            'name': name,
-            'workspace': workspace,
-            'title': info.get("title", None),
-            'url': url_for('rest_workspace_map.get', mapname=name, username=workspace),
-            'uuid': info["uuid"],
-            'access_rights': info['access_rights'],
-        }
-        for (workspace, _, name), info in map_infos_whole.items()
-    ]
-    return jsonify(infos), 200
+    return rest_common.get_publications(MAP_TYPE, user)
