79b4a91b45cad9c33e88f38cea7b4672329f8002
index-git <sileny.index@gmail.com>
2021-03-15 14:03:59 +0100

    Use get_authn_username instead of g.user

--- src/layman/map/rest_maps.py
+++ src/layman/map/rest_maps.py
@@ -3,7 +3,7 @@ from flask import current_app as app
 
 from layman import settings
 from . import MAP_TYPE, MAP_REST_PATH_NAME
-from layman.authn import authenticate
+from layman.authn import authenticate, get_authn_username
 from layman.authz import authorize_publications_decorator
 from layman.common import rest as rest_common
 
@@ -21,5 +21,5 @@ def before_request():
 def get():
     app.logger.info(f"GET Maps, user={g.user}")
 
-    user = g.user.get('username') if g.user else settings.ANONYM_USER
+    user = get_authn_username() or settings.ANONYM_USER
     return rest_common.get_publications(MAP_TYPE, user, request.args)
