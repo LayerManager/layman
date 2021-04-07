ba5e5fd8afa5c74aa4fb60729779f211f87bda3d
index-git <sileny.index@gmail.com>
2021-04-07 09:37:38 +0200

    Refactoring

--- src/layman/upgrade/upgrade_v1_12.py
+++ src/layman/upgrade/upgrade_v1_12.py
@@ -11,18 +11,17 @@ from layman.layer.geoserver import wms
 from layman.layer.micka import csw as layer_csw
 
 logger = logging.getLogger(__name__)
-
-db_schema = settings.LAYMAN_PRIME_SCHEMA
+DB_SCHEMA = settings.LAYMAN_PRIME_SCHEMA
 
 
 def adjust_prime_db_schema_for_fulltext_search():
     logger.info(f'    Starting - alter DB prime schema for fulltext search')
     statement = f'''CREATE EXTENSION IF NOT EXISTS unaccent;
-    drop index if exists {db_schema}.title_tsv_idx;
-    drop function if exists {db_schema}.my_unaccent;
+    drop index if exists {DB_SCHEMA}.title_tsv_idx;
+    drop function if exists {DB_SCHEMA}.my_unaccent;
 
-    CREATE FUNCTION {db_schema}.my_unaccent(text) RETURNS tsvector LANGUAGE SQL IMMUTABLE AS 'SELECT to_tsvector(unaccent($1))';
-    CREATE INDEX title_tsv_idx ON {db_schema}.publications USING GIST ({db_schema}.my_unaccent(title));
+    CREATE FUNCTION {DB_SCHEMA}.my_unaccent(text) RETURNS tsvector LANGUAGE SQL IMMUTABLE AS 'SELECT to_tsvector(unaccent($1))';
+    CREATE INDEX title_tsv_idx ON {DB_SCHEMA}.publications USING GIST ({DB_SCHEMA}.my_unaccent(title));
     '''
 
     db_util.run_statement(statement)
@@ -31,7 +30,7 @@ def adjust_prime_db_schema_for_fulltext_search():
 
 def adjust_prime_db_schema_for_last_change_search():
     logger.info(f'    Starting - alter DB prime schema for search by updated_at')
-    statement = f'ALTER TABLE {db_schema}.publications ADD COLUMN IF NOT EXISTS updated_at timestamp with time zone;'
+    statement = f'ALTER TABLE {DB_SCHEMA}.publications ADD COLUMN IF NOT EXISTS updated_at timestamp with time zone;'
     db_util.run_statement(statement)
 
     logger.info(f'      Set updated_at for all publications')
@@ -39,8 +38,8 @@ def adjust_prime_db_schema_for_last_change_search():
        w.name,
        p.type,
        p.name
-from {db_schema}.publications p inner join
-     {db_schema}.workspaces w on w.id = p.id_workspace
+from {DB_SCHEMA}.publications p inner join
+     {DB_SCHEMA}.workspaces w on w.id = p.id_workspace
 ;'''
     publications = db_util.run_query(query)
     for (id, workspace, type, name, ) in publications:
@@ -59,10 +58,10 @@ from {db_schema}.publications p inner join
         updated_at = datetime.datetime.fromtimestamp(updated_at, datetime.timezone.utc)\
             if updated_at else datetime.datetime.now(datetime.timezone.utc)
 
-        update = f'update {db_schema}.publications set updated_at = %s where id = %s;'
+        update = f'update {DB_SCHEMA}.publications set updated_at = %s where id = %s;'
         db_util.run_statement(update, (updated_at, id, ))
 
-    statement = f'ALTER TABLE {db_schema}.publications ALTER COLUMN updated_at SET NOT NULL;'
+    statement = f'ALTER TABLE {DB_SCHEMA}.publications ALTER COLUMN updated_at SET NOT NULL;'
     db_util.run_statement(statement)
     logger.info(f'    DONE - alter DB prime schema for search by updated_at')
 
@@ -72,8 +71,8 @@ def migrate_layer_metadata(workspace_filter=None):
     query = f'''
     select  w.name,
             p.name
-    from {db_schema}.publications p inner join
-         {db_schema}.workspaces w on w.id = p.id_workspace
+    from {DB_SCHEMA}.publications p inner join
+         {DB_SCHEMA}.workspaces w on w.id = p.id_workspace
     where p.type = %s
     '''
     params = (LAYER_TYPE,)
@@ -103,6 +102,6 @@ def migrate_layer_metadata(workspace_filter=None):
 
 def adjust_prime_db_schema_for_bbox_search():
     logger.info(f'    Starting - alter DB prime schema for search by bbox')
-    statement = f'ALTER TABLE {db_schema}.publications ADD COLUMN IF NOT EXISTS bbox box2d;'
+    statement = f'ALTER TABLE {DB_SCHEMA}.publications ADD COLUMN IF NOT EXISTS bbox box2d;'
     db_util.run_statement(statement)
     logger.info(f'    DONE - alter DB prime schema for search by bbox')
