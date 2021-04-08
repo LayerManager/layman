758df6dae6491b7408b49a6331a9902238891440
Jiri Kozel <jirik@users.noreply.github.com>
2021-04-08 10:03:48 +0200

    Migrate layer metadata records: Add LAYERS URL parameter
    Part of #302

--- src/layman/upgrade/upgrade_v1_12_test.py
+++ src/layman/upgrade/upgrade_v1_12_test.py
@@ -3,10 +3,61 @@ from test import process_client
 import pytest
 
 from layman import app, settings
+from layman.common import geoserver as gs_common
+from layman.common.filesystem import uuid as uuid_common
+from layman.common.micka import util as micka_util
 from layman.common.prime_db_schema import util as db_util
+from layman.layer import geoserver as gs_layer, NO_STYLE_DEF, db
+from layman.layer.geoserver import wms
+from layman.layer.prime_db_schema import table as prime_db_schema_table
+from layman.uuid import generate_uuid
 from . import upgrade_v1_12
 
 db_schema = settings.LAYMAN_PRIME_SCHEMA
+
+
+@pytest.fixture()
+def ensure_layer():
+    def ensure_layer_internal(workspace, layer):
+        access_rights = {'read': [settings.RIGHTS_EVERYONE_ROLE], 'write': [settings.RIGHTS_EVERYONE_ROLE], }
+        with app.app_context():
+            uuid_str = generate_uuid()
+            prime_db_schema_table.post_layer(workspace,
+                                             layer,
+                                             access_rights,
+                                             layer,
+                                             uuid_str,
+                                             None,
+                                             NO_STYLE_DEF,
+                                             )
+            file_path = '/code/sample/layman.layer/small_layer.geojson'
+            uuid_common.assign_publication_uuid('layman.layer', workspace, layer, uuid_str=uuid_str)
+            db.ensure_workspace(workspace)
+            db.import_layer_vector_file(workspace, layer, file_path, None)
+            # wfs
+            created = gs_common.ensure_workspace(workspace, settings.LAYMAN_GS_AUTH)
+            if created:
+                gs_common.create_db_store(workspace, settings.LAYMAN_GS_AUTH, db_schema=workspace)
+            gs_layer.publish_layer_from_db(workspace, layer, layer, layer, None, workspace)
+            # wms
+            geoserver_workspace = wms.get_geoserver_workspace(workspace)
+            created = gs_common.ensure_workspace(geoserver_workspace, settings.LAYMAN_GS_AUTH)
+            if created:
+                gs_common.create_db_store(geoserver_workspace, settings.LAYMAN_GS_AUTH, db_schema=workspace)
+            gs_layer.publish_layer_from_db(workspace, layer, layer, layer, None, geoserver_workspace)
+
+            md_path = '/code/src/layman/upgrade/upgrade_v1_12_test_layer_metadata.xml'
+            with open(md_path, 'r') as template_file:
+                md_template = template_file.read()
+            record = md_template.format(uuid=uuid_str,
+                                        md_date_stamp=datetime.date.today().strftime('%Y-%m-%d'),
+                                        publication_date=datetime.date.today().strftime('%Y-%m-%d'),
+                                        workspace=workspace,
+                                        layer=layer,
+                                        )
+            micka_util.soap_insert_record(record, is_public=True)
+
+    yield ensure_layer_internal
 
 
 @pytest.mark.usefixtures('ensure_layman')
@@ -53,3 +104,46 @@ where w.name = %s
 
     process_client.delete_workspace_layer(workspace, layer)
     process_client.delete_workspace_map(workspace, map)
+
+
+@pytest.mark.usefixtures('ensure_layman')
+def test_migrate_layer_metadata(ensure_layer):
+    def assert_md_keys(layer_info):
+        for key in ['comparison_url', 'csw_url', 'identifier', 'record_url']:
+            assert key in layer_info['metadata']
+
+    def assert_csw_value(md_comparison, prop_key, exp_value):
+        csw_prefix = f"http://localhost:3080/csw"
+        csw_src_key = process_client.get_source_key_from_metadata_comparison(md_comparison, csw_prefix)
+        assert csw_src_key is not None
+        md_props = md_comparison['metadata_properties']
+        assert md_props[prop_key]['equal'] is True
+        assert md_props[prop_key]['equal_or_null'] is True
+        assert md_props[prop_key]['values'][csw_src_key] == exp_value
+
+    workspace = 'test_migrate_layer_metadata_workspace'
+    layer = 'test_migrate_layer_metadata_layer'
+    ensure_layer(workspace, layer)
+
+    layer_info = process_client.get_workspace_layer(workspace, layer)
+    assert_md_keys(layer_info)
+
+    md_comparison = process_client.get_workspace_layer_metadata_comparison(workspace, layer)
+    exp_wms_url = f"http://localhost:8000/geoserver/{workspace}_wms/ows?SERVICE=WMS&REQUEST=GetCapabilities&VERSION=1.3.0"
+    assert_csw_value(md_comparison, 'wms_url', exp_wms_url)
+    exp_wfs_url = f"http://localhost:8000/geoserver/{workspace}/wfs?SERVICE=WFS&REQUEST=GetCapabilities&VERSION=2.0.0"
+    assert_csw_value(md_comparison, 'wfs_url', exp_wfs_url)
+
+    with app.app_context():
+        upgrade_v1_12.migrate_layer_metadata(workspace)
+
+    layer_info = process_client.get_workspace_layer(workspace, layer)
+    assert_md_keys(layer_info)
+
+    md_comparison = process_client.get_workspace_layer_metadata_comparison(workspace, layer)
+    exp_wms_url = f"{exp_wms_url}&LAYERS={layer}"
+    assert_csw_value(md_comparison, 'wms_url', exp_wms_url)
+    exp_wfs_url = f"{exp_wfs_url}&LAYERS={layer}"
+    assert_csw_value(md_comparison, 'wfs_url', exp_wfs_url)
+
+    process_client.delete_workspace_layer(workspace, layer)
