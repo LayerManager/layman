d75993ffdb56d05e8c2674fa99b4601bd9e62735
index-git <sileny.index@gmail.com>
2021-04-08 16:02:41 +0200

    Move set_bbox method to common/prime_db_schema

--- src/layman/layer/prime_db_schema/tasks.py
+++ src/layman/layer/prime_db_schema/tasks.py
@@ -2,9 +2,9 @@ from celery.utils.log import get_task_logger
 
 from layman.celery import AbortedException
 from layman import celery_app
+from .. import LAYER_TYPE
 from ..db import get_bbox as db_get_bbox
-from .bbox import set_bbox
-
+from ...common.prime_db_schema.publications import set_bbox
 
 logger = get_task_logger(__name__)
 
@@ -27,7 +27,7 @@ def refresh_bbox(
         raise AbortedException
 
     bbox = db_get_bbox(username, layername)
-    set_bbox(username, layername, bbox, )
+    set_bbox(username, LAYER_TYPE, layername, bbox, )
 
     if self.is_aborted():
         raise AbortedException
