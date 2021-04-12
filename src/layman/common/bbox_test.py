43cdb0878dc5072bbb8c34d043e5ff8f99dce797
Jiri Kozel <jirik@users.noreply.github.com>
2021-04-12 11:40:48 +0200

    Add contains_bbox util method

--- src/layman/common/bbox_test.py
+++ src/layman/common/bbox_test.py
@@ -13,3 +13,15 @@ from . import bbox as bbox_util
 ])
 def test_is_empty(bbox, expected_result):
     assert bbox_util.is_empty(bbox) == expected_result
+
+
+@pytest.mark.parametrize('bbox1, bbox2, expected_result', [
+    ((1, 1, 4, 4, ), (2, 2, 3, 3, ), True),
+    ((1, 1, 4, 4, ), (1, 1, 4, 4, ), True),
+    ((1, 1, 4, 4, ), (1, 1, 4, 5, ), False),
+    ((-4, -4, -1, -1, ), (-3, -3, -2, -2, ), True),
+    ((1, 1, 4, 4, ), (None, None, None, None, ), False),
+    ((None, None, None, None, ), (1, 1, 4, 4, ), False),
+])
+def test_contains_bbox(bbox1, bbox2, expected_result):
+    assert bbox_util.contains_bbox(bbox1, bbox2) == expected_result
