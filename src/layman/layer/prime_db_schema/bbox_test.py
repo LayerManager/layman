376d8998c96a9a83560576a00e5cb873e7d5a1f9
index-git <sileny.index@gmail.com>
2021-04-12 15:41:55 +0200

    Add tests for bbox calculation

--- src/layman/layer/prime_db_schema/bbox_test.py
+++ src/layman/layer/prime_db_schema/bbox_test.py
@@ -15,4 +15,10 @@ def test_bbox():
         info = util.get_layer_info(workspace, layer)
     test_util.assert_same_bboxes(info['bounding_box'], test_data.SMALL_LAYER_BBOX, 0.00001)
 
+    process_client.patch_workspace_layer(workspace, layer, file_paths=['test/data/bbox/layer_3_3-5_5.geojson', ])
+
+    with app.app_context():
+        info = util.get_layer_info(workspace, layer)
+    test_util.assert_same_bboxes(info['bounding_box'], [3000, 3000, 5000, 5000], 0.1)
+
     process_client.delete_workspace_layer(workspace, layer)
