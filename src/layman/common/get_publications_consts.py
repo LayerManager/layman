6b4d8d3b800cc71aed49e2a280f9fb753ae113be
Jiri Kozel <jirik@users.noreply.github.com>
2021-04-13 09:41:44 +0200

    Add 'bbox' as order_by option, check valid combination

--- src/layman/common/get_publications_consts.py
+++ src/layman/common/get_publications_consts.py
@@ -1,13 +1,11 @@
-FILTER_FULL_TEXT = 'full_text_filter'
-
-FILTER_BBOX = 'bbox_filter'
-
-ORDERING_BBOX = 'ordering_bbox'
-
 BBOX_PATTERN = r"^(-?\d+(?:\.\d+)?),(-?\d+(?:\.\d+)?),(-?\d+(?:\.\d+)?),(-?\d+(?:\.\d+)?)$"
 
-ORDER_BY_PARAM = 'order_by'
+FILTER_FULL_TEXT = 'full_text_filter'
+FILTER_BBOX = 'bbox_filter'
 
+ORDER_BY_PARAM = 'order_by'
 ORDER_BY_FULL_TEXT = 'full_text'
 ORDER_BY_TITLE = 'title'
 ORDER_BY_LAST_CHANGE = 'last_change'
+ORDER_BY_BBOX = 'bbox'
+ORDERING_BBOX = 'ordering_bbox'
