de362806f0884d0d30cd2e20b7f9ece640e97841
index-git <sileny.index@gmail.com>
2021-04-20 11:09:33 +0200

    Add query_params parameter to  get_workspace_publications method

--- test/process_client.py
+++ test/process_client.py
@@ -258,19 +258,20 @@ publish_workspace_map = partial(publish_workspace_publication, MAP_TYPE)
 publish_workspace_layer = partial(publish_workspace_publication, LAYER_TYPE)
 
 
-def get_workspace_publications_response(publication_type, workspace, headers=None,):
+def get_workspace_publications_response(publication_type, workspace, headers=None, query_params=None, ):
     headers = headers or {}
+    query_params = query_params or {}
     publication_type_def = PUBLICATION_TYPES_DEF[publication_type]
 
     with app.app_context():
         r_url = url_for(publication_type_def.get_workspace_publications_url, username=workspace)
-    r = requests.get(r_url, headers=headers,)
+    r = requests.get(r_url, headers=headers, params=query_params)
     raise_layman_error(r)
     return r
 
 
-def get_workspace_publications(publication_type, workspace, headers=None, ):
-    return get_workspace_publications_response(publication_type, workspace, headers,).json()
+def get_workspace_publications(publication_type, workspace, headers=None, query_params=None, ):
+    return get_workspace_publications_response(publication_type, workspace, headers, query_params,).json()
 
 
 get_workspace_maps = partial(get_workspace_publications, MAP_TYPE)
