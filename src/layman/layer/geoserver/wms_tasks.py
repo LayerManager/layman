0852e708ac2af64b6fddc683b8d39051883a519a
index-git <sileny.index@gmail.com>
2021-05-02 19:28:47 +0200

    geoserver.wms: patch_after_wfst

---
+++ src/layman/layer/geoserver/wms_tasks.py
@@ -0,0 +1,34 @@
+from geoserver import util as gs_util
+from layman import celery_app, settings, util as layman_util
+from layman.celery import AbortedException
+from . import wms
+from .. import geoserver, LAYER_TYPE
+
+headers_json = geoserver.headers_json
+
+
+@celery_app.task(
+    name='layman.layer.geoserver.wms.patch_after_wfst',
+    bind=True,
+    base=celery_app.AbortableTask
+)
+def patch_after_wfst(
+        self,
+        workspace,
+        layer,
+):
+    if self.is_aborted():
+        raise AbortedException
+
+    bbox = geoserver.get_layer_native_bbox(workspace, layer)
+    geoserver_workspace = wms.get_geoserver_workspace(workspace)
+    style_type = layman_util.get_publication_info(workspace, LAYER_TYPE, layer, context={'keys': ['style_type'], })['style_type']
+    if style_type == 'sld':
+        gs_util.patch_feature_type(geoserver_workspace, layer, auth=settings.LAYMAN_GS_AUTH, bbox=bbox)
+    elif style_type == 'qml':
+        gs_util.patch_wms_layer(geoserver_workspace, layer, auth=settings.LAYMAN_GS_AUTH, bbox=bbox)
+
+    wms.clear_cache(workspace)
+
+    if self.is_aborted():
+        raise AbortedException
