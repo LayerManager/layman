fce02803c3fe7625b0b76a6a10e1e1997a8815b1
index-git <sileny.index@gmail.com>
2021-05-03 09:53:11 +0200

    qgis.wms: patch_after_wfst

---
+++ src/layman/layer/qgis/wms_tasks.py
@@ -0,0 +1,29 @@
+from celery.utils.log import get_task_logger
+
+from layman import celery_app, util as layman_util
+from layman.celery import AbortedException
+from . import wms
+from .. import LAYER_TYPE
+
+logger = get_task_logger(__name__)
+
+
+@celery_app.task(
+    name='layman.layer.qgis.wms.patch_after_wfst',
+    bind=True,
+    base=celery_app.AbortableTask
+)
+def patch_after_wfst(
+        self,
+        workspace,
+        layer,
+):
+    if self.is_aborted():
+        raise AbortedException
+
+    style_type = layman_util.get_publication_info(workspace, LAYER_TYPE, layer, context={'keys': ['style_type'], })['style_type']
+    if style_type == 'qml':
+        wms.save_qgs_file(workspace, layer)
+
+    if self.is_aborted():
+        raise AbortedException
