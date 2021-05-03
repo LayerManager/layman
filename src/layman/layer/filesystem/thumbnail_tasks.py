c3c22b89ea01ae159ae129a9205f562dbf14471f
Jiri Kozel <jirik@users.noreply.github.com>
2021-05-03 17:21:55 +0200

    filesystem.thumbnail: patch_after_wfst

---
+++ src/layman/layer/filesystem/thumbnail_tasks.py
@@ -0,0 +1,21 @@
+from celery.utils.log import get_task_logger
+
+from layman.celery import AbortedException
+from layman import celery_app
+from . import thumbnail
+
+logger = get_task_logger(__name__)
+
+
+@celery_app.task(
+    name='layman.layer.filesystem.thumbnail.patch_after_wfst',
+    bind=True,
+    base=celery_app.AbortableTask
+)
+def patch_after_wfst(self, workspace, layer):
+    if self.is_aborted():
+        raise AbortedException
+    thumbnail.generate_layer_thumbnail(workspace, layer)
+
+    if self.is_aborted():
+        raise AbortedException
