7274a55d312d1382fa11e0412c1c2accd3d6d127
index-git <sileny.index@gmail.com>
2021-05-06 15:26:38 +0200

    Solve concurrency of WFS-T and other locks

--- src/layman/requests_concurrency_test.py
+++ src/layman/requests_concurrency_test.py
@@ -1,9 +1,8 @@
-import time
+from test import process_client
+from test.data import wfs as data_wfs
 import requests
 import pytest
 
-from test import process_client
-from test.data import wfs as data_wfs
 from layman import settings
 from layman.common import empty_method_returns_true
 
@@ -40,24 +39,3 @@ def test_wfst_concurrency():
     assert r.status_code == 200, r.text
 
     process_client.delete_workspace_layer(workspace, layer, )
-
-#
-# @pytest.mark.xfail
-# @pytest.mark.usefixtures('ensure_layman')
-# def test_wfst_concurrency_fail():
-#     workspace = 'test_wfst_concurrency_workspace'
-#     layer = 'test_wfst_concurrency_layer'
-#
-#     data_xml = data_wfs.get_wfs20_insert_points(workspace, layer, )
-#     rest_url = f"http://{settings.LAYMAN_SERVER_NAME}/geoserver/{workspace}/wfs?request=Transaction"
-#     headers = {
-#         'Accept': 'text/xml',
-#         'Content-type': 'text/xml',
-#     }
-#
-#     process_client.publish_workspace_layer(workspace, layer, check_response_fn=empty_method_returns_true)
-#
-#     r = requests.post(rest_url,
-#                       data=data_xml,
-#                       headers=headers)
-#     assert r.status_code == 200, r.text
