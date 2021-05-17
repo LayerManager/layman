b4ab84273ec83b124f1ff654d033bbe607058d7c
index-git <sileny.index@gmail.com>
2021-05-17 15:10:28 +0200

    Improve concurrency test

--- src/layman/requests_concurrency_test.py
+++ src/layman/requests_concurrency_test.py
@@ -1,8 +1,8 @@
 from test import process_client
 import pytest
 
-from layman import celery, util as layman_util, app
-from layman.common import empty_method_returns_true
+from layman import celery, util as layman_util, app, common as common_const
+from layman.common import empty_method_returns_true, redis
 
 
 @pytest.mark.usefixtures('ensure_layman')
@@ -15,33 +15,49 @@ def test_patch_after_feature_change_concurrency(publication_type):
 
     queue = celery.get_run_after_chain_queue(workspace, publication_type, publication)
     assert not queue
+    lock = redis.get_publication_lock(workspace, publication_type, publication)
+    assert not lock
 
     with app.app_context():
         layman_util.patch_after_feature_change(workspace, publication_type, publication)
     queue = celery.get_run_after_chain_queue(workspace, publication_type, publication)
     assert len(queue) == 0, queue
+    lock = redis.get_publication_lock(workspace, publication_type, publication)
+    assert lock == common_const.PUBLICATION_LOCK_FEATURE_CHANGE
 
     process_client.patch_workspace_publication(publication_type, workspace, publication, title='New title',
                                                check_response_fn=empty_method_returns_true)
     queue = celery.get_run_after_chain_queue(workspace, publication_type, publication)
     assert len(queue) == 1, queue
     assert queue == ['layman.util::patch_after_feature_change', ]
+    lock = redis.get_publication_lock(workspace, publication_type, publication)
+    assert lock == common_const.PUBLICATION_LOCK_PATCH or not lock
 
     with app.app_context():
         layman_util.patch_after_feature_change(workspace, publication_type, publication)
     queue = celery.get_run_after_chain_queue(workspace, publication_type, publication)
     assert len(queue) == 1, queue
     assert queue == ['layman.util::patch_after_feature_change', ]
+    lock = redis.get_publication_lock(workspace, publication_type, publication)
+    assert lock == common_const.PUBLICATION_LOCK_FEATURE_CHANGE
 
     with app.app_context():
         layman_util.patch_after_feature_change(workspace, publication_type, publication)
     queue = celery.get_run_after_chain_queue(workspace, publication_type, publication)
     assert len(queue) == 1, queue
     assert queue == ['layman.util::patch_after_feature_change', ]
+    lock = redis.get_publication_lock(workspace, publication_type, publication)
+    assert lock == common_const.PUBLICATION_LOCK_FEATURE_CHANGE
 
     process_client.wait_for_publication_status(workspace, publication_type, publication)
+    queue = celery.get_run_after_chain_queue(workspace, publication_type, publication)
+    assert not queue, queue
+    lock = redis.get_publication_lock(workspace, publication_type, publication)
+    assert not lock
 
     process_client.delete_workspace_publication(publication_type, workspace, publication, )
 
     queue = celery.get_run_after_chain_queue(workspace, publication_type, publication)
     assert not queue, queue
+    lock = redis.get_publication_lock(workspace, publication_type, publication)
+    assert not lock
