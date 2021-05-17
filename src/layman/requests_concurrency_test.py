f43f8b46b4941c0f51e33e4f15f19b14899c98e8
index-git <sileny.index@gmail.com>
2021-05-17 10:17:38 +0200

    Rewrite test_patch_after_feature_change_concurrency to call directly patch_after_feature_change instead of wfs-t query

--- src/layman/requests_concurrency_test.py
+++ src/layman/requests_concurrency_test.py
@@ -1,51 +1,47 @@
-from test import process_client, assert_util
-from test.data import wfs as data_wfs
+from test import process_client
 import pytest
 
-from layman import celery
+from layman import celery, util as layman_util, app
 from layman.common import empty_method_returns_true
 
 
 @pytest.mark.usefixtures('ensure_layman')
-def test_wfst_concurrency():
+def test_patch_after_feature_change_concurrency():
     workspace = 'test_wfst_concurrency_workspace'
     layer = 'test_wfst_concurrency_layer'
+    publication_type = process_client.LAYER_TYPE
 
-    data_xml = data_wfs.get_wfs20_insert_points(workspace, layer, )
+    process_client.publish_workspace_publication(publication_type, workspace, layer, )
 
-    process_client.publish_workspace_layer(workspace, layer, )
-
-    queue = celery.get_run_after_chain_queue(workspace, process_client.LAYER_TYPE, layer)
+    queue = celery.get_run_after_chain_queue(workspace, publication_type, layer)
     assert not queue
 
-    process_client.post_wfst(data_xml, workspace=workspace)
-
-    queue = celery.get_run_after_chain_queue(workspace, process_client.LAYER_TYPE, layer)
+    with app.app_context():
+        layman_util.patch_after_feature_change(workspace, publication_type, layer)
+    queue = celery.get_run_after_chain_queue(workspace, publication_type, layer)
     assert len(queue) == 0, queue
 
-    process_client.patch_workspace_layer(workspace, layer, title='New title', check_response_fn=empty_method_returns_true)
-    queue = celery.get_run_after_chain_queue(workspace, process_client.LAYER_TYPE, layer)
+    process_client.patch_workspace_publication(publication_type, workspace, layer, title='New title',
+                                               check_response_fn=empty_method_returns_true)
+    queue = celery.get_run_after_chain_queue(workspace, publication_type, layer)
     assert len(queue) == 1, queue
     assert queue == ['layman.util::patch_after_feature_change', ]
 
-    process_client.post_wfst(data_xml, workspace=workspace)
-
-    queue = celery.get_run_after_chain_queue(workspace, process_client.LAYER_TYPE, layer)
+    with app.app_context():
+        layman_util.patch_after_feature_change(workspace, publication_type, layer)
+    queue = celery.get_run_after_chain_queue(workspace, publication_type, layer)
     assert len(queue) == 1, queue
     assert queue == ['layman.util::patch_after_feature_change', ]
 
-    process_client.post_wfst(data_xml, workspace=workspace)
-
-    queue = celery.get_run_after_chain_queue(workspace, process_client.LAYER_TYPE, layer)
+    with app.app_context():
+        layman_util.patch_after_feature_change(workspace, publication_type, layer)
+    queue = celery.get_run_after_chain_queue(workspace, publication_type, layer)
     assert len(queue) == 1, queue
     assert queue == ['layman.util::patch_after_feature_change', ]
 
-    process_client.wait_for_publication_status(workspace, process_client.LAYER_TYPE, layer)
+    process_client.wait_for_publication_status(workspace, publication_type, layer)
 
-    expected_bbox = (1571000.0, 6268800.0, 1572590.8542062, 6269876.33561699)
-    assert_util.assert_all_sources_bbox(workspace, layer, expected_bbox)
+    process_client.delete_workspace_publication(publication_type, workspace, layer, )
 
-    process_client.delete_workspace_layer(workspace, layer, )
-
-    queue = celery.get_run_after_chain_queue(workspace, process_client.LAYER_TYPE, layer)
+    queue = celery.get_run_after_chain_queue(workspace, publication_type, layer)
     assert not queue, queue
