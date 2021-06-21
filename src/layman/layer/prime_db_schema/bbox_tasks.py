7da8189fd75ec82eaed8449c7166070bd41e8f74
index-git <sileny.index@gmail.com>
2021-06-21 15:02:31 +0200

    Do not calculate bbox for raster layers

--- src/layman/layer/prime_db_schema/bbox_tasks.py
+++ src/layman/layer/prime_db_schema/bbox_tasks.py
@@ -1,7 +1,7 @@
 from celery.utils.log import get_task_logger
 
 from layman.celery import AbortedException
-from layman import celery_app
+from layman import celery_app, util as layman_util, settings
 from .. import LAYER_TYPE
 from ..db import get_bbox as db_get_bbox
 from ...common.prime_db_schema.publications import set_bbox
@@ -22,6 +22,8 @@ def patch_after_feature_change(
     if self.is_aborted():
         raise AbortedException
 
+    file_type = layman_util.get_publication_info(username, LAYER_TYPE, layername, context={'keys': ['file']})['file']['file_type']
+    assert file_type == settings.FILE_TYPE_VECTOR
     bbox = db_get_bbox(username, layername)
 
     if self.is_aborted():
