2eec0a0867b74d4aec631c8ea575596118bac5b3
index-git <sileny.index@gmail.com>
2021-06-21 15:03:12 +0200

    Do not store raster layers with WMS source

--- src/layman/layer/geoserver/tasks.py
+++ src/layman/layer/geoserver/tasks.py
@@ -2,10 +2,10 @@ from celery.utils.log import get_task_logger
 
 from geoserver import util as gs_util
 from layman.celery import AbortedException
-from layman import celery_app, settings
+from layman import celery_app, settings, util as layman_util
 from layman.common import empty_method_returns_true
 from . import wms, wfs, sld
-from .. import geoserver
+from .. import geoserver, LAYER_TYPE
 
 logger = get_task_logger(__name__)
 
@@ -30,35 +30,37 @@ def refresh_wms(
         ensure_user=False,
         access_rights=None,
 ):
-    if description is None:
-        description = layername
-    if title is None:
-        title = layername
-    geoserver_workspace = wms.get_geoserver_workspace(username)
-    if ensure_user:
-        geoserver.ensure_workspace(username)
+    file_type = layman_util.get_publication_info(username, LAYER_TYPE, layername, context={'keys': ['file']})['file']['file_type']
+    if file_type == settings.FILE_TYPE_VECTOR:
+        if description is None:
+            description = layername
+        if title is None:
+            title = layername
+        geoserver_workspace = wms.get_geoserver_workspace(username)
+        if ensure_user:
+            geoserver.ensure_workspace(username)
 
-    if self.is_aborted():
-        raise AbortedException
-    if store_in_geoserver:
-        gs_util.delete_wms_layer(geoserver_workspace, layername, settings.LAYMAN_GS_AUTH)
-        gs_util.delete_wms_store(geoserver_workspace, settings.LAYMAN_GS_AUTH, wms.get_qgis_store_name(layername))
-        geoserver.publish_layer_from_db(username,
-                                        layername,
-                                        description,
-                                        title,
-                                        access_rights,
-                                        geoserver_workspace=geoserver_workspace,
-                                        )
-    else:
-        gs_util.delete_feature_type(geoserver_workspace, layername, settings.LAYMAN_GS_AUTH)
-        geoserver.publish_layer_from_qgis(username,
-                                          layername,
-                                          description,
-                                          title,
-                                          access_rights,
-                                          geoserver_workspace=geoserver_workspace,
-                                          )
+        if self.is_aborted():
+            raise AbortedException
+        if store_in_geoserver:
+            gs_util.delete_wms_layer(geoserver_workspace, layername, settings.LAYMAN_GS_AUTH)
+            gs_util.delete_wms_store(geoserver_workspace, settings.LAYMAN_GS_AUTH, wms.get_qgis_store_name(layername))
+            geoserver.publish_layer_from_db(username,
+                                            layername,
+                                            description,
+                                            title,
+                                            access_rights,
+                                            geoserver_workspace=geoserver_workspace,
+                                            )
+        else:
+            gs_util.delete_feature_type(geoserver_workspace, layername, settings.LAYMAN_GS_AUTH)
+            geoserver.publish_layer_from_qgis(username,
+                                              layername,
+                                              description,
+                                              title,
+                                              access_rights,
+                                              geoserver_workspace=geoserver_workspace,
+                                              )
     wms.clear_cache(username)
 
     if self.is_aborted():
--- src/layman/layer/geoserver/wms_tasks.py
+++ src/layman/layer/geoserver/wms_tasks.py
@@ -20,13 +20,15 @@ def patch_after_feature_change(
     if self.is_aborted():
         raise AbortedException
 
-    bbox = geoserver.get_layer_bbox(workspace, layer)
-    geoserver_workspace = wms.get_geoserver_workspace(workspace)
-    style_type = layman_util.get_publication_info(workspace, LAYER_TYPE, layer, context={'keys': ['style_type'], })['style_type']
-    if style_type == 'sld':
-        gs_util.patch_feature_type(geoserver_workspace, layer, auth=settings.LAYMAN_GS_AUTH, bbox=bbox)
-    elif style_type == 'qml':
-        gs_util.patch_wms_layer(geoserver_workspace, layer, auth=settings.LAYMAN_GS_AUTH, bbox=bbox)
+    file_type = layman_util.get_publication_info(workspace, LAYER_TYPE, layer, context={'keys': ['file']})['file']['file_type']
+    if file_type == settings.FILE_TYPE_VECTOR:
+        bbox = geoserver.get_layer_bbox(workspace, layer)
+        geoserver_workspace = wms.get_geoserver_workspace(workspace)
+        style_type = layman_util.get_publication_info(workspace, LAYER_TYPE, layer, context={'keys': ['style_type'], })['style_type']
+        if style_type == 'sld':
+            gs_util.patch_feature_type(geoserver_workspace, layer, auth=settings.LAYMAN_GS_AUTH, bbox=bbox)
+        elif style_type == 'qml':
+            gs_util.patch_wms_layer(geoserver_workspace, layer, auth=settings.LAYMAN_GS_AUTH, bbox=bbox)
 
     wms.clear_cache(workspace)
 
