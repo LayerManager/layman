f5f2406aada31f73bf1da93e305ec811487ff6fb
index-git <sileny.index@gmail.com>
2021-06-22 10:09:34 +0200

    Set timeout to 60 for test on GitHub CI

--- test.sh
+++ test.sh
@@ -9,7 +9,7 @@ rm -rf tmp/artifacts/*
 
 if [ "$CI" == "true" ]
 then
-  python3 src/assert_db.py && python3 src/wait_for_deps.py && python3 src/clear_layman_data.py && python3 -m pytest -m "not irritating" -W ignore::DeprecationWarning -xvv
+  python3 src/assert_db.py && python3 src/wait_for_deps.py && python3 src/clear_layman_data.py && python3 -m pytest -m "not irritating" --timeout=60 -W ignore::DeprecationWarning -xvv
 else
   python3 src/assert_db.py && python3 src/wait_for_deps.py && python3 src/clear_layman_data.py && python3 -m pytest -W ignore::DeprecationWarning -xvv
 fi
