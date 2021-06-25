459b5cb8ec23dc89ba33feb8b014a3db252d9edd
Jiri Kozel <jirik@users.noreply.github.com>
2021-06-25 21:58:06 +0200

    Normalize more raster types

--- src/layman/layer/filesystem/gdal_test.py
+++ src/layman/layer/filesystem/gdal_test.py
@@ -11,11 +11,11 @@ from . import gdal
     ('sample/layman.layer/sample_tif_rgb.tif', None),
     ('sample/layman.layer/sample_tif_rgb_nodata.tif', None),
     ('sample/layman.layer/sample_tif_rgba.tif', None),
-    ('sample/layman.layer/sample_tiff_rgba.tiff', None),
-    ('sample/layman.layer/sample_tif_tfw_rgba.tif', None),
-    ('sample/layman.layer/sample_tif_colortable_nodata.tif', None),
+    ('sample/layman.layer/sample_tiff_rgba_opaque.tiff', None),
+    ('sample/layman.layer/sample_tif_tfw_rgba_opaque.tif', None),
+    ('sample/layman.layer/sample_tif_colortable_nodata_opaque.tif', None),
     ('sample/layman.layer/sample_tif_grayscale_alpha_nodata.tif', None),
-    ('sample/layman.layer/sample_tif_grayscale_nodata.tif', None),
+    ('sample/layman.layer/sample_tif_grayscale_nodata_opaque.tif', None),
     ('sample/layman.layer/sample_tif_rg.tif', {'http_code': 400,
                                                'code': 2,
                                                'detail': {'parameter': 'file',
@@ -41,11 +41,11 @@ def test_assert_valid_raster(file_path, exp_error):
     ('sample/layman.layer/sample_tif_rgb.tif', [None, None, None]),
     ('sample/layman.layer/sample_tif_rgb_nodata.tif', [0, 0, 0]),
     ('sample/layman.layer/sample_tif_rgba.tif', [None, None, None, None]),
-    ('sample/layman.layer/sample_tiff_rgba.tiff', [None, None, None, None]),
-    ('sample/layman.layer/sample_tif_tfw_rgba.tif', [None, None, None, None]),
-    ('sample/layman.layer/sample_tif_colortable_nodata.tif', [255]),
+    ('sample/layman.layer/sample_tiff_rgba_opaque.tiff', [None, None, None, None]),
+    ('sample/layman.layer/sample_tif_tfw_rgba_opaque.tif', [None, None, None, None]),
+    ('sample/layman.layer/sample_tif_colortable_nodata_opaque.tif', [255]),
     ('sample/layman.layer/sample_tif_grayscale_alpha_nodata.tif', [3.402823466e+38, 3.402823466e+38]),
-    ('sample/layman.layer/sample_tif_grayscale_nodata.tif', [3.402823466e+38]),
+    ('sample/layman.layer/sample_tif_grayscale_nodata_opaque.tif', [3.402823466e+38]),
     ('sample/layman.layer/sample_tif_rg.tif', [None, None]),
 ])
 def test_get_nodata_values(file_path, exp_result):
@@ -57,11 +57,11 @@ def test_get_nodata_values(file_path, exp_result):
     ('sample/layman.layer/sample_tif_rgb.tif', [0.04373913043462064, -0.04374999999923662]),
     ('sample/layman.layer/sample_tif_rgb_nodata.tif', [0.04373913043462064, -0.04374999999923662]),
     ('sample/layman.layer/sample_tif_rgba.tif', [0.04373913043462064, -0.04374999999923662]),
-    ('sample/layman.layer/sample_tiff_rgba.tiff', [14.716791979949875, -14.733496332518337]),
-    ('sample/layman.layer/sample_tif_tfw_rgba.tif', [14.716791979949875, -14.733496332518337]),
-    ('sample/layman.layer/sample_tif_colortable_nodata.tif', [92.930501930501933, -92.976470588235287]),
+    ('sample/layman.layer/sample_tiff_rgba_opaque.tiff', [14.716791979949875, -14.733496332518337]),
+    ('sample/layman.layer/sample_tif_tfw_rgba_opaque.tif', [14.716791979949875, -14.733496332518337]),
+    ('sample/layman.layer/sample_tif_colortable_nodata_opaque.tif', [92.930501930501933, -92.976470588235287]),
     ('sample/layman.layer/sample_tif_grayscale_alpha_nodata.tif', [15.293650793650794, -15.308571428571428]),
-    ('sample/layman.layer/sample_tif_grayscale_nodata.tif', [15.293650793650794, -15.308571428571428]),
+    ('sample/layman.layer/sample_tif_grayscale_nodata_opaque.tif', [15.293650793650794, -15.308571428571428]),
     ('sample/layman.layer/sample_tif_rg.tif', [0.04373913043462064, -0.04374999999923662]),
 ])
 def test_get_pixel_size(file_path, exp_result):
@@ -73,11 +73,11 @@ def test_get_pixel_size(file_path, exp_result):
     ('sample/layman.layer/sample_tif_rgb.tif', [[0, 251], [0, 253], [0, 254]]),
     ('sample/layman.layer/sample_tif_rgb_nodata.tif', [[33, 251], [57, 253], [1, 254]]),
     ('sample/layman.layer/sample_tif_rgba.tif', [[0, 251], [0, 253], [0, 254], [0, 127]]),
-    ('sample/layman.layer/sample_tiff_rgba.tiff', [[0, 254], [0, 222], [0, 216], [255, 255]]),
-    ('sample/layman.layer/sample_tif_tfw_rgba.tif', [[0, 254], [0, 222], [0, 216], [255, 255]]),
-    ('sample/layman.layer/sample_tif_colortable_nodata.tif', [[0, 20]]),
+    ('sample/layman.layer/sample_tiff_rgba_opaque.tiff', [[0, 254], [0, 222], [0, 216], [255, 255]]),
+    ('sample/layman.layer/sample_tif_tfw_rgba_opaque.tif', [[0, 254], [0, 222], [0, 216], [255, 255]]),
+    ('sample/layman.layer/sample_tif_colortable_nodata_opaque.tif', [[0, 20]]),
     ('sample/layman.layer/sample_tif_grayscale_alpha_nodata.tif', [[-0.0094339624047279, 0.91247737407684], [0, 255]]),
-    ('sample/layman.layer/sample_tif_grayscale_nodata.tif', [[-0.040201004594564, 0.91754120588303]]),
+    ('sample/layman.layer/sample_tif_grayscale_nodata_opaque.tif', [[-0.040201004594564, 0.91754120588303]]),
     ('sample/layman.layer/sample_tif_rg.tif', [[0, 251], [0, 253]]),
 ])
 def test_get_statistics(file_path, exp_result):
@@ -92,11 +92,11 @@ def test_get_statistics(file_path, exp_result):
     ('sample/layman.layer/sample_tif_rgb.tif', False),  # no alpha, no nodata
     ('sample/layman.layer/sample_tif_rgb_nodata.tif', True),  # no alpha, min <= nodata <= max
     ('sample/layman.layer/sample_tif_rgba.tif', True),  # alpha with at least one value < 255
-    ('sample/layman.layer/sample_tiff_rgba.tiff', False),  # alpha with all values = 255, no nodata
-    ('sample/layman.layer/sample_tif_tfw_rgba.tif', False),  # alpha with all values = 255, no nodata
-    ('sample/layman.layer/sample_tif_colortable_nodata.tif', False),  # no alpha, nodata > max in each band
+    ('sample/layman.layer/sample_tiff_rgba_opaque.tiff', False),  # alpha with all values = 255, no nodata
+    ('sample/layman.layer/sample_tif_tfw_rgba_opaque.tif', False),  # alpha with all values = 255, no nodata
+    ('sample/layman.layer/sample_tif_colortable_nodata_opaque.tif', False),  # no alpha, nodata > max in each band
     ('sample/layman.layer/sample_tif_grayscale_alpha_nodata.tif', True),  # alpha with at least one value < 255
-    ('sample/layman.layer/sample_tif_grayscale_nodata.tif', False),  # no alpha, nodata > max in each band
+    ('sample/layman.layer/sample_tif_grayscale_nodata_opaque.tif', False),  # no alpha, nodata > max in each band
     ('sample/layman.layer/sample_tif_rg.tif', False),  # no alpha, no nodata
 ])
 def test_is_normalized_alpha_needed(file_path, exp_result):
@@ -110,11 +110,11 @@ def test_is_normalized_alpha_needed(file_path, exp_result):
     ('sample/layman.layer/sample_tif_rgb.tif', False),
     ('sample/layman.layer/sample_tif_rgb_nodata.tif', False),
     ('sample/layman.layer/sample_tif_rgba.tif', False),
-    ('sample/layman.layer/sample_tiff_rgba.tiff', False),
-    ('sample/layman.layer/sample_tif_tfw_rgba.tif', False),
-    ('sample/layman.layer/sample_tif_colortable_nodata.tif', True),
+    ('sample/layman.layer/sample_tiff_rgba_opaque.tiff', False),
+    ('sample/layman.layer/sample_tif_tfw_rgba_opaque.tif', False),
+    ('sample/layman.layer/sample_tif_colortable_nodata_opaque.tif', True),
     ('sample/layman.layer/sample_tif_grayscale_alpha_nodata.tif', True),
-    ('sample/layman.layer/sample_tif_grayscale_nodata.tif', True),
+    ('sample/layman.layer/sample_tif_grayscale_nodata_opaque.tif', True),
     ('sample/layman.layer/sample_tif_rg.tif', False),
 ])
 def test_is_nodata_out_of_min_max(file_path, exp_result):
@@ -127,11 +127,11 @@ def test_is_nodata_out_of_min_max(file_path, exp_result):
     ('sample/layman.layer/sample_tif_rgb.tif', None),
     ('sample/layman.layer/sample_tif_rgb_nodata.tif', None),
     ('sample/layman.layer/sample_tif_rgba.tif', None),
-    ('sample/layman.layer/sample_tiff_rgba.tiff', 'sample/layman.layer/sample_tiff_rgba.vrt'),
-    ('sample/layman.layer/sample_tif_tfw_rgba.tif', 'sample/layman.layer/sample_tif_tfw_rgba.vrt'),
-    ('sample/layman.layer/sample_tif_colortable_nodata.tif', None),
+    ('sample/layman.layer/sample_tiff_rgba_opaque.tiff', 'sample/layman.layer/sample_tiff_rgba_opaque.vrt'),
+    ('sample/layman.layer/sample_tif_tfw_rgba_opaque.tif', 'sample/layman.layer/sample_tif_tfw_rgba_opaque.vrt'),
+    ('sample/layman.layer/sample_tif_colortable_nodata_opaque.tif', None),
     ('sample/layman.layer/sample_tif_grayscale_alpha_nodata.tif', None),
-    ('sample/layman.layer/sample_tif_grayscale_nodata.tif', None),
+    ('sample/layman.layer/sample_tif_grayscale_nodata_opaque.tif', None),
 ])
 def test_create_vrt_file_if_needed(file_path, exp_result):
     if exp_result:
