36fd2cc12841a6cb8479f1862b988194f3740d5b
index-git <sileny.index@gmail.com>
2021-07-12 15:21:50 +0200

    Rewrite layer/thumbnail_test::test_sld_style_applied_in_thumbnail to separated tests

--- src/layman/layer/filesystem/thumbnail_test.py
+++ src/layman/layer/filesystem/thumbnail_test.py
@@ -1,37 +1,10 @@
-from test_tools import process_client, util
+from test_tools import process_client
 import pytest
-from layman import app
-from . import thumbnail
-
 
 headers_sld = {
     'Accept': 'application/vnd.ogc.sld+xml',
     'Content-type': 'application/xml',
 }
-
-
-@pytest.mark.usefixtures('ensure_layman')
-def test_sld_style_applied_in_thumbnail():
-    workspace = 'test_sld_style_applied_in_thumbnail_workspace'
-    layer = 'test_sld_style_applied_in_thumbnail_layer'
-    geojson_file = ['/code/tmp/naturalearth/110m/cultural/ne_110m_admin_0_countries.geojson']
-    style_file = 'sample/style/generic-blue_sld.xml'
-    expected_file = 'sample/style/test_sld_style_applied_in_thumbnail_layer.png'
-
-    process_client.publish_workspace_layer(workspace,
-                                           layer,
-                                           file_paths=geojson_file,
-                                           style_file=style_file)
-
-    with app.app_context():
-        thumbnail_path = thumbnail.get_layer_thumbnail_path(workspace, layer)
-
-    diffs = util.compare_images(expected_file, thumbnail_path)
-
-    assert diffs < 1000
-
-    process_client.delete_workspace_layer(workspace,
-                                          layer)
 
 
 @pytest.mark.usefixtures('ensure_layman')
