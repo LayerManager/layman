8f3401dea4149b7128c889cf1ada15a6c778ab4a
index-git <sileny.index@gmail.com>
2021-07-13 09:37:07 +0200

    Assert before publication creation

--- src/test/single_static_publication/data.py
+++ src/test/single_static_publication/data.py
@@ -1,4 +1,6 @@
-from layman import util, app
+import os
+
+from layman import util, app, settings
 from test_tools import process_client
 from .. import single_static_publication as data
 
@@ -7,6 +9,11 @@ def ensure_publication(workspace, publ_type, publication):
     with app.app_context():
         info = util.get_publication_info(workspace, publ_type, publication, context={'keys': ['name']})
     if not info.get('name'):
+        workspace_directory = f'{settings.LAYMAN_QGIS_DATA_DIR}/workspaces/{workspace}'
+        layer_directory = f'{workspace_directory}/layers/{publication}'
+        assert not os.path.exists(layer_directory)
+
         for idx, params in enumerate(data.PUBLICATIONS[(workspace, publ_type, publication)][data.DEFINITION]):
+
             write_method = process_client.patch_workspace_publication if idx > 0 else process_client.publish_workspace_publication
             write_method(publ_type, workspace, publication, **params)
