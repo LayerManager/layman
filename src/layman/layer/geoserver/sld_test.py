39613012cada3aa360d2a2211b6d795b0ddcf26c
index-git <sileny.index@gmail.com>
2021-07-14 14:47:39 +0200

    Rewrite layer/test_sld_style_in_wms_workspace to separated tests

--- src/layman/layer/geoserver/sld_test.py
+++ src/layman/layer/geoserver/sld_test.py
@@ -1,8 +1,5 @@
-from urllib.parse import urljoin
-import requests
 import pytest
 
-from geoserver import GS_REST, GS_AUTH
 from layman import settings
 from test_tools import process_client, assert_util
 
@@ -11,30 +8,6 @@ headers_sld = {
     'Accept': 'application/vnd.ogc.sld+xml',
     'Content-type': 'application/xml',
 }
-
-
-@pytest.mark.usefixtures('ensure_layman')
-def test_sld_style_in_wms_workspace():
-    workspace = 'test_sld_style_file_workspace'
-    layer = 'test_sld_style_file_layer'
-    geojson_file = ['/code/tmp/naturalearth/110m/cultural/ne_110m_admin_0_countries.geojson']
-    style_file = 'sample/style/generic-blue_sld.xml'
-
-    process_client.publish_workspace_layer(workspace,
-                                           layer,
-                                           file_paths=geojson_file,
-                                           style_file=style_file)
-
-    url = urljoin(GS_REST, f'workspaces/{workspace}_wms/styles/{layer}')
-
-    response = requests.get(url,
-                            auth=GS_AUTH,
-                            headers=headers_sld,
-                            timeout=5,
-                            )
-    response.raise_for_status()
-    process_client.delete_workspace_layer(workspace,
-                                          layer)
 
 
 @pytest.mark.usefixtures('ensure_layman')
