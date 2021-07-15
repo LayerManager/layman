5f8dd8cd7914cfa84b17df7127263246fc69ed76
index-git <sileny.index@gmail.com>
2021-07-15 11:10:31 +0200

    Rewrite layer/test_style_value to separated tests

--- src/test/single_static_publication/test/layers_test.py
+++ src/test/single_static_publication/test/layers_test.py
@@ -29,9 +29,14 @@ def test_info(workspace, publ_type, publication):
     ensure_publication(workspace, publ_type, publication)
     wms_url = f"http://localhost:8000/geoserver/{workspace}{settings.LAYMAN_GS_WMS_WORKSPACE_POSTFIX}/ows"
     wfs_url = f"http://localhost:8000/geoserver/{workspace}/wfs"
-
     headers = data.PUBLICATIONS[(workspace, publ_type, publication)][data.TEST_DATA].get('headers')
+    style = data.PUBLICATIONS[(workspace, publ_type, publication)][data.TEST_DATA]['style_type']
+
     info = process_client.get_workspace_publication(publ_type, workspace, publication, headers=headers)
+    with app.app_context():
+        info_internal = layer_util.get_layer_info(workspace, publication)
+        expected_style_url = url_for('rest_workspace_layer_style.get', workspace=workspace, layername=publication,
+                                     internal=False)
 
     assert info['wms'].get('url') == wms_url, f'r_json={info}, wms_url={wms_url}'
     assert 'wms' in info, f'info={info}'
@@ -46,6 +51,11 @@ def test_info(workspace, publ_type, publication):
         assert info.get('file', dict()).get('file_type') == 'raster', info
         assert 'wfs' not in info, f'info={info}'
         assert 'db_table' not in info, f'info={info}'
+
+    assert (info_internal['style_type'] == 'qml') == (style == 'qml'), f'info={info}'
+    assert info['style']['type'] == style if style else 'sld', info.get('style')
+    external_style_url = info['style']['url']
+    assert external_style_url == expected_style_url, (info, external_style_url)
 
 
 @pytest.mark.parametrize('workspace, publ_type, publication', data.LIST_LAYERS)
@@ -83,6 +93,7 @@ def test_get_layer_style(workspace, publ_type, publication):
         rest_url = url_for('rest_workspace_layer_style.get', workspace=workspace, layername=publication)
     response = requests.get(rest_url, headers=headers)
     assert response.status_code == 200, response.text
+
     # lxml does not support importing from utf8 string
     xml_tree = ET.fromstring(bytes(response.text, encoding='utf8'))
 
@@ -94,6 +105,9 @@ def test_get_layer_style(workspace, publ_type, publication):
         assert ET.QName(xml_tree) == "qgis", response.text
         assert len(xml_tree.xpath('/qgis/renderer-v2')) == 1, response.text
         assert xml_tree.attrib, response.text
+
+    r_del = requests.delete(rest_url)
+    assert r_del.status_code >= 400, (r_del.text, rest_url)
 
 
 @pytest.mark.parametrize('workspace, publ_type, publication', data.LIST_LAYERS)
@@ -108,16 +122,10 @@ def test_wms_layer(workspace, publ_type, publication):
     wms_stores_url = urljoin(GS_REST_WORKSPACES, f'{workspace}_wms/wmsstores/')
     wms_layers_url = urljoin(GS_REST_WORKSPACES, f'{workspace}_wms/wmslayers/')
 
-    with app.app_context():
-        info = layer_util.get_layer_info(workspace, publication, context={'keys': ['style_type', 'style'], })
-    assert (info['style_type'] == 'qml') == (style == 'qml'), info.get('style_type', None)
-
     if style_file:
         assert (os.path.exists(expected_style_file + '.qml')) == (style_file == 'qml')
         assert (os.path.exists(expected_style_file + '.sld')) == (style_file == 'sld')
     assert (os.path.exists(expected_qgis_file)) == (style == 'qml')
-    assert info['style']['type'] == style if style else 'sld', info.get('style')
-    assert info['style']['url'], info.get('style')
 
     response = requests.get(wms_stores_url,
                             auth=settings.LAYMAN_GS_AUTH,
