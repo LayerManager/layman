a02016e4ee67f8b8f266988e88f3c19b58a8db9e
index-git <66255344+index-git@users.noreply.github.com>
2021-07-20 13:26:12 +0200

    Simplify test

--- src/test/static_publications/single_publication/layers_test.py
+++ src/test/static_publications/single_publication/layers_test.py
@@ -52,7 +52,7 @@ def test_info(workspace, publ_type, publication):
         assert 'wfs' not in info, f'info={info}'
         assert 'db_table' not in info, f'info={info}'
 
-    assert (info_internal['style_type'] == 'qml') == (style == 'qml'), f'info={info}'
+    assert info_internal['style_type'] == style, f'info_internal={info_internal}'
     assert info['style']['type'] == style if style else 'sld', info.get('style')
     external_style_url = info['style']['url']
     assert external_style_url == expected_style_url, (info, external_style_url)
