6a698ae4ee1f345fbdee9ca50216e73b942f157e
index-git <66255344+index-git@users.noreply.github.com>
2021-07-21 12:12:22 +0200

    Assert directories for all created publications

--- src/test/static_publications/data.py
+++ src/test/static_publications/data.py
@@ -12,14 +12,19 @@ def ensure_publication(workspace, publ_type, publication):
     with app.app_context():
         info = util.get_publication_info(workspace, publ_type, publication, context={'keys': ['name']})
     if not info.get('name'):
+        workspace_directory = f'{settings.LAYMAN_QGIS_DATA_DIR}/workspaces/{workspace}'
         if publ_type == process_client.MAP_TYPE:
+            map_directory = f'{workspace_directory}/maps/{publication}'
+            assert not os.path.exists(map_directory)
+
             layers = data.PUBLICATIONS[(workspace, publ_type, publication)][data.TEST_DATA].get('layers') or list()
             for layer_workspace, layer_type, layer in layers:
                 ensure_publication(layer_workspace, layer_type, layer)
-
-        workspace_directory = f'{settings.LAYMAN_QGIS_DATA_DIR}/workspaces/{workspace}'
-        layer_directory = f'{workspace_directory}/layers/{publication}'
-        assert not os.path.exists(layer_directory)
+        elif publ_type == process_client.LAYER_TYPE:
+            layer_directory = f'{workspace_directory}/layers/{publication}'
+            assert not os.path.exists(layer_directory)
+        else:
+            assert False, f'Unknown publication type {publ_type}'
 
         for idx, params in enumerate(data.PUBLICATIONS[(workspace, publ_type, publication)][data.DEFINITION]):
             write_method = process_client.patch_workspace_publication if idx > 0 else process_client.publish_workspace_publication
