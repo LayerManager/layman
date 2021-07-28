0b2e1755f5b319f855361fd0007938dadbc41c21
Jiri Kozel <jirik@users.noreply.github.com>
2021-07-28 17:35:37 +0200

    Rename src/test to tests

--- src/test/static_publications/conftest.py
+++
@@ -1,3 +0,0 @@
-from . import data
-
-ensure_test_data = data.ensure_test_data
---
+++ tests/static_publications/conftest.py
@@ -0,0 +1,6 @@
+from test_tools import process
+from . import data
+
+ensure_test_data = data.ensure_test_data
+liferay_mock = process.liferay_mock
+ensure_layman = process.ensure_layman
