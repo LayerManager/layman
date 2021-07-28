8c66af588d146bcd444706a3eba465dfb3dc7f16
Jiri Kozel <jirik@users.noreply.github.com>
2021-07-28 11:30:50 +0200

    Focus on failing test

--- test.sh
+++ test.sh
@@ -9,9 +9,9 @@ rm -rf tmp/artifacts/*
 
 if [ "$CI" == "true" ]
 then
-  python3 src/assert_db.py && python3 src/wait_for_deps.py && python3 src/clear_layman_data.py && python3 -m pytest -m "not irritating" --timeout=60 -W ignore::DeprecationWarning -xvv
+  python3 src/assert_db.py && python3 src/wait_for_deps.py && python3 src/clear_layman_data.py && python3 -m pytest -m "not irritating" --timeout=60 -W ignore::DeprecationWarning -xvv src/layman/map/rest_workspace_test.py::test_map_composed_from_local_layers
 else
-  python3 src/assert_db.py && python3 src/wait_for_deps.py && python3 src/clear_layman_data.py && python3 -m pytest -W ignore::DeprecationWarning -xvv
+  python3 src/assert_db.py && python3 src/wait_for_deps.py && python3 src/clear_layman_data.py && python3 -m pytest -W ignore::DeprecationWarning -xvv --capture=tee-sys src/layman/map/rest_workspace_test.py::test_map_composed_from_local_layers
 fi
 #python3 src/assert_db.py && python3 src/wait_for_deps.py && python3 src/clear_layman_data.py && python3 -m pytest -W ignore::DeprecationWarning --capture=tee-sys -xvv src/layman/gs_wfs_proxy_test.py
 #python3 src/assert_db.py && python3 src/wait_for_deps.py && python3 src/clear_layman_data.py && python3 -m pytest -W ignore::DeprecationWarning -xsvv src/layman/layer/client_test.py
