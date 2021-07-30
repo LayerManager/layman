0e7fb7a72e2bc8653f57f7267f93373bfc432287
Jiri Kozel <jirik@users.noreply.github.com>
2021-07-30 17:58:21 +0200

    Test raster overviews

--- tests/static_publications/single_publication/layers_files_test.py
+++ tests/static_publications/single_publication/layers_files_test.py
@@ -1,3 +1,4 @@
+import math
 import os
 import pytest
 from layman import app, settings
@@ -15,13 +16,24 @@ def test_raster_files(workspace, publ_type, publication):
     ensure_publication(workspace, publ_type, publication)
     with app.app_context():
         directory_path = input_file.get_layer_input_file_dir(workspace, publication)
-    for ext in data.PUBLICATIONS[(workspace, publ_type, publication)][data.TEST_DATA]['file_extensions']:
+    publ_test_data = data.PUBLICATIONS[(workspace, publ_type, publication)][data.TEST_DATA]
+    for ext in publ_test_data['file_extensions']:
         file_path = os.path.join(directory_path, publication + ext)
         assert os.path.exists(file_path), file_path
     norm_file_path = gdal.get_normalized_raster_layer_main_filepath(workspace, publication)
     assert os.path.exists(norm_file_path), norm_file_path
-    normalized_color_interp = data.PUBLICATIONS[(workspace, publ_type, publication)][data.TEST_DATA]['normalized_color_interp']
+    normalized_color_interp = publ_test_data['normalized_color_interp']
     assert normalized_color_interp == gdal.get_color_interpretations(norm_file_path)
+
+    # check number of overviews
+    raster_size = max(gdal.get_raster_size(norm_file_path))
+    overview_counts = gdal.get_overview_counts(norm_file_path)
+    # https://gdal.org/programs/gdaladdo.html#cmdoption-gdaladdo-minsize
+    exp_overview_count = max(math.ceil(math.log(raster_size / 256, 2)), 0)
+    assert overview_counts == [exp_overview_count] * len(overview_counts)
+    exp_def_overview_count = publ_test_data.get('normalized_overviews')
+    if exp_def_overview_count is not None:
+        assert overview_counts == [exp_def_overview_count] * len(overview_counts)
 
 
 @pytest.mark.parametrize('workspace, publ_type, publication', data.LIST_QML_LAYERS)
