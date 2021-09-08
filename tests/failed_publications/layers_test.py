fadd83747b3323e158eb67515914f3c2aa5c8034
index-git <sileny.index@gmail.com>
2021-09-08 12:22:52 +0200

    Parametrize failed_publications_test

--- tests/failed_publications/layers_test.py
+++ tests/failed_publications/layers_test.py
@@ -6,33 +6,37 @@ from test_tools import process_client, util as test_util
 COMMON_WORKSPACE = 'failed_publications_workspace'
 
 
+@pytest.mark.parametrize('params, expected_exc, error_async_part', [
+    (
+            {'file_paths': ['sample/layman.layer/sample_png_pgw_rgba.pgw',
+                            'sample/layman.layer/sample_png_pgw_rgba.png', ]},
+            {'http_code': 400,
+             'code': 4,
+             'message': 'Unsupported CRS of data file',
+             'detail': {'found': 'None', 'supported_values': settings.INPUT_SRS_LIST},
+             },
+            'file',
+    ),
+])
 @pytest.mark.usefixtures('ensure_layman')
-def test_png_without_crs():
+def test_publication_error(params, expected_exc, error_async_part):
     publ_type = process_client.LAYER_TYPE
     workspace = COMMON_WORKSPACE
     layer = 'test_png_without_crs_layer'
 
-    expected_exc = {'http_code': 400,
-                    'code': 4,
-                    'message': 'Unsupported CRS of data file',
-                    'detail': {'found': 'None', 'supported_values': settings.INPUT_SRS_LIST},
-                    }
-
     # Synchronous POST
     with pytest.raises(LaymanError) as exc_info:
         process_client.publish_workspace_publication(publ_type, workspace, layer,
-                                                     file_paths=['sample/layman.layer/sample_png_pgw_rgba.pgw',
-                                                                 'sample/layman.layer/sample_png_pgw_rgba.png', ], )
+                                                     **params, )
     test_util.assert_error(expected_exc, exc_info)
 
     # Asynchronous POST
     process_client.publish_workspace_publication(publ_type, workspace, layer,
-                                                 file_paths=['sample/layman.layer/sample_png_pgw_rgba.pgw',
-                                                             'sample/layman.layer/sample_png_pgw_rgba.png', ],
+                                                 **params,
                                                  with_chunks=True,
                                                  )
     info = process_client.get_workspace_publication(publ_type, workspace, layer, )
-    test_util.assert_async_error(expected_exc, info['file']['error'])
+    test_util.assert_async_error(expected_exc, info[error_async_part]['error'])
 
     # Preparation for PATCH tests
     process_client.delete_workspace_layer(workspace, layer)
@@ -41,8 +45,7 @@ def test_png_without_crs():
     # Synchronous PATCH
     with pytest.raises(LaymanError) as exc_info:
         process_client.patch_workspace_publication(publ_type, workspace, layer,
-                                                   file_paths=['sample/layman.layer/sample_png_pgw_rgba.pgw',
-                                                               'sample/layman.layer/sample_png_pgw_rgba.png', ], )
+                                                   **params, )
     test_util.assert_error(expected_exc, exc_info)
 
     # Preparation for PATCH tests
@@ -51,11 +54,10 @@ def test_png_without_crs():
 
     # Asynchronous PATCH
     process_client.patch_workspace_publication(publ_type, workspace, layer,
-                                               file_paths=['sample/layman.layer/sample_png_pgw_rgba.pgw',
-                                                           'sample/layman.layer/sample_png_pgw_rgba.png', ],
+                                               **params,
                                                with_chunks=True,
                                                )
     info = process_client.get_workspace_publication(publ_type, workspace, layer, )
-    test_util.assert_async_error(expected_exc, info['file']['error'])
+    test_util.assert_async_error(expected_exc, info[error_async_part]['error'])
 
     process_client.delete_workspace_layer(workspace, layer)
