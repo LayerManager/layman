177dd6ca1e2c126f11f5972e821b10849c61420a
Jiri Kozel <jirik@users.noreply.github.com>
2021-09-14 09:04:28 +0200

    launder_attribute_name lowers only A-Z letters, not others

--- src/layman/common/db/__init__.py
+++ src/layman/common/db/__init__.py
@@ -3,4 +3,6 @@ import re
 
 def launder_attribute_name(attr_name):
     # https://github.com/OSGeo/gdal/blob/355b41831cd2685c85d1aabe5b95665a2c6e99b7/gdal/ogr/ogrsf_frmts/pgdump/ogrpgdumpdatasource.cpp#L129,L155
-    return re.sub(r"['\-#]", '_', attr_name.lower())
+    # lower case only 26 English letters, not other letters, in the same way as ogr2ogr
+    lower_name = re.sub(r'([A-Z]+)', lambda match: match.group(1).lower(), attr_name)
+    return re.sub(r"['\-#]", '_', lower_name)
