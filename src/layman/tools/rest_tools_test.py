90e5a5c0632bbb3e8d0f5e1133b54c918e678223
index-git <sileny.index@gmail.com>
2021-09-17 10:23:26 +0200

    Test get_style_info for error raising

--- src/layman/tools/rest_tools_test.py
+++ src/layman/tools/rest_tools_test.py
@@ -1,6 +1,7 @@
 import pytest
 
-from test_tools import process_client
+from layman import LaymanError
+from test_tools import process_client, util as test_util
 
 
 @pytest.mark.parametrize('style_file, expected_json', [
@@ -11,3 +12,27 @@ from test_tools import process_client
 def test_get_style_info(style_file, expected_json):
     style_info = process_client.get_style_info(style_file=style_file)
     assert style_info == expected_json
+
+
+@pytest.mark.parametrize('params, expected_exc', [
+    ({'style_file': 'sample/style/generic-blue_sld.xml',
+      },
+     {'http_code': 400,
+      'code': 2,
+      'message': 'Wrong parameter value',
+      'detail': {'parameter': 'style', 'supported_values': ['qml']},
+      },
+     ),
+    (dict(),
+     {'http_code': 400,
+      'code': 1,
+      'message': 'Missing parameter',
+      'detail': {'parameter': 'style', },
+      },
+     ),
+])
+@pytest.mark.usefixtures('ensure_layman')
+def test_get_style_info_error(params, expected_exc):
+    with pytest.raises(LaymanError) as exc_info:
+        process_client.get_style_info(**params)
+    test_util.assert_error(expected_exc, exc_info)
