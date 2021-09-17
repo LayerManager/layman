40d2cddb09ecf83423f2b160844a4e208c003754
index-git <sileny.index@gmail.com>
2021-09-17 10:48:04 +0200

    Return type without external_files for sld files in /tools/styl-info response

--- src/layman/tools/rest_tools.py
+++ src/layman/tools/rest_tools.py
@@ -8,7 +8,7 @@ bp = Blueprint('rest_tools', __name__)
 
 @bp.route('/style-info', methods=['GET'])
 def get_style_info():
-    supported_style_types = ['qml']
+    supported_style_types_for_external_files = ['qml']
 
     style_file = None
     if 'style' in request.files and not request.files['style'].filename == '':
@@ -17,10 +17,8 @@ def get_style_info():
         raise LaymanError(1, {'parameter': 'style'})
 
     detected_type = input_style.get_style_type_from_file_storage(style_file).code
-    if detected_type not in supported_style_types:
-        raise LaymanError(2, {'parameter': 'style', 'supported_values': supported_style_types})
-
-    external_files = input_style.get_external_files_from_qml_file(style_file)
-    result = {'type': detected_type,
-              'external_files': list(external_files)}
+    result = {'type': detected_type, }
+    if detected_type in supported_style_types_for_external_files:
+        external_files = input_style.get_external_files_from_qml_file(style_file)
+        result['external_files'] = list(external_files)
     return jsonify(result), 200
