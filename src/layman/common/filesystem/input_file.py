a9957cf6ccc88e5813ac84402526d837e03ddb66
index-git <sileny.index@gmail.com>
2021-09-22 14:38:37 +0200

    Add prefix to common.filesystem.input_file::save_files

--- src/layman/common/filesystem/input_file.py
+++ src/layman/common/filesystem/input_file.py
@@ -1,7 +1,11 @@
-def save_files(files, filepath_mapping):
+import os
+
+
+def save_files(files, filepath_mapping, *, prefix=None):
     for file in files:
         if filepath_mapping[file.filename] is None:
             continue
         # logger.info(
         #     f'Saving file {file.filename} as {filepath_mapping[file.filename]}')
-        file.save(filepath_mapping[file.filename])
+        file_path = os.path.join(prefix, filepath_mapping[file.filename]) if prefix else filepath_mapping[file.filename]
+        file.save(file_path)
