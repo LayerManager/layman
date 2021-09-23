7c1c350f25720912da715fb9d94ba52e151aaa11
index-git <sileny.index@gmail.com>
2021-09-23 12:08:42 +0200

    Add directory creation to common.filesystem.input_file::save_files

--- src/layman/common/filesystem/input_file.py
+++ src/layman/common/filesystem/input_file.py
@@ -1,7 +1,15 @@
-def save_files(files, filepath_mapping):
+import os
+import pathlib
+
+
+def save_files(files, filepath_mapping, *, prefix=None):
     for file in files:
         if filepath_mapping[file.filename] is None:
             continue
         # logger.info(
         #     f'Saving file {file.filename} as {filepath_mapping[file.filename]}')
-        file.save(filepath_mapping[file.filename])
+        filepath = filepath_mapping[file.filename]
+        full_file_path = os.path.join(prefix, filepath) if prefix else filepath
+        directory = os.path.dirname(full_file_path)
+        pathlib.Path(directory).mkdir(parents=True, exist_ok=True)
+        file.save(full_file_path)
