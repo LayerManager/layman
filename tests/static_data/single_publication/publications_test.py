c370b749a051d74ee1ad67490496e01b40d16926
index-git <sileny.index@gmail.com>
2021-10-01 14:02:24 +0200

    Add asserts/publication.py::infos and use it

--- tests/static_data/single_publication/publications_test.py
+++ tests/static_data/single_publication/publications_test.py
@@ -38,22 +38,10 @@ def test_get_publication_info_items(workspace, publ_type, publication):
 def test_infos(workspace, publ_type, publication):
     ensure_publication(workspace, publ_type, publication)
 
-    title = data.PUBLICATIONS[(workspace, publ_type, publication)][data.TEST_DATA].get('title') or publication
     headers = data.HEADERS.get(data.PUBLICATIONS[(workspace, publ_type, publication)][data.TEST_DATA].get('users_can_write', [None])[0])
-    infos = process_client.get_workspace_publications(publ_type, workspace, headers=headers)
-
-    publication_infos = [info for info in infos if info['name'] == publication]
-    assert len(publication_infos) == 1, f'publication_infos={publication_infos}'
-
-    info = next(iter(publication_infos))
-    assert info['title'] == title, f'publication_infos={publication_infos}'
-
-    get_workspace_publication_url = process_client.PUBLICATION_TYPES_DEF[publ_type].get_workspace_publication_url
-    param_name = process_client.PUBLICATION_TYPES_DEF[publ_type].url_param_name
-    with app.app_context():
-        expected_url = test_util.url_for(get_workspace_publication_url, workspace=workspace, **{param_name: publication},
-                                         internal=False)
-        assert info['url'] == expected_url, f'publication_infos={publication_infos}, expected_url={expected_url}'
+    asserts.same_title_in_source_and_rest_multi(workspace, publ_type, publication, headers)
+    asserts.is_in_rest_multi(workspace, publ_type, publication, headers)
+    asserts.correct_url_in_rest_multi(workspace, publ_type, publication, headers)
 
 
 @pytest.mark.parametrize('workspace, publ_type, publication', data.LIST_ALL_PUBLICATIONS)
