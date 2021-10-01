6bb3da1f104cb8b9cd78ec1a2fbd87fa08b90faa
index-git <sileny.index@gmail.com>
2021-10-01 14:52:30 +0200

    Add asserts/publication.py::geoserver_workspaces and use it

--- tests/asserts/final/publication.py
+++ tests/asserts/final/publication.py
@@ -1,3 +1,5 @@
+import requests
+
 from layman import app, util as layman_util, settings
 from test_tools import util as test_util, process_client
 
@@ -127,3 +129,31 @@ def is_complete_in_rest(rest_publication_detail):
 
 def mandatory_keys_in_rest(rest_publication_detail):
     assert {'name', 'title', 'access_rights', 'uuid', 'metadata', 'file'}.issubset(set(rest_publication_detail)), rest_publication_detail
+
+
+def workspace_wms_1_3_0_capabilities_available(workspace):
+    with app.app_context():
+        internal_wms_url = test_util.url_for('geoserver_proxy_bp.proxy', subpath=workspace + settings.LAYMAN_GS_WMS_WORKSPACE_POSTFIX + '/ows')
+
+    r_wms = requests.get(internal_wms_url, params={
+        'service': 'WMS',
+        'request': 'GetCapabilities',
+        'version': '1.3.0',
+    })
+    assert r_wms.status_code == 200
+
+
+def workspace_wfs_2_0_0_capabilities_available_if_vector(workspace, publ_type, name):
+    with app.app_context():
+        internal_wfs_url = test_util.url_for('geoserver_proxy_bp.proxy', subpath=workspace + '/wfs')
+
+    with app.app_context():
+        file_info = layman_util.get_publication_info(workspace, publ_type, name, {'keys': ['file']})['file']
+    file_type = file_info['file_type']
+    if file_type == settings.FILE_TYPE_VECTOR:
+        r_wfs = requests.get(internal_wfs_url, params={
+            'service': 'WFS',
+            'request': 'GetCapabilities',
+            'version': '2.0.0',
+        })
+        assert r_wfs.status_code == 200
