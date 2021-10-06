d2fdb5684fd875f75a5b93de1631d7ddcda6b94b
index-git <sileny.index@gmail.com>
2021-10-06 17:32:41 +0200

    Rewrite same_info into valid_post with calculating data

--- tests/asserts/processing/response.py
+++ tests/asserts/processing/response.py
@@ -1,5 +1,14 @@
+from layman import settings
 from .. import util as assert_util
 
 
-def same_infos(expected, response):
-    assert assert_util.same_infos(expected, response), f'expected={expected}, response={response}'
+def valid_post(workspace, publ_type, name, response, expected=None):
+    expected = expected or dict()
+    publ_type_dir = assert_util.get_directory_name_from_publ_type(publ_type)
+    exp_response = {
+        'name': name,
+        'url': f'http://{settings.LAYMAN_PROXY_SERVER_NAME}/rest/workspaces/{workspace}/{publ_type_dir}/{name}'
+    }
+    exp_response = assert_util.recursive_dict_update(exp_response, expected)
+    assert assert_util.same_value_for_keys(expected=exp_response,
+                                           tested=response), f'exp_response={exp_response}\nresponse={response}\nexpected={expected}'
