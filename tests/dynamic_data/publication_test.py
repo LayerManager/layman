1c7faa772acd38df2e00dd8a44963222446b3767
index-git <sileny.index@gmail.com>
2021-10-06 12:37:51 +0200

    Separate list of publications to test in dynamic_data tests

--- tests/dynamic_data/publication_test.py
+++ tests/dynamic_data/publication_test.py
@@ -2,8 +2,9 @@ from contextlib import nullcontext as does_not_raise
 import pytest
 
 from test_tools import process_client, process
+from . import publications as data
 from ..asserts import util
-from .. import Action, dynamic_data as data
+from .. import Action, dynamic_data as consts
 
 
 @pytest.fixture(scope="session", autouse=True)
@@ -28,22 +29,22 @@ def publication_id(publication):
 def test_action_chain(publication):
     for step in data.PUBLICATIONS[publication]:
         response = None
-        action = step[data.KEY_ACTION]
-        exp_exception = pytest.raises(action[data.KEY_CALL_EXCEPTION][data.KEY_EXCEPTION]) if data.KEY_CALL_EXCEPTION in action else does_not_raise()
+        action = step[consts.KEY_ACTION]
+        exp_exception = pytest.raises(action[consts.KEY_CALL_EXCEPTION][consts.KEY_EXCEPTION]) if consts.KEY_CALL_EXCEPTION in action else does_not_raise()
         with exp_exception as exception_info:
-            action_call = action[data.KEY_CALL]
+            action_call = action[consts.KEY_CALL]
             response = util.run_action(publication, action_call)
         exception_assert_param = {'thrown': exception_info}
-        for assert_call in action.get(data.KEY_CALL_EXCEPTION, dict()).get(data.KEY_EXCEPTION_ASSERTS, list()):
+        for assert_call in action.get(consts.KEY_CALL_EXCEPTION, dict()).get(consts.KEY_EXCEPTION_ASSERTS, list()):
             params = dict(**exception_assert_param, **assert_call.params)
             util.run_action(publication, Action(assert_call.method, params))
 
         if not exception_info:
             response_assert_param = {'response': response}
-            for assert_response in action.get(data.KEY_RESPONSE_ASSERTS, list()):
+            for assert_response in action.get(consts.KEY_RESPONSE_ASSERTS, list()):
                 params = dict(**response_assert_param, **assert_response.params)
                 util.run_action(publication, Action(assert_response.method, params))
 
         data_cache = dict()
-        for assert_call in step[data.KEY_FINAL_ASSERTS]:
+        for assert_call in step[consts.KEY_FINAL_ASSERTS]:
             util.run_action(publication, assert_call, cache=data_cache)
