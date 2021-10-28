821c9b3633b9da3ec4ffb21a8612c66ea3b65e80
index-git <sileny.index@gmail.com>
2021-10-28 10:27:40 +0200

    Add more info to dynamic tests logging

--- tests/dynamic_data/publication_test.py
+++ tests/dynamic_data/publication_test.py
@@ -28,7 +28,7 @@ def publication_id(publication):
 @pytest.mark.parametrize('publication', data.PUBLICATIONS, ids=publication_id)
 @pytest.mark.usefixtures('liferay_mock', 'ensure_layman')
 def test_action_chain(publication):
-    for step in data.PUBLICATIONS[publication]:
+    for action_idx, step in enumerate(data.PUBLICATIONS[publication]):
         response = None
         action = step[consts.KEY_ACTION]
         exp_exception = pytest.raises(action[consts.KEY_CALL_EXCEPTION][consts.KEY_EXCEPTION]) if consts.KEY_CALL_EXCEPTION in action else does_not_raise()
@@ -47,5 +47,10 @@ def test_action_chain(publication):
                 util.run_action(publication, Action(assert_response.method, params))
 
         data_cache = dict()
-        for assert_call in step[consts.KEY_FINAL_ASSERTS]:
-            util.run_action(publication, assert_call, cache=data_cache)
+        for final_assert_idx, assert_call in enumerate(step[consts.KEY_FINAL_ASSERTS]):
+            try:
+                util.run_action(publication, assert_call, cache=data_cache)
+            except AssertionError as exc:
+                print(
+                    f'Final assert error raised: publication={publication}, action_idx={action_idx}, final_assert_idx={final_assert_idx}, assert_call={assert_call}')
+                raise exc from exc
