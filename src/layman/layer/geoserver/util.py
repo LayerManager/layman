f5b269bb5d4417d6b0dc2d2c7283456ce5caf257
index-git <sileny.index@gmail.com>
2021-11-16 09:18:59 +0100

    Treat 404 for wms_direct and wfs_direct to nor raise exception

--- src/layman/layer/geoserver/util.py
+++ src/layman/layer/geoserver/util.py
@@ -1,4 +1,6 @@
+import logging
 from urllib.parse import urlparse
+import requests.exceptions
 from owslib.wms import WebMapService
 from owslib.wfs import WebFeatureService
 
@@ -6,6 +8,7 @@ from geoserver.util import get_proxy_base_url
 from layman.cache.mem import CACHE as MEM_CACHE
 
 
+logger = logging.getLogger(__name__)
 CACHE_GS_PROXY_BASE_URL_KEY = f'{__name__}:GS_PROXY_BASE_URL'
 
 from layman import settings
@@ -22,7 +25,13 @@ def get_gs_proxy_base_url():
 def wms_direct(wms_url, xml=None, version=None, headers=None):
     from layman.layer.geoserver.wms import VERSION
     version = version or VERSION
-    wms = WebMapService(wms_url, xml=xml.encode('utf-8') if xml is not None else xml, version=version, headers=headers)
+    try:
+        wms = WebMapService(wms_url, xml=xml.encode('utf-8') if xml is not None else xml, version=version, headers=headers)
+    except requests.exceptions.HTTPError as exc:
+        if exc.response.status_code == 404:
+            wms = None
+        else:
+            raise exc
     return wms
 
 
@@ -48,7 +57,13 @@ def wms_proxy(wms_url, xml=None, version=None, headers=None):
 def wfs_direct(wfs_url, xml=None, version=None, headers=None):
     from layman.layer.geoserver.wfs import VERSION
     version = version or VERSION
-    wfs = WebFeatureService(wfs_url, xml=xml.encode('utf-8') if xml is not None else xml, version=version, headers=headers)
+    try:
+        wfs = WebFeatureService(wfs_url, xml=xml.encode('utf-8') if xml is not None else xml, version=version, headers=headers)
+    except requests.exceptions.HTTPError as exc:
+        if exc.response.status_code == 404:
+            wfs = None
+        else:
+            raise exc
     return wfs
 
 
