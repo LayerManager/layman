ea84928a87592747aff5e8e7c7b4fe070e51ec02
index-git <sileny.index@gmail.com>
2021-12-13 16:52:50 +0100

    Add no_padding_area into CRS definitions

--- src/crs/__init__.py
+++ src/crs/__init__.py
@@ -3,6 +3,12 @@ from collections import namedtuple
 CRSTypeDef = namedtuple('CRSTypeDef', [
     # Bounding box used if data file is empty (in EPSG:3857)
     'world_bbox',
+    # If bounding box of layman.layer has no area in at least one dimension,
+    # this padding in meters will be added to all dimensions whose coordinates equal
+    # for GeoServer feature type definiton and thumbnail rendering.
+    # E.g. if bbox is [5, 100, 5, 200] and NO_AREA_BBOX_PADDING = 10,
+    # thumbnail will be rendered with bbox [-5, 100, 15, 200].
+    'no_padding_area',
 ])
 
 EPSG_3857 = 'EPSG:3857'
@@ -16,6 +22,7 @@ CRSDefinitions = {
             20026376.39,
             20048966.10,
         ),
+        no_padding_area=10,
     ),
     EPSG_4326: CRSTypeDef(
         world_bbox=(
@@ -24,5 +31,6 @@ CRSDefinitions = {
             180,
             90,
         ),
+        no_padding_area=0.00001,
     )
 }
