3742540fe8b7aa62fafb727e005d243ff86ab154
index-git <sileny.index@gmail.com>
2021-12-13 17:40:17 +0100

    Fix bbox transform method to respect world boundaries

--- src/crs/__init__.py
+++ src/crs/__init__.py
@@ -5,10 +5,12 @@ CRSTypeDef = namedtuple('CRSTypeDef', [
     'world_bbox',
     # If bounding box of layman.layer has no area in at least one dimension,
     # this padding in meters will be added to all dimensions whose coordinates equal
-    # for GeoServer feature type definiton and thumbnail rendering.
+    # for GeoServer feature type definition and thumbnail rendering.
     # E.g. if bbox is [5, 100, 5, 200] and NO_AREA_BBOX_PADDING = 10,
     # thumbnail will be rendered with bbox [-5, 100, 15, 200].
     'no_padding_area',
+    # Maximum coordinates of other CRS, which can be transformed
+    'world_bounds',
 ])
 
 EPSG_3857 = 'EPSG:3857'
@@ -23,6 +25,14 @@ CRSDefinitions = {
             20048966.10,
         ),
         no_padding_area=10,
+        world_bounds={
+            EPSG_4326: (
+                -180,
+                -85.06,
+                180,
+                85.06,
+            )
+        }
     ),
     EPSG_4326: CRSTypeDef(
         world_bbox=(
@@ -32,5 +42,6 @@ CRSDefinitions = {
             90,
         ),
         no_padding_area=0.00001,
+        world_bounds=dict(),
     )
 }
