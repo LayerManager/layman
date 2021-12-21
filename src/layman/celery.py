c3806a3b370dfaa65eb3a47b44067588c5923c59
Jiri Kozel <jirik@users.noreply.github.com>
2021-12-21 12:12:41 +0100

    Add to_chain_info_with_states

--- src/layman/celery.py
+++ src/layman/celery.py
@@ -139,6 +139,15 @@ def is_task_running(task_name, workspace, publication_name=None):
     return result
 
 
+def to_chain_info_with_states(chain_info):
+    return {
+        **chain_info,
+        'last': chain_info['last'].state,
+        'by_name': {k: v.state for k, v in chain_info['by_name']},
+        'by_order': [t.state for t in chain_info['by_order']],
+    }
+
+
 def get_publication_chain_info_dict(workspace, publication_type, publication_name):
     rds = settings.LAYMAN_REDIS
     key = PUBLICATION_CHAIN_INFOS
