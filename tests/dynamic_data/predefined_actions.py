dbdadee3bcfe9569206757c2b1c652143d5a7415
index-git <sileny.index@gmail.com>
2022-01-03 17:56:42 +0100

    Move shp_without_prj to wrong_input generator

--- tests/dynamic_data/predefined_actions.py
+++ tests/dynamic_data/predefined_actions.py
@@ -1,4 +1,3 @@
-from layman import LaymanError
 import tests.asserts.processing as processing
 from test_tools import process_client
 from .. import dynamic_data as consts, Action
@@ -11,30 +10,12 @@ _POST_ZIP_SHP_WITHOUT_PRJ_DEFINITION = {
         'tmp/naturalearth/110m/cultural/ne_110m_admin_0_boundary_lines_land.shp',
         'tmp/naturalearth/110m/cultural/ne_110m_admin_0_boundary_lines_land.shx',
     ],
-    'compress': True,
-}
-
-POST_ZIP_SHP_WITHOUT_PRJ = {
-    consts.KEY_CALL: Action(process_client.publish_workspace_publication, _POST_ZIP_SHP_WITHOUT_PRJ_DEFINITION),
-    consts.KEY_CALL_EXCEPTION: {
-        consts.KEY_EXCEPTION: LaymanError,
-        consts.KEY_EXCEPTION_ASSERTS: [
-            Action(processing.exception.response_exception, {'expected': {'http_code': 400,
-                                                                          'code': 18,
-                                                                          'message': 'Missing one or more ShapeFile files.',
-                                                                          'detail': {
-                                                                              'missing_extensions': ['.prj'],
-                                                                              'suggestion': 'Missing .prj file can be fixed also by setting "crs" parameter.',
-                                                                              'path': 'temporary_zip_file.zip/ne_110m_admin_0_boundary_lines_land.shp',
-                                                                          },
-                                                                          }, }, ),
-        ],
-    },
 }
 
 POST_ZIP_SHP_WITHOUT_PRJ_WITH_CRS = {
     consts.KEY_CALL: Action(process_client.publish_workspace_publication, {
         **_POST_ZIP_SHP_WITHOUT_PRJ_DEFINITION,
+        'compress': True,
         'crs': 'EPSG:4326',
     }),
     consts.KEY_RESPONSE_ASSERTS: [
