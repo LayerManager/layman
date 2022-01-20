00ee67c918f97319b1f8a15ba46e19fa1382dff3
index-git <sileny.index@gmail.com>
2022-01-20 09:41:31 +0100

    Test wfs bounding_box value

--- src/layman/common/bbox.py
+++ src/layman/common/bbox.py
@@ -15,9 +15,10 @@ def is_valid(coords):
         and (is_empty(coords) or (coords[0] <= coords[2] and coords[1] <= coords[3]))
 
 
-def contains_bbox(bbox1, bbox2):
+def contains_bbox(bbox1, bbox2, *, precision=0):
     return not is_empty(bbox1) and not is_empty(bbox2) \
-        and bbox1[0] <= bbox2[0] and bbox2[2] <= bbox1[2] and bbox1[1] <= bbox2[1] and bbox2[3] <= bbox1[3]
+        and bbox1[0] <= bbox2[0] + precision and bbox2[2] <= bbox1[2] + precision and bbox1[1] <= bbox2[1] + precision \
+        and bbox2[3] <= bbox1[3] + precision
 
 
 def intersects(bbox1, bbox2):
