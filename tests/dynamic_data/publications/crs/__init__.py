469ae0726ff6c1dea6885b02ea622120aa6e1c36
index-git <sileny.index@gmail.com>
2022-01-20 17:39:19 +0100

    Refactor crs dynamic tests generator

--- tests/dynamic_data/publications/crs/__init__.py
+++ tests/dynamic_data/publications/crs/__init__.py
@@ -56,63 +56,42 @@ def generate_local(workspace=None):
         exp_thumbnail = tc_params[KEY_THUMBNAIL]
 
         for rest_param_dict in rest_param_dicts:
-            test_case_postfix = '_'.join([REST_PARAMETRIZATION[key][value]
-                                          for key, value in rest_param_dict.items()
-                                          if REST_PARAMETRIZATION[key][value]])
-            publ_name = "_".join([part for part in [testcase, 'post', test_case_postfix] if part])
-            if any(k in rest_param_dict and rest_param_dict[k] != v for k, v in action_params.items()):
-                continue
+            for action_code, action_method, action_predecessor in [
+                ('post', process_client.publish_workspace_publication, []),
+                ('patch', process_client.patch_workspace_publication, [layers.DEFAULT_POST])
+            ]:
+                test_case_postfix = '_'.join([REST_PARAMETRIZATION[key][value]
+                                              for key, value in rest_param_dict.items()
+                                              if REST_PARAMETRIZATION[key][value]])
+                publ_name = "_".join([part for part in [testcase, action_code, test_case_postfix] if part])
+                if any(k in rest_param_dict and rest_param_dict[k] != v for k, v in action_params.items()):
+                    continue
 
-            post_info_values = copy.deepcopy(info_values)
-            if rest_param_dict['compress']:
-                post_info_values['gdal_prefix'] = '/vsizip/'
-                post_info_values['file_extension'] = f'zip/{file_name}.shp'
+                post_info_values = copy.deepcopy(info_values)
+                if rest_param_dict['compress']:
+                    post_info_values['gdal_prefix'] = '/vsizip/'
+                    post_info_values['file_extension'] = f'zip/{file_name}.shp'
 
-            action_def = {
-                consts.KEY_ACTION: {
-                    consts.KEY_CALL: Action(process_client.publish_workspace_publication,
-                                            {**action_params,
-                                             **rest_param_dict}),
-                    consts.KEY_RESPONSE_ASSERTS: [
-                        Action(processing.response.valid_post, dict()),
-                    ],
-                },
-                consts.KEY_FINAL_ASSERTS: [
-                    *publication.IS_LAYER_COMPLETE_AND_CONSISTENT,
-                    Action(publication.internal.correct_values_in_detail, copy.deepcopy(post_info_values)),
-                    Action(publication.internal.thumbnail_equals, {'exp_thumbnail': exp_thumbnail, }),
-                    *tc_params.get(consts.KEY_FINAL_ASSERTS, list()),
-                ]
-            }
-            result[Publication(workspace, process_client.LAYER_TYPE, publ_name)] = [action_def]
+                action_def = {
+                    consts.KEY_ACTION: {
+                        consts.KEY_CALL: Action(action_method,
+                                                {**action_params,
+                                                 **rest_param_dict}),
+                        consts.KEY_RESPONSE_ASSERTS: [
+                            Action(processing.response.valid_post, dict()),
+                        ],
+                    },
+                    consts.KEY_FINAL_ASSERTS: [
+                        *publication.IS_LAYER_COMPLETE_AND_CONSISTENT,
+                        Action(publication.internal.correct_values_in_detail, copy.deepcopy(post_info_values)),
+                        Action(publication.internal.thumbnail_equals, {'exp_thumbnail': exp_thumbnail, }),
+                        *tc_params.get(consts.KEY_FINAL_ASSERTS, list()),
+                    ]
+                }
+                actions_list = copy.deepcopy(action_predecessor)
+                actions_list.append(action_def)
+                result[Publication(workspace, process_client.LAYER_TYPE, publ_name)] = actions_list
 
-        for rest_param_dict in rest_param_dicts:
-            test_case_postfix = '_'.join([REST_PARAMETRIZATION[key][value]
-                                          for key, value in rest_param_dict.items()
-                                          if REST_PARAMETRIZATION[key][value]])
-            publ_name = "_".join([part for part in [testcase, 'patch', test_case_postfix] if part])
-            patch_info_values = copy.deepcopy(info_values)
-            if rest_param_dict['compress']:
-                patch_info_values['gdal_prefix'] = '/vsizip/'
-                patch_info_values['file_extension'] = f'zip/{file_name}.shp'
-
-            patch_action = {
-                consts.KEY_ACTION: {
-                    consts.KEY_CALL: Action(process_client.patch_workspace_publication,
-                                            {**action_params,
-                                             **rest_param_dict}),
-                    consts.KEY_RESPONSE_ASSERTS: [
-                        Action(processing.response.valid_post, dict()),
-                    ],
-                },
-                consts.KEY_FINAL_ASSERTS: [
-                    *publication.IS_LAYER_COMPLETE_AND_CONSISTENT,
-                    Action(publication.internal.correct_values_in_detail, copy.deepcopy(patch_info_values)),
-                    Action(publication.internal.thumbnail_equals, {'exp_thumbnail': exp_thumbnail, }),
-                    *tc_params.get(consts.KEY_FINAL_ASSERTS, list()),
-                ]
-            }
-            result[Publication(workspace, process_client.LAYER_TYPE, publ_name)] = [layers.DEFAULT_POST, patch_action]
     return result
 
 
