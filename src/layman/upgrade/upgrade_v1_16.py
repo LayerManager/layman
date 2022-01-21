785264de62e14032a7d1ff496d92240a79b522d9
index-git <sileny.index@gmail.com>
2022-01-21 09:45:22 +0100

    Basic SRID in publication table migration

--- src/layman/upgrade/upgrade_v1_16.py
+++ src/layman/upgrade/upgrade_v1_16.py
@@ -1,5 +1,6 @@
 import logging
 
+from crs import EPSG_3857
 from db import util as db_util
 from layman import settings
 
@@ -12,6 +13,18 @@ def adjust_db_for_srid():
 
     statement = f'ALTER TABLE {DB_SCHEMA}.publications ADD COLUMN IF NOT EXISTS srid integer;'
     db_util.run_statement(statement)
+
+
+def adjust_db_publication_srid_data():
+    crs = EPSG_3857
+    srid = db_util.get_srid(crs)
+
+    statement = f'''
+    update {DB_SCHEMA}.publications set
+      srid = %s
+    where srid is null
+    ;'''
+    db_util.run_statement(statement, (srid, ))
 
     statement = f'alter table {DB_SCHEMA}.publications add constraint bbox_with_crs_check CHECK ' \
                 f'(bbox is null or srid is not null);'
