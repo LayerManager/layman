5f55a89649a6751ca58fda97c65f094e76e16b0c
index-git <sileny.index@gmail.com>
2022-02-25 11:43:49 +0100

    Prepare dynamic tests file_input for maps

--- tests/asserts/final/publication/__init__.py
+++ tests/asserts/final/publication/__init__.py
@@ -1,4 +1,5 @@
 from tests import Action
+from test_tools import process_client
 from . import geoserver, internal, internal_rest, rest
 
 
@@ -22,3 +23,27 @@ IS_LAYER_COMPLETE_AND_CONSISTENT = [
     Action(geoserver.workspace_wms_1_3_0_capabilities_available, dict()),
     Action(geoserver.workspace_wfs_2_0_0_capabilities_available_if_vector, dict()),
 ]
+
+IS_MAP_COMPLETE_AND_CONSISTENT = [
+    Action(internal.source_has_its_key_or_it_is_empty, dict()),
+    Action(internal.source_internal_keys_are_subset_of_source_sibling_keys, dict()),
+    Action(internal_rest.same_title_in_source_and_rest_multi, dict()),
+    Action(internal_rest.same_values_in_internal_and_rest, dict()),
+    Action(rest.is_in_rest_multi, dict()),
+    Action(rest.correct_url_in_rest_multi, dict()),
+    Action(internal.same_value_of_key_in_all_sources, dict()),
+    Action(internal.mandatory_keys_in_all_sources, dict()),
+    Action(internal.metadata_key_sources_do_not_contain_other_keys, dict()),
+    Action(internal.thumbnail_key_sources_do_not_contain_other_keys, dict()),
+    Action(internal.mandatory_keys_in_primary_db_schema_of_actor, dict()),
+    Action(internal.other_keys_not_in_primary_db_schema_of_actor, dict()),
+    Action(internal.mandatory_keys_in_all_sources_of_actor, dict()),
+    Action(rest.is_complete_in_rest, dict()),
+    Action(rest.mandatory_keys_in_rest, dict()),
+]
+
+
+IS_PUBLICATION_COMPLETE_AND_CONSISTENT = {
+    process_client.LAYER_TYPE: IS_LAYER_COMPLETE_AND_CONSISTENT,
+    process_client.MAP_TYPE: IS_MAP_COMPLETE_AND_CONSISTENT,
+}
