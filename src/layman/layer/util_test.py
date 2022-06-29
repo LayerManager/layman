d3290b1d8598d13b8439704008080c488c4814bf
index-git <sileny.index@gmail.com>
2022-06-29 12:50:24 +0200

    Allow publication name to start with numbers

--- src/layman/layer/util_test.py
+++ src/layman/layer/util_test.py
@@ -9,8 +9,8 @@ from .util import to_safe_layer_name, fill_in_partial_info_statuses
 def test_to_safe_layer_name():
     assert to_safe_layer_name('') == 'layer'
     assert to_safe_layer_name(' ?:"+  @') == 'layer'
-    assert to_safe_layer_name('01 Stanice vodních toků 26.4.2017 (voda)') == \
-        'layer_01_stanice_vodnich_toku_26_4_2017_voda'
+    assert to_safe_layer_name('01 Stanice vodních toků 26.4.2017 (voda)') == '01_stanice_vodnich_toku_26_4_2017_voda'
+    assert to_safe_layer_name('řecko') == 'recko'
 
 
 def test_fill_in_partial_info_statuses():
