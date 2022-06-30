8fce2383bd46f6a914bd3874c1e490ef10cb2393
index-git <sileny.index@gmail.com>
2022-06-30 10:36:16 +0200

    Enable force cleanup after tests

--- test_tools/cleanup.py
+++ test_tools/cleanup.py
@@ -2,8 +2,8 @@ from test_tools import process_client
 from tests.asserts import util
 
 
-def cleanup_publications(request, publications):
-    if request.session.testsfailed == 0 and not request.config.option.nocleanup:
+def cleanup_publications(request, publications, *, force=False):
+    if request.session.testsfailed == 0 and (not request.config.option.nocleanup or force):
         for publication in publications:
             if util.get_publication_exists(publication):
                 headers = util.get_publication_header(publication)
