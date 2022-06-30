8c3bc22bf162e33f4128481a2ff2d69095e37bd4
index-git <sileny.index@gmail.com>
2022-06-30 10:54:55 +0200

    Add definition of pytest_generate_tests to see future differences

--- tests/dynamic_data/publications/publication_name_test.py
+++ tests/dynamic_data/publications/publication_name_test.py
@@ -1,9 +1,12 @@
+import copy
 import os
 
 import tests
 from test_tools import process_client
 from tests.asserts.final.publication import util as assert_util
 from tests.dynamic_data import base_test
+from ... import Publication, TestTypes, TestKeys
+
 
 DIRECTORY = os.path.dirname(os.path.abspath(__file__))
 
@@ -12,7 +15,38 @@ TEST_CASES = {
 }
 
 
-pytest_generate_tests = base_test.pytest_generate_tests
+def pytest_generate_tests(metafunc):
+    # used for parametrizing subclasses of TestSingleRestPublication, called once per each test function
+    # https://docs.pytest.org/en/6.2.x/parametrize.html#pytest-generate-tests
+    test_type_str = os.getenv(TestKeys.TYPE.value, TestTypes.MANDATORY.value)
+    test_type = TestTypes(test_type_str)
+    cls = metafunc.cls
+    rest_methods = cls.rest_parametrization['method']
+    argvalues = []
+    ids = []
+
+    test_cases_for_type = {key: params for key, params in cls.test_cases.items() if
+                           test_type == TestTypes.OPTIONAL or params.get(TestKeys.TYPE, cls.default_test_type) == TestTypes.MANDATORY}
+    for key, params in test_cases_for_type.items():
+        for method_function_name, method_name in rest_methods.items():
+            publ_name = cls.key_to_publication_base_name(key) + f"_{method_name}"
+            publication = Publication(cls.workspace, cls.publication_type, publ_name)
+            rest_method = getattr(cls, method_function_name)
+            argvalues.append([
+                publication,
+                key,
+                copy.deepcopy(params),
+                rest_method,
+                (publication, method_name),
+            ])
+            ids.append(publ_name)
+    publ_type_name = cls.publication_type.split('.')[-1]
+    metafunc.parametrize(
+        argnames=f'{publ_type_name}, key, params, rest_method, post_before_patch',
+        argvalues=argvalues,
+        ids=ids,
+        indirect=['post_before_patch'],
+    )
 
 
 class TestPublication(base_test.TestSingleRestPublication):
