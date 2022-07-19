2eca3d0a60238f8c46075cab8cc089d1166d682c
index-git <sileny.index@gmail.com>
2022-07-19 14:45:08 +0200

    Clean file_type for layers during patch

--- src/layman/layer/prime_db_schema/file_data.py
+++ src/layman/layer/prime_db_schema/file_data.py
@@ -1,4 +1,4 @@
-from layman import patch_mode
+from layman import patch_mode, settings
 from layman.common import empty_method, empty_method_returns_none, empty_method_returns_dict
 from layman.common.prime_db_schema import publications
 from .. import LAYER_TYPE
@@ -7,11 +7,18 @@ PATCH_MODE = patch_mode.DELETE_IF_DEPENDANT
 
 get_publication_uuid = empty_method_returns_none
 get_layer_info = empty_method_returns_dict
-patch_layer = empty_method
 pre_publication_action_check = empty_method
-post_layer = empty_method
 get_metadata_comparison = empty_method_returns_dict
 
 
 def delete_layer(workspace, layername):
     publications.set_bbox(workspace, LAYER_TYPE, layername, bbox=(None, None, None, None, ), crs=None)
+    publications.set_file_type(workspace, LAYER_TYPE, layername, settings.FILE_TYPE_UNKNOWN, )
+
+
+def patch_layer(workspace, layername, *, file_type):
+    publications.set_file_type(workspace, LAYER_TYPE, layername, file_type, )
+
+
+def post_layer(workspace, layername, *, file_type):
+    publications.set_file_type(workspace, LAYER_TYPE, layername, file_type, )
