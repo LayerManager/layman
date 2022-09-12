5d92b8a629a69b88d4817a37c86e7d9b80b31774
Jiri Kozel <jirik@users.noreply.github.com>
2022-09-12 14:10:09 +0200

    Add native_* keys to map internal sources

--- src/layman/map/__init__.py
+++ src/layman/map/__init__.py
@@ -48,7 +48,8 @@ PUBLICATION_TYPES = {
         'internal_sources': OrderedDict([
             ('layman.map.filesystem.uuid', InternalSourceTypeDef(info_items=[]),),
             ('layman.map.prime_db_schema.table', InternalSourceTypeDef(info_items=['access_rights', 'name', 'title', 'uuid',
-                                                                                   'bounding_box', ]),),
+                                                                                   'bounding_box', 'native_crs',
+                                                                                   'native_bounding_box', ]),),
             ('layman.map.filesystem.input_file', InternalSourceTypeDef(info_items=['description', 'file']),),
             ('layman.map.prime_db_schema.file_data', InternalSourceTypeDef(info_items=[]),),
             ('layman.map.filesystem.thumbnail', InternalSourceTypeDef(info_items=['thumbnail', ]),),
