83b563f7f517d7a5e9a1cf3d4a1299b293c7d6ff
Jiri Kozel <jirik@users.noreply.github.com>
2022-09-12 13:55:30 +0200

    Minor code management in test assert

--- tests/asserts/final/publication/internal.py
+++ tests/asserts/final/publication/internal.py
@@ -21,12 +21,14 @@ def source_has_its_key_or_it_is_empty(workspace, publ_type, name):
 
 def source_internal_keys_are_subset_of_source_sibling_keys(workspace, publ_type, name):
     with app.app_context():
-        all_items = layman_util.get_publication_types()[publ_type]['internal_sources'].values()
-        for source_def in all_items:
+        internal_sources = layman_util.get_publication_types()[publ_type]['internal_sources']
+        for source_def in internal_sources.values():
             for key in source_def.info_items:
                 context = {'keys': [key]}
                 info = layman_util.get_publication_info(workspace, publ_type, name, context)
-                all_sibling_keys = set(sibling_key for item_list in all_items for sibling_key in item_list.info_items
+                all_sibling_keys = set(sibling_key
+                                       for item_list in internal_sources.values()
+                                       for sibling_key in item_list.info_items
                                        if key in item_list.info_items)
                 internal_keys = [key[1:] for key in info if key.startswith('_')]
                 assert set(internal_keys) <= all_sibling_keys, \
