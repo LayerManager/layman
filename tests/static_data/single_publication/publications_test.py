70e02d99923e516b5b081a2e5883dfdf21f76250
index-git <sileny.index@gmail.com>
2022-09-13 10:31:50 +0200

    Add all_keys_assigned_to_source test

--- tests/static_data/single_publication/publications_test.py
+++ tests/static_data/single_publication/publications_test.py
@@ -27,6 +27,7 @@ def test_get_publication_info_items(workspace, publ_type, publication):
     ensure_publication(workspace, publ_type, publication)
     asserts_internal.source_has_its_key_or_it_is_empty(workspace, publ_type, publication)
     asserts_internal.source_internal_keys_are_subset_of_source_sibling_keys(workspace, publ_type, publication)
+    asserts_internal.all_keys_assigned_to_source(workspace, publ_type, publication)
 
 
 @pytest.mark.parametrize('workspace, publ_type, publication', data.LIST_ALL_PUBLICATIONS)
