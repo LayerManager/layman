21b73589262f573bc5637a1e2a6df3f07a85f8d1
Jiri Kozel <jirik@users.noreply.github.com>
2022-09-22 09:52:43 +0200

    Be more specific about 0 values in customized SLD creation

--- src/layman/layer/geoserver/sld.py
+++ src/layman/layer/geoserver/sld.py
@@ -96,10 +96,10 @@ def create_customized_grayscale_sld(*, file_path, min_value, max_value, nodata_v
     # It seems GS changes NoData values to 0 during rendering,
     # and then it treats both NoData cells and 0 cells in the same way.
     if nodata_value is not None:
-        # if nodata_value > max_value, setting nodata_high_entry seems not necessary
-        if nodata_value < min_value:
+        # if 0 < min_value <= max_value < nodata_value, setting nodata_high_entry seems not necessary
+        if 0 < nodata_value < min_value:
             nodata_low_entry = f'<sld:ColorMapEntry color="#000000" quantity="{nodata_value}" opacity="0" />'
-        if min_value < nodata_value < max_value:
+        if 0 < min_value < nodata_value < max_value:
             nodata_low_entry = f'<sld:ColorMapEntry color="#000000" quantity="0" opacity="0" />'
     with open(os.path.join(DIRECTORY, 'sld_customized_raster_template.sld'), 'r') as template_file:
         template_str = template_file.read()
