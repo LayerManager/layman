da2f42d1f9ca8da3357a78ae3f60fd25b2174162
index-git <sileny.index@gmail.com>
2022-11-10 17:07:16 +0100

    Catch wms error `AttributeError: 'NoneType' object has no attribute 'find'`

--- src/layman/layer/geoserver/util.py
+++ src/layman/layer/geoserver/util.py
@@ -25,16 +25,17 @@ def wms_proxy(wms_url, xml=None, version=None, headers=None):
     wms_url_path = urlparse(wms_url).path
     # current_app.logger.info(f"xml=\n{xml}")
     wms = wms_direct(wms_url, xml=xml, version=version, headers=headers)
-    for operation in wms.operations:
-        # app.logger.info(operation.name)
-        for method in operation.methods:
-            method_url = urlparse(method['url'])
-            method_url = method_url._replace(
-                netloc=settings.LAYMAN_GS_HOST + ':' + settings.LAYMAN_GS_PORT,
-                path=wms_url_path,
-                scheme='http'
-            )
-            method['url'] = method_url.geturl()
+    if wms:
+        for operation in wms.operations:
+            # app.logger.info(operation.name)
+            for method in operation.methods:
+                method_url = urlparse(method['url'])
+                method_url = method_url._replace(
+                    netloc=settings.LAYMAN_GS_HOST + ':' + settings.LAYMAN_GS_PORT,
+                    path=wms_url_path,
+                    scheme='http'
+                )
+                method['url'] = method_url.geturl()
     return wms
 
 
