a4f53f78900919652d9cbbe0bcbf0fdd9b309cd7
index-git <sileny.index@gmail.com>
2022-11-22 16:45:04 +0100

    Let test all optional tests

--- test_separated.sh
+++ test_separated.sh
@@ -7,5 +7,8 @@ bash src/clear-python-cache.sh
 mkdir -p tmp/artifacts
 rm -rf tmp/artifacts/*
 
-python3 src/assert_db.py && python3 src/wait_for_deps.py && python3 src/clear_layman_data.py && python3 -m pytest -W ignore::DeprecationWarning -xvv --ignore=tests/static_data/ tests
+max_fail="$1"
+if [ -z "$max_fail" ]; then max_fail=1; fi
+
+python3 src/assert_db.py && python3 src/wait_for_deps.py && python3 src/clear_layman_data.py && python3 -m pytest -W ignore::DeprecationWarning --maxfail="$max_fail" -vv --ignore=tests/static_data/ tests
 #python3 src/assert_db.py && python3 src/wait_for_deps.py && python3 src/clear_layman_data.py && python3 -m TEST_TYPE=optional pytest -W ignore::DeprecationWarning -sxvv --capture=tee-sys --nocleanup --ignore=tests/static_data/ tests
