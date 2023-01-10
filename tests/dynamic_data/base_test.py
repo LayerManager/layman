058018d662f54a9d578ea69887f5325e25e7d7ee
Jiri Kozel <jirik@users.noreply.github.com>
2023-01-10 13:47:07 +0100

    Parametrize implicit_name_test.py with new rest_parametrization

--- tests/dynamic_data/base_test.py
+++ tests/dynamic_data/base_test.py
@@ -4,7 +4,7 @@ from enum import Enum
 import copy
 from dataclasses import dataclass, field
 import os
-from typing import final, List
+from typing import final, List, Union, Callable
 import pytest
 import _pytest.mark.structures
 from test_tools import process_client, cleanup
@@ -12,9 +12,10 @@ from .. import Publication, EnumTestTypes, EnumTestKeys
 
 
 class RestArgDomain(Enum):
-    def __init__(self, raw_value, publ_name_part):
+    def __init__(self, raw_value, publ_name_part, other_rest_args=None):
         self.raw_value = raw_value
         self.publ_name_part = publ_name_part
+        self.other_rest_args = other_rest_args or {}
 
 
 class WithChunksDomain(RestArgDomain):
@@ -22,18 +23,23 @@ class WithChunksDomain(RestArgDomain):
     TRUE = (True, 'chunks')
 
 
-class CompressDomain(RestArgDomain):
+class CompressDomainBase(RestArgDomain):
+    pass
+
+
+class CompressDomain(CompressDomainBase):
     FALSE = (False, None)
     TRUE = (True, 'zipped')
 
 
 class RestArgs(Enum):
     WITH_CHUNKS = ('with_chunks', WithChunksDomain)
-    COMPRESS = ('compress', CompressDomain)
+    COMPRESS = ('compress', CompressDomain, CompressDomainBase)
 
-    def __init__(self, name, domain):
+    def __init__(self, name, domain, base_domain=None):
         self.arg_name = name
         self.domain = domain
+        self.base_domain = base_domain or domain
 
 
 class RestMethod(Enum):
@@ -52,12 +58,14 @@ def get_dimension_enum(dimension):
 @dataclass(frozen=True)
 class TestCaseType:
     pytest_id: str = None
-    publication: Publication = None
+    publication: Union[Publication, Callable[[dict], Publication]] = None
     key: str = None
     rest_method: RestMethod = None
     rest_args: dict = field(default_factory=dict)
     params: dict = field(default_factory=dict)
+    specific_params: dict = field(default_factory=dict)
     type: EnumTestTypes = EnumTestTypes.OPTIONAL
+    specific_types: dict = field(default_factory=dict)
     marks: List[_pytest.mark.structures.Mark] = field(default_factory=list)
 
 
@@ -74,6 +82,8 @@ def pytest_generate_tests(metafunc):
     test_cases_for_type = [test_case for test_case in test_cases if
                            test_type == EnumTestTypes.OPTIONAL or test_case.type == EnumTestTypes.MANDATORY]
     for test_case in test_cases_for_type:
+        assert not test_case.specific_params
+        assert not test_case.specific_types
         rest_method = getattr(cls, test_case.rest_method.function_name)
         rest_args = test_case.rest_args
         argvalues.append(pytest.param(
@@ -96,18 +106,43 @@ def pytest_generate_tests(metafunc):
 
 
 def check_rest_parametrization(cls):
-    possible_dimensions = [RestMethod] + list(RestArgs)
     for val in cls.rest_parametrization:
-        assert val in possible_dimensions, f"Only dimensions are allowed in cls.rest_parametrization. Found: {val}"
+        simple_type = val == RestMethod or val in list(RestArgs)
+        base_arg = inspect.isclass(val) and next((arg for arg in RestArgs if issubclass(val, arg.base_domain)), None)
+        custom_type = not simple_type and base_arg
+        assert simple_type or custom_type, f"Only dimensions are allowed in cls.rest_parametrization. Found: {val}"
+        if (not simple_type) and custom_type:
+            base_arg_domain_raw_values = set(v.raw_value for v in base_arg.domain)
+            domain_raw_values = set(v.raw_value for v in val)
+            assert domain_raw_values <= base_arg_domain_raw_values
 
 
 def check_input_test_cases(cls, parametrizations):
     for test_case in cls.test_cases:
+        if test_case.type:
+            for parametrization, specific_type in test_case.specific_types.items():
+                assert specific_type != test_case.type, f"No need to set specific test type that is same as main type: specific_type{specific_type}, type={test_case.type} test_case={test_case.key}"
+
+        all_specific_parametrizations = set(test_case.specific_types.keys()).union(set(test_case.specific_params.keys()))
+        for sp_parametrization in all_specific_parametrizations:
+            assert len(sp_parametrization) == len(cls.rest_parametrization), f"Specific parametrization must have same number of members as cls.rest_paramertization"
+            for dimension in cls.rest_parametrization:
+                dimension_enum = get_dimension_enum(dimension)
+                param_values = [v for v in sp_parametrization if v in dimension_enum]
+                assert len(param_values) == 1, f"Specific parametrization {sp_parametrization} does not have value of dimension {dimension}"
+
         rest_args = test_case.rest_args
         for parametrization in parametrizations:
             for arg_value in [v for v in parametrization if v not in RestMethod]:
-                arg = next(a for a in RestArgs if arg_value in a.domain)
-                assert arg.arg_name not in rest_args, f"REST argument can be set either in parametrization or in test case, not both: {arg}, test_case={test_case.key}"
+                base_arg = get_base_arg_of_arg_value(cls, arg_value)
+                assert base_arg.arg_name not in rest_args, f"REST argument can be set either in parametrization or in test case, not both: {base_arg}, test_case={test_case.key}"
+
+
+def get_base_arg_of_arg_value(cls, arg_value):
+    dimension = next(dim for dim in cls.rest_parametrization if arg_value in get_dimension_enum(dim))
+    domain = get_dimension_enum(dimension)
+    base_arg = next(arg for arg in RestArgs if issubclass(domain, arg.base_domain))
+    return base_arg
 
 
 @pytest.mark.usefixtures('ensure_layman_module')
@@ -142,20 +177,30 @@ class TestSingleRestPublication:
         test_cases = []
         for input_test_case in cls.test_cases:
             assert input_test_case.rest_method is None  # Maybe enable it later
+            assert input_test_case.pytest_id is None  # Maybe enable it later
 
             for parametrization in parametrizations:
+                specific_params = copy.deepcopy(input_test_case.specific_params.get(frozenset(parametrization), {}))
+                params = {**copy.deepcopy(input_test_case.params), **specific_params}
+                input_publication = None
+                if isinstance(input_test_case.publication, Publication):
+                    input_publication = input_test_case.publication
+                elif callable(input_test_case.publication):
+                    input_publication = input_test_case.publication(params)
+
                 workspace = cls.workspace
                 publication_type = cls.publication_type
 
-                if input_test_case.publication:
-                    workspace = input_test_case.publication.workspace or workspace
-                    publication_type = input_test_case.publication.type or publication_type
+                if input_publication:
+                    workspace = input_publication.workspace or workspace
+                    publication_type = input_publication.type or publication_type
                 publ_name_parts = [publication_type.split('.')[1], input_test_case.key.replace(':', '_').lower()] +\
                                   [p.publ_name_part for p in parametrization if p.publ_name_part]
                 name = '_'.join(publ_name_parts)
+                pytest_id = name
 
-                if input_test_case.publication:
-                    name = input_test_case.publication.name or name
+                if input_publication:
+                    name = input_publication.name or name
 
                 rest_method = next(
                     (v for v in parametrization if v in RestMethod),
@@ -163,16 +208,20 @@ class TestSingleRestPublication:
                 )
                 rest_args = copy.deepcopy(input_test_case.rest_args)
                 for arg_value in [v for v in parametrization if v not in RestMethod]:
-                    arg = next(a for a in RestArgs if arg_value in a.domain)
-                    rest_args[arg.arg_name] = arg_value.raw_value
+                    base_arg = get_base_arg_of_arg_value(cls, arg_value)
+                    rest_args[base_arg.arg_name] = arg_value.raw_value
+                    rest_args.update(copy.deepcopy(arg_value.other_rest_args))
 
-                test_case = TestCaseType(pytest_id=input_test_case.pytest_id or name,
+                test_type = input_test_case.specific_types.get(frozenset(parametrization),
+                                                               input_test_case.type or EnumTestTypes.OPTIONAL)
+
+                test_case = TestCaseType(pytest_id=pytest_id,
                                          publication=Publication(workspace, publication_type, name),
                                          key=input_test_case.key,
                                          rest_method=rest_method,
                                          rest_args=rest_args,
-                                         params=copy.deepcopy(input_test_case.params),
-                                         type=input_test_case.type or EnumTestTypes.OPTIONAL,
+                                         params=params,
+                                         type=test_type,
                                          marks=input_test_case.marks,
                                          )
                 test_cases.append(test_case)
--- tests/dynamic_data/publications/implicit_name_test.py
+++ tests/dynamic_data/publications/implicit_name_test.py
@@ -1,8 +1,9 @@
+from copy import deepcopy
 import os
 import pytest
 
 from test_tools import process_client
-from tests import EnumTestTypes, Publication, EnumTestKeys
+from tests import EnumTestTypes, Publication
 from tests.asserts.final.publication import util as asserts_util
 from tests.dynamic_data import base_test
 
@@ -10,49 +11,39 @@ DIRECTORY = os.path.dirname(os.path.abspath(__file__))
 
 pytest_generate_tests = base_test.pytest_generate_tests
 
+
+class CompressDomain(base_test.CompressDomainBase):
+    FALSE = (False, None)
+    TRUE = (True, 'zipped', {
+        'compress_settings': process_client.CompressTypeDef(archive_name='data_zip'),
+    })
+
+
 PUBLICATIONS = {
     'one_data_file': {
-        EnumTestKeys.TYPE: EnumTestTypes.OPTIONAL,
         'publication_type': process_client.LAYER_TYPE,
         'expected_name': 'ne_110m_admin_0_countries',
-        'params': {
+        'rest_args': {
+            'do_not_post_name': True,
             'file_paths': [
                 'tmp/naturalearth/110m/cultural/ne_110m_admin_0_countries.geojson',
             ],
         },
-    },
-    'one_data_file_with_chunks': {
-        EnumTestKeys.TYPE: EnumTestTypes.OPTIONAL,
-        'publication_type': process_client.LAYER_TYPE,
-        'expected_name': 'small_layer',
-        'params': {
-            'file_paths': ['sample/layman.layer/small_layer.geojson'],
-            'with_chunks': True,
+        'mandatory_cases': {
+            frozenset([CompressDomain.TRUE, base_test.WithChunksDomain.FALSE, ]),
+            frozenset([CompressDomain.TRUE, base_test.WithChunksDomain.TRUE, ]),
         },
-    },
-    'one_data_file_compressed': {
-        'publication_type': process_client.LAYER_TYPE,
-        'expected_name': 'small_layer_with_id',
-        'params': {
-            'file_paths': ['sample/layman.layer/small_layer_with_id.geojson'],
-            'compress': True,
-            'compress_settings': process_client.CompressTypeDef(archive_name='small_layer_with_id_zip'),
-        },
-    },
-    'one_data_file_compressed_with_chunks': {
-        'publication_type': process_client.LAYER_TYPE,
-        'expected_name': 'small_zip_layer',
-        'params': {
-            'file_paths': ['sample/layman.layer/small_layer.geojson'],
-            'compress': True,
-            'compress_settings': process_client.CompressTypeDef(archive_name='small_zip_layer'),
-            'with_chunks': True,
+        'specific_params': {
+            frozenset([CompressDomain.TRUE, base_test.WithChunksDomain.TRUE, ]): {
+                'expected_name': 'data_zip',
+            },
         },
     },
     'timeseries_multi_file': {
         'publication_type': process_client.LAYER_TYPE,
         'expected_name': 's2a_msil2a_20220316t100031_n0400_r122_t33uwr_20220316t134748_tci_10m',
-        'params': {
+        'rest_args': {
+            'do_not_post_name': True,
             'time_regex': r'[0-9]{8}',
             'file_paths': [
                 os.path.join(DIRECTORY, 'timeseries',
@@ -61,45 +52,40 @@ PUBLICATIONS = {
                              'timeseries_tif/S2A_MSIL2A_20220316T100031_N0400_R122_T33UWR_20220316T134748_TCI_10m.tif'),
             ],
         },
-    },
-    'timeseries_compressed_with_chunks': {
-        EnumTestKeys.TYPE: EnumTestTypes.OPTIONAL,
-        'publication_type': process_client.LAYER_TYPE,
-        'expected_name': 'timeseries_tif',
-        'params': {
-            'time_regex': r'[0-9]{8}',
-            'file_paths': [
-                os.path.join(DIRECTORY, 'timeseries',
-                             'timeseries_tif/S2A_MSIL2A_20220319T100731_N0400_R022_T33UWR_20220319T131812_TCI_10m.TIF'),
-                os.path.join(DIRECTORY, 'timeseries',
-                             'timeseries_tif/S2A_MSIL2A_20220316T100031_N0400_R122_T33UWR_20220316T134748_TCI_10m.tif'),
-            ],
-            'compress': True,
-            'compress_settings': process_client.CompressTypeDef(archive_name='timeseries_tif',
-                                                                inner_directory='/timeseries_tif/',
-                                                                file_name=None,
-                                                                ),
-            'with_chunks': True,
+        'mandatory_cases': {
+            frozenset([CompressDomain.FALSE, base_test.WithChunksDomain.FALSE, ]),
         },
-    },
-    'timeseries_compressed': {
-        EnumTestKeys.TYPE: EnumTestTypes.OPTIONAL,
-        'publication_type': process_client.LAYER_TYPE,
-        'expected_name': 's2a_msil2a_20220319t100731_n0400_r022_t33uwr_20220319t131812_tci_10m',
-        'params': {
-            'time_regex': r'[0-9]{8}',
-            'file_paths': [
-                os.path.join(DIRECTORY, 'timeseries',
-                             'timeseries_tif/S2A_MSIL2A_20220319T100731_N0400_R022_T33UWR_20220319T131812_TCI_10m.TIF'),
-            ],
-            'compress': True,
-            'compress_settings': process_client.CompressTypeDef(archive_name='timeseries_tif',
-                                                                inner_directory='/timeseries_tif/',
-                                                                file_name=None,
-                                                                ),
+        'specific_params': {
+            frozenset([CompressDomain.TRUE, base_test.WithChunksDomain.TRUE, ]): {
+                'expected_name': 'data_zip',
+            },
         },
     },
 }
+
+
+def generate_test_cases():
+    tc_list = list()
+    for name, test_case_params in PUBLICATIONS.items():
+        all_params = deepcopy(test_case_params)
+        rest_args = all_params.pop('rest_args')
+        specific_types = {c: EnumTestTypes.MANDATORY for c in all_params.pop('mandatory_cases')}
+        specific_params = all_params.pop('specific_params')
+        test_case = base_test.TestCaseType(key=name,
+                                           publication=lambda params: Publication(
+                                               workspace='dynamic_test_workspace_implicit_name',
+                                               type=params['publication_type'],
+                                               name=params['expected_name']),
+                                           type=EnumTestTypes.OPTIONAL,
+                                           specific_types=specific_types,
+                                           rest_args=rest_args,
+                                           params=all_params,
+                                           specific_params=specific_params,
+                                           marks=[pytest.mark.xfail(reason="Not yet implemented.")]
+                                           if test_case_params.get('xfail') else []
+                                           )
+        tc_list.append(test_case)
+    return tc_list
 
 
 class TestLayer(base_test.TestSingleRestPublication):
@@ -109,28 +95,16 @@ class TestLayer(base_test.TestSingleRestPublication):
     publication_type = None
 
     rest_parametrization = {
-        'method': [
-            base_test.RestMethodType('post_publication', 'post'),
-        ],
+        base_test.WithChunksDomain,
+        CompressDomain,
     }
 
-    test_cases = [base_test.TestCaseType(pytest_id=key,
-                                         key=key,
-                                         publication=Publication(workspace='dynamic_test_workspace_implicit_name',
-                                                                 type=params['publication_type'],
-                                                                 name=params['expected_name']),
-                                         type=params.get(EnumTestKeys.TYPE, EnumTestTypes.MANDATORY),
-                                         params=params,
-                                         marks=[pytest.mark.xfail(reason="Not yet implemented.")]
-                                         if params.get('xfail') else []
-                                         ) for key, params in PUBLICATIONS.items()]
+    test_cases = generate_test_cases()
 
     # pylint: disable=unused-argument
     @staticmethod
-    def test_implicit_name(publication: Publication, key, params, rest_method):
+    def test_implicit_name(publication: Publication, key, params, rest_method, rest_args):
         """Parametrized using pytest_generate_tests"""
-        rest_method(publication, params={**params.get('params', {}),
-                                         'do_not_post_name': True,
-                                         })
+        rest_method(publication, args=rest_args)
 
         asserts_util.is_publication_valid_and_complete(publication)
