0f0fdbe278ac4d5058f5596ead4dbdd62778f14f
Jiri Kozel <jirik@users.noreply.github.com>
2023-01-11 18:00:36 +0100

    Adjust contrast_test.py new rest_parametrization

--- tests/dynamic_data/publications/layer_raster_contrast/contrast_test.py
+++ tests/dynamic_data/publications/layer_raster_contrast/contrast_test.py
@@ -96,7 +96,7 @@ TEST_CASES = {
     },
     'timeseries': {
         EnumTestKeys.TYPE: EnumTestTypes.OPTIONAL,
-        'input': {
+        'rest_args': {
             'file_paths': [
                 'timeseries/S2A_MSIL2A_20220316T100031_N0400_R122_T33UWR_20220316T134748_TCI_10m.tif',
                 'timeseries/S2A_MSIL2A_20220319T100731_N0400_R022_T33UWR_20220319T131812_TCI_10m.TIF',
@@ -122,18 +122,18 @@ TEST_CASES = {
 
 
 def prepare_test_case(key):
-    test_case_def = deepcopy(TEST_CASES[key])
+    all_params = deepcopy(TEST_CASES[key])
 
     base_file_name = key
 
-    rest_input = test_case_def.get('input', {})
-    rest_input.setdefault('file_paths', [f"{base_file_name}.{base_file_name.split('_')[0]}"])
-    rest_input['file_paths'] = [os.path.join(DIRECTORY, fp) for fp in rest_input['file_paths']]
-    test_case_def['input'] = rest_input
+    rest_args = all_params.pop('rest_args', {})
+    rest_args.setdefault('file_paths', [f"{base_file_name}.{base_file_name.split('_')[0]}"])
+    rest_args['file_paths'] = [os.path.join(DIRECTORY, fp) for fp in rest_args['file_paths']]
+    test_type = all_params.pop(EnumTestKeys.TYPE, EnumTestTypes.MANDATORY)
 
-    return base_test.TestCaseType(key=key, type=test_case_def.get(EnumTestKeys.TYPE, EnumTestTypes.MANDATORY), params=test_case_def,
+    return base_test.TestCaseType(key=key, type=test_type, params=all_params, rest_args=rest_args,
                                   marks=[pytest.mark.xfail(reason="Not yet implemented.")]
-                                  if test_case_def.get('xfail') else [],
+                                  if all_params.get('xfail') else [],
                                   )
 
 
@@ -156,18 +156,16 @@ class TestLayer(base_test.TestSingleRestPublication):
 
     test_cases = [prepare_test_case(key) for key in TEST_CASES]
 
-    # pylint: disable=unused-argument
     @staticmethod
-    def test_contrast(layer: Publication, key, params, rest_method):
+    def test_contrast(layer: Publication, params, rest_method, rest_args):
         """Parametrized using pytest_generate_tests"""
 
-        rest_params = params['input']
         expected_input = params['expected_input']
-        for abs_file_path in rest_params['file_paths']:
+        for abs_file_path in rest_args['file_paths']:
             rel_file_path = os.path.relpath(abs_file_path, DIRECTORY)
             assert_input_file(abs_file_path, expected_input.get(rel_file_path, expected_input))
 
-        rest_method(layer, params=rest_params)
+        rest_method(layer, args=rest_args)
 
         assert_util.is_publication_valid_and_complete(layer)
         base_file_name = '_'.join(layer.name.split('_')[1:])
