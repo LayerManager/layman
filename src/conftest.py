2686a0c157f831dfaaa5feb49c9867c157317732
Jiri Kozel <jirik@users.noreply.github.com>
2023-01-17 14:30:58 +0100

    Ensure test DB by fixture

--- src/conftest.py
+++ src/conftest.py
@@ -1,7 +1,8 @@
-from test_tools import process
+from test_tools import process, external_db
 
 
 ensure_layman_session = process.ensure_layman_session
 liferay_mock = process.liferay_mock
 ensure_layman = process.ensure_layman
 ensure_layman_module = process.ensure_layman_module
+ensure_external_db = external_db.ensure_db
--- test_tools/external_db.py
+++ test_tools/external_db.py
@@ -1,3 +1,4 @@
+import pytest
 from db import util as db_util
 from layman import settings, app
 from tests import EXTERNAL_DB_NAME
@@ -5,6 +6,7 @@ from tests import EXTERNAL_DB_NAME
 URI = f'''postgresql://{settings.LAYMAN_PG_USER}:{settings.LAYMAN_PG_PASSWORD}@{settings.LAYMAN_PG_HOST}:{settings.LAYMAN_PG_PORT}/{EXTERNAL_DB_NAME}'''
 
 
+@pytest.fixture(scope="session")
 def ensure_db():
     query = f"SELECT count(*) FROM pg_database WHERE datname = %s"
     statement = f"""CREATE DATABASE {EXTERNAL_DB_NAME} TEMPLATE {settings.LAYMAN_PG_TEMPLATE_DBNAME}"""
@@ -13,6 +15,8 @@ def ensure_db():
         res = db_util.run_query(query, (EXTERNAL_DB_NAME,))
         if not res[0][0] == 1:
             db_util.run_statement(statement)
+
+    yield
 
 
 def ensure_table(schema, name, geo_column):
