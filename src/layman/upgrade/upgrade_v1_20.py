7e7742610d43740bbf4ce5dd61031324db44042a
Jiri Kozel <jirik@users.noreply.github.com>
2023-01-18 14:11:37 +0100

    Fix name of publications table constraint

--- src/layman/upgrade/upgrade_v1_20.py
+++ src/layman/upgrade/upgrade_v1_20.py
@@ -14,6 +14,6 @@ def adjust_db_for_table_uri():
     ALTER TABLE {DB_SCHEMA}.publications ADD COLUMN IF NOT EXISTS table_uri json;'''
     db_util.run_statement(statement)
 
-    statement = f'alter table {DB_SCHEMA}.publications add constraint table_uri_with_publ_type_check CHECK ' \
+    statement = f'alter table {DB_SCHEMA}.publications add constraint table_uri_with_file_type_check CHECK ' \
                 f'(table_uri IS NULL OR file_type = %s);'
     db_util.run_statement(statement, (settings.FILE_TYPE_VECTOR, ))
