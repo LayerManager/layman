389e4467f16c56142eada8cac11efa58f4680a25
Jiri Kozel <jirik@users.noreply.github.com>
2023-01-19 11:26:39 +0100

    Test dangerous geo_column name

--- tests/dynamic_data/publications/layer_external_db/external_db_test.py
+++ tests/dynamic_data/publications/layer_external_db/external_db_test.py
@@ -18,48 +18,56 @@ TEST_CASES = {
         'input_file_name': 'all',
         'schema_name': 'public',
         'table_name': 'all',
+        'geo_column_name': 'wkb_geometry',
         'exp_geometry_type': 'GEOMETRY',
     },
     'geometrycollection_mixed_case_table_name': {
         'input_file_name': 'geometrycollection',
         'schema_name': 'public',
         'table_name': 'MyGeometryCollection',
+        'geo_column_name': 'wkb_geometry',
         'exp_geometry_type': 'GEOMETRYCOLLECTION',
     },
     'linestring_dangerous_table_name': {
         'input_file_name': 'linestring',
         'schema_name': 'public',
         'table_name': DANGEROUS_NAME,
+        'geo_column_name': 'wkb_geometry',
         'exp_geometry_type': 'LINESTRING',
     },
     'multilinestring_dangerous_schema_name': {
         'input_file_name': 'multilinestring',
         'schema_name': DANGEROUS_NAME,
         'table_name': 'multilinestring',
+        'geo_column_name': 'wkb_geometry',
         'exp_geometry_type': 'MULTILINESTRING',
     },
-    'multipoint': {
+    'multipoint_dangerous_geo_column_name': {
         'input_file_name': 'multipoint',
         'schema_name': 'public',
         'table_name': 'multipoint',
+        'geo_column_name': DANGEROUS_NAME,
         'exp_geometry_type': 'MULTIPOINT',
     },
     'multipolygon': {
         'input_file_name': 'multipolygon',
         'schema_name': 'public',
         'table_name': 'multipolygon',
+        'geo_column_name': 'wkb_geometry',
         'exp_geometry_type': 'MULTIPOLYGON',
     },
     'point': {
         'input_file_name': 'point',
         'schema_name': 'public',
         'table_name': 'point',
+        'geo_column_name': 'wkb_geometry',
         'exp_geometry_type': 'POINT',
     },
     'polygon': {
         'input_file_name': 'polygon',
         'schema_name': 'public',
         'table_name': 'polygon',
+        'geo_column_name': 'wkb_geometry',
         'exp_geometry_type': 'POLYGON',
     },
 }
@@ -80,7 +88,7 @@ class TestLayer(base_test.TestSingleRestPublication):
                                              'db_connection': f"{external_db.URI_STR}"
                                                               f"?schema={quote(value['schema_name'])}"
                                                               f"&table={quote(value['table_name'])}"
-                                                              f"&geo_column=wkb_geometry",
+                                                              f"&geo_column={quote(value['geo_column_name'])}",
                                          },
                                          params=value,
                                          ) for key, value in TEST_CASES.items()]
@@ -91,10 +99,11 @@ class TestLayer(base_test.TestSingleRestPublication):
         file_path = f"sample/data/geometry-types/{params['input_file_name']}.geojson"
         schema = params['schema_name']
         table = params['table_name']
-        external_db.import_table(file_path, table=table, schema=schema)
+        geo_column = params['geo_column_name']
+        external_db.import_table(file_path, table=table, schema=schema, geo_column=geo_column)
         conn_cur = db_util.create_connection_cursor(external_db.URI_STR)
         query = f'''select type from geometry_columns where f_table_schema = %s and f_table_name = %s and f_geometry_column = %s'''
-        result = db_util.run_query(query, (schema, table, 'wkb_geometry'), conn_cur=conn_cur)
+        result = db_util.run_query(query, (schema, table, geo_column), conn_cur=conn_cur)
         assert result[0][0] == params['exp_geometry_type']
 
         rest_method(layer, args=rest_args)
