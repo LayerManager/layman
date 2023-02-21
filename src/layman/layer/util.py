8f11d9f47ed44fb6a6c8642088c807f7b6ef705d
index-git <sileny.index@gmail.com>
2023-02-21 16:13:17 +0100

    Add redact_uri method

--- src/layman/layer/util.py
+++ src/layman/layer/util.py
@@ -449,3 +449,14 @@ WHERE
                       )
 
     return result
+
+
+def redact_uri(uri):
+    url_components = parse.urlparse(uri)
+    if url_components.username or url_components.password:
+        hostname = f'{url_components.hostname}:{url_components.port}' if url_components.port else url_components.hostname
+        url_components = url_components._replace(
+            netloc=f"{url_components.username}@{hostname}",
+        )
+
+    return url_components.geturl()
