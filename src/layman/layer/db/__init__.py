ff5e1bddc09ec3f933a64e1904c9da02b623e64a
Jiri Kozel <jirik@users.noreply.github.com>
2023-02-23 13:10:39 +0100

    Require use_internal_srid parameter in better way

--- src/layman/layer/db/__init__.py
+++ src/layman/layer/db/__init__.py
@@ -564,8 +564,7 @@ def get_bbox(schema, table_name, conn_cur=None, column=settings.OGR_DEFAULT_GEOM
     return result
 
 
-def get_table_crs(schema, table_name, conn_cur=None, column=settings.OGR_DEFAULT_GEOMETRY_COLUMN, *, use_internal_srid=None):
-    assert use_internal_srid is not None
+def get_table_crs(schema, table_name, conn_cur=None, column=settings.OGR_DEFAULT_GEOMETRY_COLUMN, *, use_internal_srid):
     srid = get_column_srid(schema, table_name, column, conn_cur=conn_cur)
     crs = db_util.get_crs_from_srid(srid, conn_cur, use_internal_srid=use_internal_srid)
     return crs
