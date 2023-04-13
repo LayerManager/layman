251323c5ee5963e22ee514e290819ef77f5075ab
Jiri Kozel <jirik@users.noreply.github.com>
2023-04-13 11:36:46 +0200

    Add django-allauth plugin

--- deps/wagtail/laymanportal/laymanportal/settings/base.py
+++ deps/wagtail/laymanportal/laymanportal/settings/base.py
@@ -57,6 +57,10 @@ INSTALLED_APPS = [
     "django.contrib.messages",
     "django.contrib.staticfiles",
     "django.contrib.sitemaps",
+    "django.contrib.sites",
+    "allauth",
+    "allauth.account",
+    "allauth.socialaccount",
 ]
 
 MIDDLEWARE = [
@@ -96,6 +100,16 @@ TEMPLATES = [
         },
     },
 ]
+
+AUTHENTICATION_BACKENDS = [
+    # Needed to login by username in Django admin, regardless of `allauth`
+    'django.contrib.auth.backends.ModelBackend',
+
+    # `allauth` specific authentication methods, such as login by e-mail
+    'allauth.account.auth_backends.AuthenticationBackend',
+]
+
+SITE_ID = 1
 
 WSGI_APPLICATION = "laymanportal.wsgi.application"
 
