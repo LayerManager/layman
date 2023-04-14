ed655900db140d97963cdfbb451d6a8e197e7f87
index-git <sileny.index@gmail.com>
2023-04-14 10:53:38 +0200

    Create wfs_wms_status migration

--- src/layman/upgrade/upgrade_v1_21.py
+++ src/layman/upgrade/upgrade_v1_21.py
@@ -2,6 +2,7 @@ import logging
 
 from db import util as db_util
 from layman import settings
+from layman.layer import LAYER_TYPE
 
 logger = logging.getLogger(__name__)
 DB_SCHEMA = settings.LAYMAN_PRIME_SCHEMA
@@ -21,3 +22,18 @@ def adjust_db_for_wfs_wms_status():
         wfs_wms_status {DB_SCHEMA}.enum_wfs_wms_status;'''
 
     db_util.run_statement(statement)
+
+
+def adjust_publications_wfs_wms_status():
+    logger.info(f'    Adjust wfs_wms_status of publications')
+
+    query = f'''update {DB_SCHEMA}.publications set
+    wfs_wms_status = %s
+    where type = %s
+    ;'''
+    params = (settings.EnumWfsWmsStatus.AVAILABLE.value, LAYER_TYPE,)
+    db_util.run_statement(query, params)
+
+    statement = f'alter table {DB_SCHEMA}.publications add constraint wfs_wms_status_with_type_check CHECK ' \
+                f'(wfs_wms_status IS NULL OR type = %s);'
+    db_util.run_statement(statement, (LAYER_TYPE,))
