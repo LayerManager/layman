5bb3313abd4ef769bd5db5770e00aa09ecf375eb
Jiri Kozel <jirik@users.noreply.github.com>
2023-05-05 14:32:19 +0200

    Add new template variables related to ZIP file

--- tests/dynamic_data/publications/wrong_input/util.py
+++ tests/dynamic_data/publications/wrong_input/util.py
@@ -2,7 +2,7 @@ import inspect
 import itertools
 
 from tests import Publication
-from tests.dynamic_data import base_test_classes
+from tests.dynamic_data import base_test, base_test_classes
 
 
 def case_to_simple_parametrizations(case):
@@ -24,11 +24,19 @@ def case_to_simple_parametrizations(case):
     return result
 
 
-def format_exception(exception_info: dict, publication: Publication):
+def format_exception(exception_info: dict, publication: Publication, parametrization: base_test.Parametrization):
+    compress_value = parametrization.rest_arg_dict[base_test.RestArgs.COMPRESS].raw_value
+    chunks_value = parametrization.rest_arg_dict[base_test.RestArgs.WITH_CHUNKS].raw_value
+    zip_file_name = f"{publication.name}.zip" if chunks_value is True else 'temporary_zip_file.zip'
+    zip_path_prefix = f"{zip_file_name}/"
+
     format_variables = {
         'publication_name': publication.name,
         'workspace': publication.workspace,
+        'zip_file_name': zip_file_name if compress_value is True else '',
+        'path_prefix': zip_path_prefix if compress_value is True else '',
     }
+
     if 'data' in exception_info and isinstance(exception_info['data'], dict):
         if 'path' in exception_info['data']:
             exception_info['data']['path'] = exception_info['data']['path'].format(**format_variables)
