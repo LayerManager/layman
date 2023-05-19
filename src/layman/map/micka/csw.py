586b72887819d7ab046ab7a80088673cdbe9de49
index-git <sileny.index@gmail.com>
2023-05-19 11:33:09 +0200

    Encapsulate get_metadata_url in common.micka

--- src/layman/map/micka/csw.py
+++ src/layman/map/micka/csw.py
@@ -45,7 +45,7 @@ def get_map_info(workspace, mapname):
             'metadata': {
                 'identifier': muuid,
                 'csw_url': settings.CSW_PROXY_URL,
-                'record_url': settings.CSW_RECORD_URL.format(identifier=muuid),
+                'record_url': common_util.get_metadata_url(uuid),
                 'comparison_url': url_for('rest_workspace_map_metadata_comparison.get', workspace=workspace, mapname=mapname),
             }
         }
