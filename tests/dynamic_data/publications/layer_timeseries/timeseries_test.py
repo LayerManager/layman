fad6678b90ef42f6a67c6bd668cfdecea77496e3
index-git <sileny.index@gmail.com>
2023-05-26 10:26:51 +0200

    Split timeseries tests to complex and simple

--- tests/dynamic_data/publications/layer_timeseries/timeseries_test.py
+++ tests/dynamic_data/publications/layer_timeseries/timeseries_test.py
@@ -29,6 +29,7 @@ LAYERS = {
                                                                 file_name=None,
                                                                 ),
         },
+        'do_complex_test': True,
         'detail_values': {
             'exp_publication_detail': {
                 'bounding_box': [1737105.4141226907, 6491458.724017749, 1765157.537707582, 6509901.824098258],
@@ -68,6 +69,7 @@ LAYERS = {
                 os.path.join(DIRECTORY, 'timeseries_tif/S2A_MSIL2A_20220319T100731.3.tif'),
             ],
         },
+        'do_complex_test': True,
         'detail_values': {
             'exp_publication_detail': {
                 'bounding_box': [1737105.4141226907, 6491458.724017749, 1765157.537707582, 6509901.824098258],
@@ -114,6 +116,7 @@ LAYERS = {
                                                                 file_name=None,
                                                                 ),
         },
+        'do_complex_test': False,
         'detail_values': {
             'exp_publication_detail': {
                 'bounding_box': [2707260.9569237595, 7740717.799460372, 2708414.90486888, 7741573.954387397],
@@ -134,8 +137,6 @@ LAYERS = {
                 'timeseries_tif/210_long_name_20221031_sample_jpg_aux_rgba_aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa.jpeg',
             ]
         },
-        'expected_thumbnail': os.path.join(DIRECTORY, 'thumbnail_longname.png'),
-        'wms_bbox': [2707260.9569237595, 7740717.799460372, 2708414.90486888, 7741573.954387397],
     },
     'longname_one_file': {
         EnumTestKeys.TYPE: EnumTestTypes.OPTIONAL,
@@ -151,6 +152,7 @@ LAYERS = {
             ],
             'compress': False,
         },
+        'do_complex_test': False,
         'detail_values': {
             'exp_publication_detail': {
                 'bounding_box': [2707260.9569237595, 7740717.799460372, 2708414.90486888, 7741573.954387397],
@@ -169,8 +171,6 @@ LAYERS = {
                 '210_long_name_20221031_sample_jpg_aux_rgba_aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa.jpeg',
             ]
         },
-        'expected_thumbnail': os.path.join(DIRECTORY, 'thumbnail_longname.png'),
-        'wms_bbox': [2707260.9569237595, 7740717.799460372, 2708414.90486888, 7741573.954387397],
     },
     'diacritics_and_spaces_zip': {
         EnumTestKeys.TYPE: EnumTestTypes.OPTIONAL,
@@ -186,6 +186,7 @@ LAYERS = {
                                                                 file_name=None,
                                                                 ),
         },
+        'do_complex_test': False,
         'detail_values': {
             'exp_publication_detail': {
                 'bounding_box': [1737105.4141226907, 6491458.724017749, 1765157.537707582, 6509901.824098258],
@@ -208,8 +209,6 @@ LAYERS = {
                 'timeseries_tif/Cerekvice nad Bystřicí 20220319.TIF',
             ]
         },
-        'expected_thumbnail': os.path.join(DIRECTORY, 'thumbnail_timeseries.png'),
-        'wms_bbox': [1743913.19942603237, 6499107.284021802247, 1755465.937341974815, 6503948.597792930901, ],
     },
     'diacritics_and_spaces': {
         EnumTestKeys.TYPE: EnumTestTypes.OPTIONAL,
@@ -221,6 +220,7 @@ LAYERS = {
             ],
             'compress': False,
         },
+        'do_complex_test': False,
         'detail_values': {
             'exp_publication_detail': {
                 'bounding_box': [1737105.4141226907, 6491458.724017749, 1765157.537707582, 6509901.824098258],
@@ -241,8 +241,6 @@ LAYERS = {
                 'Cerekvice_nad_Bystrici_20220319.TIF',
             ]
         },
-        'expected_thumbnail': os.path.join(DIRECTORY, 'thumbnail_timeseries.png'),
-        'wms_bbox': [1743913.19942603237, 6499107.284021802247, 1755465.937341974815, 6503948.597792930901, ],
     },
 }
 
@@ -286,51 +284,52 @@ class TestLayer(base_test.TestSingleRestPublication):
                                                        **params.get('detail_values', {}),
                                                        )
 
-        time_snaps = [time_snap[:10] for time_snap in params['detail_values']['exp_publication_detail']['wms']['time']['values']]
-        for time in time_snaps:
-            exp_wms = os.path.join(DIRECTORY, f"wms_{time}.png")
-            asserts_publ.geoserver.wms_spatial_precision(layer.workspace, layer.type, layer.name, crs=crs_def.EPSG_3857,
-                                                         extent=params['wms_bbox'],
-                                                         img_size=(1322, 554),
-                                                         wms_version='1.3.0',
-                                                         pixel_diff_limit=200,
-                                                         obtained_file_path=f'tmp/artifacts/test_timeseries/downloaded_wms_{layer.name}_{time}.png',
-                                                         expected_file_path=exp_wms,
-                                                         time=time,
-                                                         )
+        if params['do_complex_test']:
+            time_snaps = [time_snap[:10] for time_snap in params['detail_values']['exp_publication_detail']['wms']['time']['values']]
+            for time in time_snaps:
+                exp_wms = os.path.join(DIRECTORY, f"wms_{time}.png")
+                asserts_publ.geoserver.wms_spatial_precision(layer.workspace, layer.type, layer.name, crs=crs_def.EPSG_3857,
+                                                             extent=params['wms_bbox'],
+                                                             img_size=(1322, 554),
+                                                             wms_version='1.3.0',
+                                                             pixel_diff_limit=200,
+                                                             obtained_file_path=f'tmp/artifacts/test_timeseries/downloaded_wms_{layer.name}_{time}.png',
+                                                             expected_file_path=exp_wms,
+                                                             time=time,
+                                                             )
 
-        exp_thumbnail_file = params.get('expected_thumbnail')
-        if exp_thumbnail_file is not None:
-            exp_thumbnail = os.path.join(DIRECTORY, exp_thumbnail_file)
-            asserts_publ.internal.thumbnail_equals(layer.workspace, layer.type, layer.name, exp_thumbnail, max_diffs=1)
+            exp_thumbnail_file = params.get('expected_thumbnail')
+            if exp_thumbnail_file is not None:
+                exp_thumbnail = os.path.join(DIRECTORY, exp_thumbnail_file)
+                asserts_publ.internal.thumbnail_equals(layer.workspace, layer.type, layer.name, exp_thumbnail, max_diffs=1)
 
-        if rest_method == self.post_publication:  # pylint: disable=W0143
-            http_method = common.REQUEST_METHOD_POST
-        elif rest_method == self.patch_publication:  # pylint: disable=W0143
-            http_method = common.REQUEST_METHOD_PATCH
-        else:
-            raise NotImplementedError(f"Unknown rest_method: {rest_method}")
+            if rest_method == self.post_publication:  # pylint: disable=W0143
+                http_method = common.REQUEST_METHOD_POST
+            elif rest_method == self.patch_publication:  # pylint: disable=W0143
+                http_method = common.REQUEST_METHOD_PATCH
+            else:
+                raise NotImplementedError(f"Unknown rest_method: {rest_method}")
 
-        asserts_publ.metadata.correct_values_in_layer_metadata(layer.workspace, layer.type, layer.name, http_method=http_method)
+            asserts_publ.metadata.correct_values_in_layer_metadata(layer.workspace, layer.type, layer.name, http_method=http_method)
 
-        process_client.patch_workspace_layer(layer.workspace,
-                                             layer.name,
-                                             title='Title: ' + layer.name)
-        asserts_util.is_publication_valid_and_complete(layer)
+            process_client.patch_workspace_layer(layer.workspace,
+                                                 layer.name,
+                                                 title='Title: ' + layer.name)
+            asserts_util.is_publication_valid_and_complete(layer)
 
-        process_client.patch_workspace_layer(layer.workspace,
-                                             layer.name,
-                                             file_paths=['sample/layman.layer/small_layer.geojson'],
-                                             title=layer.name)
-        asserts_util.is_publication_valid_and_complete(layer)
+            process_client.patch_workspace_layer(layer.workspace,
+                                                 layer.name,
+                                                 file_paths=['sample/layman.layer/small_layer.geojson'],
+                                                 title=layer.name)
+            asserts_util.is_publication_valid_and_complete(layer)
 
-        asserts_publ.internal.correct_values_in_detail(layer.workspace, layer.type, layer.name,
-                                                       exp_publication_detail={
-                                                           'bounding_box': [1571204.369948366, 6268896.225570714, 1572590.854206196,
-                                                                            6269876.335616991],
-                                                           'native_crs': 'EPSG:4326',
-                                                           'native_bounding_box': [14.114369, 48.964832, 14.126824, 48.970612],
-                                                       },
-                                                       file_extension='geojson',
-                                                       publ_type_detail=('vector', 'sld'),
-                                                       )
+            asserts_publ.internal.correct_values_in_detail(layer.workspace, layer.type, layer.name,
+                                                           exp_publication_detail={
+                                                               'bounding_box': [1571204.369948366, 6268896.225570714, 1572590.854206196,
+                                                                                6269876.335616991],
+                                                               'native_crs': 'EPSG:4326',
+                                                               'native_bounding_box': [14.114369, 48.964832, 14.126824, 48.970612],
+                                                           },
+                                                           file_extension='geojson',
+                                                           publ_type_detail=('vector', 'sld'),
+                                                           )
