2e3571d24d9af41baa8fd09d606face6fd950cb2
Jiri Kozel <jirik@users.noreply.github.com>
2023-05-26 12:38:12 +0200

    Use new bbox signature

--- tests/dynamic_data/publications/multi_publications/test_multiendpoints.py
+++ tests/dynamic_data/publications/multi_publications/test_multiendpoints.py
@@ -17,58 +17,64 @@ WORKSPACE2 = 'test_select_publications_complex_workspace2'
 WORKSPACE3 = 'test_select_publications_complex_workspace3'
 WORKSPACES = [WORKSPACE1, WORKSPACE2, WORKSPACE3]
 
-MAP_1E_2_4X6_6 = Publication(WORKSPACE1, process_client.MAP_TYPE, 'test_select_publications_map1e')
-MAP_1E_3_3X3_3 = Publication(WORKSPACE1, process_client.MAP_TYPE, 'test_select_publications_map1e_3_3x3_3')
-MAP_1O_2_2X3_6 = Publication(WORKSPACE1, process_client.MAP_TYPE, 'test_select_publications_map1o')
-MAP_1OE_3_7X5_9 = Publication(WORKSPACE1, process_client.MAP_TYPE, 'test_select_publications_map1oe')
-MAP_2E_3_3X5_5 = Publication(WORKSPACE2, process_client.MAP_TYPE, 'test_select_publications_map2e')
-MAP_2O_2_2X4_4 = Publication(WORKSPACE2, process_client.MAP_TYPE, 'test_select_publications_map2o')
-MAP_3O_NULL = Publication(WORKSPACE3, process_client.MAP_TYPE, 'test_select_publications_map3o')
+MAP_1E_BBOX_BF46 = Publication(WORKSPACE1, process_client.MAP_TYPE, 'test_select_publications_map1e')
+MAP_1E_BBOX_C3 = Publication(WORKSPACE1, process_client.MAP_TYPE, 'test_select_publications_map1e_3_3x3_3')
+MAP_1O_BBOX_BC26 = Publication(WORKSPACE1, process_client.MAP_TYPE, 'test_select_publications_map1o')
+MAP_1OE_BBOX_CE79 = Publication(WORKSPACE1, process_client.MAP_TYPE, 'test_select_publications_map1oe')
+MAP_2E_BBOX_CE35 = Publication(WORKSPACE2, process_client.MAP_TYPE, 'test_select_publications_map2e')
+MAP_2O_BBOX_BD24 = Publication(WORKSPACE2, process_client.MAP_TYPE, 'test_select_publications_map2o')
+MAP_3O_BBOX_EMPTY = Publication(WORKSPACE3, process_client.MAP_TYPE, 'test_select_publications_map3o')
 
 PUBLICATIONS = {
-    MAP_1E_2_4X6_6: {
+    MAP_1E_BBOX_BF46: {
         'title': 'Příliš žluťoučký Kůň úpěl ďábelské ódy',
         'access_rights': {'read': {settings.RIGHTS_EVERYONE_ROLE},
                           'write': {settings.RIGHTS_EVERYONE_ROLE}},
+        # rect
         'bbox': (1842000, 6324000, 1846000, 6326000),
         'crs': crs_def.EPSG_3857,
     },
-    MAP_1E_3_3X3_3: {
+    MAP_1E_BBOX_C3: {
         'title': 'Jednobodová vrstva Kodaň',
         'access_rights': {'read': {settings.RIGHTS_EVERYONE_ROLE},
                           'write': {settings.RIGHTS_EVERYONE_ROLE}},
+        # point 1843000 6323000
         'bbox': (16.55595068632278, 49.28289621550056, 16.55595068632278, 49.28289621550056),
         'crs': crs_def.EPSG_4326,
     },
-    MAP_1O_2_2X3_6: {
+    MAP_1O_BBOX_BC26: {
         'title': 'Ďůlek kun Karel',
         'access_rights': {'read': {WORKSPACE1},
                           'write': {WORKSPACE1}},
+        # rect 1842011 6321892 1842988 6326107
         'bbox': (-601597.1407428421, -1151286.9441679057, -600665.4471820187, -1148625.2345234894),
         'crs': crs_def.EPSG_5514,
     },
-    MAP_1OE_3_7X5_9: {
+    MAP_1OE_BBOX_CE79: {
         'title': 'jedna dva tři čtyři kód',
         'access_rights': {'read': {settings.RIGHTS_EVERYONE_ROLE},
                           'write': {WORKSPACE1}},
+        # rect 1842958 6327000 1845041 6328999
         'bbox': (613077.7082822081, 5462674.538979379, 614410.4777841105, 5464003.656058598),
         'crs': crs_def.EPSG_32633,
     },
-    MAP_2E_3_3X5_5: {
+    MAP_2E_BBOX_CE35: {
         'title': 'Svíčky is the best game',
         'access_rights': {'read': {settings.RIGHTS_EVERYONE_ROLE},
                           'write': {settings.RIGHTS_EVERYONE_ROLE}},
+        # rect 1843006 6322882 1844993 6325117
         'bbox': (176844.09626803786, 5468335.761457844, 178226.55642100016, 5469714.707582928),
         'crs': crs_def.EPSG_32634,
     },
-    MAP_2O_2_2X4_4: {
+    MAP_2O_BBOX_BD24: {
         'title': 'druhá mapa JeDnA óda',
         'access_rights': {'read': {WORKSPACE2},
                           'write': {WORKSPACE2}},
+        # rect 1841824 6322015 1844176 6323984
         'bbox': (4459469.209282893, 2527850.676486253, 4460838.491401795, 2529216.681901061),
         'crs': crs_def.EPSG_3034,
     },
-    MAP_3O_NULL: {
+    MAP_3O_BBOX_EMPTY: {
         'title': 'Nullový bounding box',
         'access_rights': {'read': {WORKSPACE3},
                           'write': {WORKSPACE3}},
@@ -79,75 +85,75 @@ PUBLICATIONS = {
 
 INTERNAL_TEST_CASES = [
     ({}, {
-        'items': [MAP_1E_2_4X6_6,
-                  MAP_1E_3_3X3_3,
-                  MAP_1O_2_2X3_6,
-                  MAP_1OE_3_7X5_9,
-                  MAP_2E_3_3X5_5,
-                  MAP_2O_2_2X4_4,
-                  MAP_3O_NULL,
+        'items': [MAP_1E_BBOX_BF46,
+                  MAP_1E_BBOX_C3,
+                  MAP_1O_BBOX_BC26,
+                  MAP_1OE_BBOX_CE79,
+                  MAP_2E_BBOX_CE35,
+                  MAP_2O_BBOX_BD24,
+                  MAP_3O_BBOX_EMPTY,
                   ],
         'total_count': 7,
         'content_range': (1, 7),
     }),
     ({'reader': settings.ANONYM_USER}, {
-        'items': [MAP_1E_2_4X6_6,
-                  MAP_1E_3_3X3_3,
-                  MAP_1OE_3_7X5_9,
-                  MAP_2E_3_3X5_5,
+        'items': [MAP_1E_BBOX_BF46,
+                  MAP_1E_BBOX_C3,
+                  MAP_1OE_BBOX_CE79,
+                  MAP_2E_BBOX_CE35,
                   ],
         'total_count': 4,
         'content_range': (1, 4),
     }),
     ({'reader': WORKSPACE2}, {
-        'items': [MAP_1E_2_4X6_6,
-                  MAP_1E_3_3X3_3,
-                  MAP_1OE_3_7X5_9,
-                  MAP_2E_3_3X5_5,
-                  MAP_2O_2_2X4_4,
+        'items': [MAP_1E_BBOX_BF46,
+                  MAP_1E_BBOX_C3,
+                  MAP_1OE_BBOX_CE79,
+                  MAP_2E_BBOX_CE35,
+                  MAP_2O_BBOX_BD24,
                   ],
         'total_count': 5,
         'content_range': (1, 5),
     }),
     ({'writer': settings.ANONYM_USER}, {
-        'items': [MAP_1E_2_4X6_6,
-                  MAP_1E_3_3X3_3,
-                  MAP_2E_3_3X5_5,
+        'items': [MAP_1E_BBOX_BF46,
+                  MAP_1E_BBOX_C3,
+                  MAP_2E_BBOX_CE35,
                   ],
         'total_count': 3,
         'content_range': (1, 3),
     }),
     ({'writer': WORKSPACE2}, {
-        'items': [MAP_1E_2_4X6_6,
-                  MAP_1E_3_3X3_3,
-                  MAP_2E_3_3X5_5,
-                  MAP_2O_2_2X4_4,
+        'items': [MAP_1E_BBOX_BF46,
+                  MAP_1E_BBOX_C3,
+                  MAP_2E_BBOX_CE35,
+                  MAP_2O_BBOX_BD24,
                   ],
         'total_count': 4,
         'content_range': (1, 4),
     }),
     ({'full_text_filter': 'dva'}, {
-        'items': [MAP_1OE_3_7X5_9,
+        'items': [MAP_1OE_BBOX_CE79,
                   ],
         'total_count': 1,
         'content_range': (1, 1),
     }),
     ({'full_text_filter': 'games'}, {
-        'items': [MAP_2E_3_3X5_5,
+        'items': [MAP_2E_BBOX_CE35,
                   ],
         'total_count': 1,
         'content_range': (1, 1),
     }),
     ({'full_text_filter': 'kun'}, {
-        'items': [MAP_1E_2_4X6_6,
-                  MAP_1O_2_2X3_6,
+        'items': [MAP_1E_BBOX_BF46,
+                  MAP_1O_BBOX_BC26,
                   ],
         'total_count': 2,
         'content_range': (1, 2),
     }),
     ({'full_text_filter': 'jedna'}, {
-        'items': [MAP_1OE_3_7X5_9,
-                  MAP_2O_2_2X4_4,
+        'items': [MAP_1OE_BBOX_CE79,
+                  MAP_2O_BBOX_BD24,
                   ],
         'total_count': 2,
         'content_range': (1, 2),
@@ -158,201 +164,202 @@ INTERNAL_TEST_CASES = [
         'content_range': (0, 0),
     }),
     ({'full_text_filter': 'dva kun'}, {
-        'items': [MAP_1E_2_4X6_6,
-                  MAP_1O_2_2X3_6,
-                  MAP_1OE_3_7X5_9,
+        'items': [MAP_1E_BBOX_BF46,
+                  MAP_1O_BBOX_BC26,
+                  MAP_1OE_BBOX_CE79,
                   ],
         'total_count': 3,
         'content_range': (1, 3),
     }),
     ({'full_text_filter': 'dn'}, {
-        'items': [MAP_1E_3_3X3_3,
-                  MAP_1OE_3_7X5_9,
-                  MAP_2O_2_2X4_4,
+        'items': [MAP_1E_BBOX_C3,
+                  MAP_1OE_BBOX_CE79,
+                  MAP_2O_BBOX_BD24,
                   ],
         'total_count': 3,
         'content_range': (1, 3),
     }),
     ({'full_text_filter': 'oda', 'ordering_full_text': 'oda', 'order_by_list': ['full_text'], }, {
-        'items': [MAP_2O_2_2X4_4,
-                  MAP_1E_3_3X3_3,
+        'items': [MAP_2O_BBOX_BD24,
+                  MAP_1E_BBOX_C3,
                   ],
         'total_count': 2,
         'content_range': (1, 2),
     }),
     ({'full_text_filter': 'va kód', 'ordering_full_text': 'va kód', 'order_by_list': ['full_text'], }, {
-        'items': [MAP_1OE_3_7X5_9,
-                  MAP_1E_3_3X3_3,
+        'items': [MAP_1OE_BBOX_CE79,
+                  MAP_1E_BBOX_C3,
                   ],
         'total_count': 2,
         'content_range': (1, 2),
     }),
     ({'order_by_list': ['full_text'], 'ordering_full_text': 'jedna'}, {
-        'items': [MAP_1OE_3_7X5_9,
-                  MAP_2O_2_2X4_4,
-                  MAP_1E_2_4X6_6,
-                  MAP_1E_3_3X3_3,
-                  MAP_1O_2_2X3_6,
-                  MAP_2E_3_3X5_5,
-                  MAP_3O_NULL,
+        'items': [MAP_1OE_BBOX_CE79,
+                  MAP_2O_BBOX_BD24,
+                  MAP_1E_BBOX_BF46,
+                  MAP_1E_BBOX_C3,
+                  MAP_1O_BBOX_BC26,
+                  MAP_2E_BBOX_CE35,
+                  MAP_3O_BBOX_EMPTY,
                   ],
         'total_count': 7,
         'content_range': (1, 7),
     }),
     ({'full_text_filter': 'dva kun', 'order_by_list': ['full_text'], 'ordering_full_text': 'karel kun'}, {
-        'items': [MAP_1O_2_2X3_6,
-                  MAP_1E_2_4X6_6,
-                  MAP_1OE_3_7X5_9,
+        'items': [MAP_1O_BBOX_BC26,
+                  MAP_1E_BBOX_BF46,
+                  MAP_1OE_BBOX_CE79,
                   ],
         'total_count': 3,
         'content_range': (1, 3),
     }),
     ({'order_by_list': ['title'], }, {
-        'items': [MAP_2O_2_2X4_4,
-                  MAP_1O_2_2X3_6,
-                  MAP_1OE_3_7X5_9,
-                  MAP_1E_3_3X3_3,
-                  MAP_3O_NULL,
-                  MAP_1E_2_4X6_6,
-                  MAP_2E_3_3X5_5,
+        'items': [MAP_2O_BBOX_BD24,
+                  MAP_1O_BBOX_BC26,
+                  MAP_1OE_BBOX_CE79,
+                  MAP_1E_BBOX_C3,
+                  MAP_3O_BBOX_EMPTY,
+                  MAP_1E_BBOX_BF46,
+                  MAP_2E_BBOX_CE35,
                   ],
         'total_count': 7,
         'content_range': (1, 7),
     }),
     ({'order_by_list': ['last_change'], }, {
-        'items': [MAP_3O_NULL,
-                  MAP_2O_2_2X4_4,
-                  MAP_2E_3_3X5_5,
-                  MAP_1OE_3_7X5_9,
-                  MAP_1O_2_2X3_6,
-                  MAP_1E_3_3X3_3,
-                  MAP_1E_2_4X6_6,
+        'items': [MAP_3O_BBOX_EMPTY,
+                  MAP_2O_BBOX_BD24,
+                  MAP_2E_BBOX_CE35,
+                  MAP_1OE_BBOX_CE79,
+                  MAP_1O_BBOX_BC26,
+                  MAP_1E_BBOX_C3,
+                  MAP_1E_BBOX_BF46,
                   ],
         'total_count': 7,
         'content_range': (1, 7),
     }),
     ({'order_by_list': ['bbox'],
-      'ordering_bbox': (1842999, 6322999, 1845001, 6325001),
+      'ordering_bbox': (1842999, 6322999, 1845001, 6325001),  # BBOX CE35
       'ordering_bbox_crs': crs_def.EPSG_3857,
-      }, {'items': [MAP_2E_3_3X5_5,
-                    MAP_1E_2_4X6_6,
-                    MAP_2O_2_2X4_4,
-                    MAP_1O_2_2X3_6,
-                    MAP_1E_3_3X3_3,
-                    MAP_1OE_3_7X5_9,
-                    MAP_3O_NULL,
+      }, {'items': [MAP_2E_BBOX_CE35,   # area 4
+                    MAP_1E_BBOX_BF46,   # area 1
+                    MAP_2O_BBOX_BD24,   # area 1
+                    MAP_1O_BBOX_BC26,   # area 0, line 2
+                    MAP_1E_BBOX_C3,     # area 0, point
+                    MAP_1OE_BBOX_CE79,  # no intersection
+                    MAP_3O_BBOX_EMPTY,  # no intersection
                     ],
           'total_count': 7,
           'content_range': (1, 7),
           }),
     ({'order_by_list': ['bbox'],
-      'ordering_bbox': (16.5559417, 49.2828904, 16.573926, 49.2946205),
+      'ordering_bbox': (16.5559417, 49.2828904, 16.573926, 49.2946205),  # BBOX CE35
       # EPSG:3857 (1842999, 6322999, 1845001, 6325001)
       'ordering_bbox_crs': crs_def.EPSG_4326,
-      }, {'items': [MAP_2E_3_3X5_5,
-                    MAP_1E_2_4X6_6,
-                    MAP_2O_2_2X4_4,
-                    MAP_1E_3_3X3_3,
-                    MAP_1O_2_2X3_6,
-                    MAP_1OE_3_7X5_9,
-                    MAP_3O_NULL,
+      }, {'items': [MAP_2E_BBOX_CE35,   # area 4
+                    MAP_1E_BBOX_BF46,   # area 1
+                    MAP_2O_BBOX_BD24,   # area 1
+                    MAP_1E_BBOX_C3,     # area 0, point
+                    MAP_1O_BBOX_BC26,   # area 0, line 2
+                    MAP_1OE_BBOX_CE79,  # no intersection
+                    MAP_3O_BBOX_EMPTY,  # no intersection
                     ],
           'total_count': 7,
           'content_range': (1, 7),
           }),
     ({'order_by_list': ['bbox'],
-      'ordering_bbox': (-600879.43, -1150642.64, -599437.98, -1149487.13),
+      'ordering_bbox': (-600879.43, -1150642.64, -599437.98, -1149487.13),  # BBOX CE35
       # EPSG:3857 (1842999, 6322999, 1845001, 6325001)
       'ordering_bbox_crs': crs_def.EPSG_5514,
-      }, {'items': [MAP_2E_3_3X5_5,
-                    MAP_1E_2_4X6_6,
-                    MAP_2O_2_2X4_4,
-                    MAP_1O_2_2X3_6,
-                    MAP_1E_3_3X3_3,
-                    MAP_1OE_3_7X5_9,
-                    MAP_3O_NULL,
+      }, {'items': [MAP_2E_BBOX_CE35,   # area 4
+                    MAP_1E_BBOX_BF46,   # area 1
+                    MAP_2O_BBOX_BD24,   # area 1
+                    MAP_1O_BBOX_BC26,   # area 0, line 2
+                    MAP_1E_BBOX_C3,     # area 0, point
+                    MAP_1OE_BBOX_CE79,  # no intersection
+                    MAP_3O_BBOX_EMPTY,  # no intersection
                     ],
           'total_count': 7,
           'content_range': (1, 7),
           }),
     ({'order_by_list': ['bbox'],
-      'ordering_bbox': (1844001, 6324001, 1844001, 6324001),
+      'ordering_bbox': (1844001, 6324001, 1844001, 6324001),  # BBOX D4
       'ordering_bbox_crs': crs_def.EPSG_3857,
-      'bbox_filter': (1844001, 6324001, 1844001, 6324001),
+      'bbox_filter': (1844001, 6324001, 1844001, 6324001),  # BBOX D4
       'bbox_filter_crs': crs_def.EPSG_3857,
-      }, {'items': [MAP_2E_3_3X5_5,
-                    MAP_2O_2_2X4_4,
-                    MAP_1E_2_4X6_6,
+      }, {'items': [MAP_2E_BBOX_CE35,  # area 0, point
+                    MAP_2O_BBOX_BD24,  # area 0, point
+                    MAP_1E_BBOX_BF46,  # area 0, point
                     ],
           'total_count': 3,
           'content_range': (1, 3),
           }),
-    ({'bbox_filter': (-600665, -1150641, -599439, -1149488),  # EPSG:3857 (1843001, 6323001, 1844999, 6324999)
+    ({'bbox_filter': (-600665, -1150641, -599439, -1149488),  # BBOX CE(35)
+      # EPSG:3857 (1843001, 6323001, 1844999, 6324999)
       'bbox_filter_crs': crs_def.EPSG_5514,
-      }, {'items': [MAP_1E_2_4X6_6,
-                    MAP_2E_3_3X5_5,
-                    MAP_2O_2_2X4_4,
+      }, {'items': [MAP_1E_BBOX_BF46,  # area 1
+                    MAP_2E_BBOX_CE35,  # area 4
+                    MAP_2O_BBOX_BD24,  # area 1
                     ],
           'total_count': 3,
           'content_range': (1, 3),
           }),
-    ({'bbox_filter': (176844.09626803786, 5468335.761457844, 176844.09626803786, 5468335.7614578441),
+    ({'bbox_filter': (176844.09626803786, 5468335.761457844, 176844.09626803786, 5468335.7614578441),  # BBOX C3
       # EPSG:3857 (1843001, 6323001, 1843001, 6323001)
       'bbox_filter_crs': crs_def.EPSG_32634,
-      }, {'items': [MAP_1O_2_2X3_6,
-                    MAP_2E_3_3X5_5,
-                    MAP_2O_2_2X4_4,
+      }, {'items': [MAP_1O_BBOX_BC26,  # area 0, point
+                    MAP_2E_BBOX_CE35,  # area 0, point
+                    MAP_2O_BBOX_BD24,  # area 0, point
                     ],
           'total_count': 3,
           'content_range': (1, 3),
           }),
     ({'limit': 2}, {
-        'items': [MAP_1E_2_4X6_6,
-                  MAP_1E_3_3X3_3,
-                  # MAP_1O_2_2X3_6,
-                  # MAP_1OE_3_7X5_9,
-                  # MAP_2E_3_3X5_5,
-                  # MAP_2O_2_2X4_4,
-                  # MAP_3O_NULL,
+        'items': [MAP_1E_BBOX_BF46,
+                  MAP_1E_BBOX_C3,
+                  # MAP_1O_BBOX_BC26,
+                  # MAP_1OE_BBOX_CE79,
+                  # MAP_2E_BBOX_CE35,
+                  # MAP_2O_BBOX_BD24,
+                  # MAP_3O_BBOX_EMPTY,
                   ],
         'total_count': 7,
         'content_range': (1, 2),
     }),
     ({'offset': 2}, {
         'items': [
-            # MAP_1E_2_4X6_6,
-            # MAP_1E_3_3X3_3,
-            MAP_1O_2_2X3_6,
-            MAP_1OE_3_7X5_9,
-            MAP_2E_3_3X5_5,
-            MAP_2O_2_2X4_4,
-            MAP_3O_NULL,
+            # MAP_1E_BBOX_BF46,
+            # MAP_1E_BBOX_C3,
+            MAP_1O_BBOX_BC26,
+            MAP_1OE_BBOX_CE79,
+            MAP_2E_BBOX_CE35,
+            MAP_2O_BBOX_BD24,
+            MAP_3O_BBOX_EMPTY,
         ],
         'total_count': 7,
         'content_range': (3, 7),
     }),
     ({'limit': 5, 'offset': 5}, {
         'items': [
-            # MAP_1E_2_4X6_6,
-            # MAP_1E_3_3X3_3,
-            # MAP_1O_2_2X3_6,
-            # MAP_1OE_3_7X5_9,
-            # MAP_2E_3_3X5_5,
-            MAP_2O_2_2X4_4,
-            MAP_3O_NULL,
+            # MAP_1E_BBOX_BF46,
+            # MAP_1E_BBOX_C3,
+            # MAP_1O_BBOX_BC26,
+            # MAP_1OE_BBOX_CE79,
+            # MAP_2E_BBOX_CE35,
+            MAP_2O_BBOX_BD24,
+            MAP_3O_BBOX_EMPTY,
         ],
         'total_count': 7,
         'content_range': (6, 7),
     }),
     ({'limit': 2, 'offset': 2}, {
         'items': [
-            # MAP_1E_2_4X6_6,
-            # MAP_1E_3_3X3_3,
-            MAP_1O_2_2X3_6,
-            MAP_1OE_3_7X5_9,
-            # MAP_2E_3_3X5_5,
-            # MAP_2O_2_2X4_4,
-            # MAP_3O_NULL,
+            # MAP_1E_BBOX_BF46,
+            # MAP_1E_BBOX_C3,
+            MAP_1O_BBOX_BC26,
+            MAP_1OE_BBOX_CE79,
+            # MAP_2E_BBOX_CE35,
+            # MAP_2O_BBOX_BD24,
+            # MAP_3O_BBOX_EMPTY,
         ],
         'total_count': 7,
         'content_range': (3, 4),
@@ -367,10 +374,10 @@ INTERNAL_TEST_CASES = [
 
 REST_TEST_CASES = [
     ({}, {
-        'items': [MAP_1E_2_4X6_6,
-                  MAP_1E_3_3X3_3,
-                  MAP_1OE_3_7X5_9,
-                  MAP_2E_3_3X5_5,
+        'items': [MAP_1E_BBOX_BF46,
+                  MAP_1E_BBOX_C3,
+                  MAP_1OE_BBOX_CE79,
+                  MAP_2E_BBOX_CE35,
                   ],
         'total_count': 4,
         'content_range': (1, 4),
