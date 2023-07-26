93c39714edcb7b70b6a649f5178be526aaffc94e
Jiri Kozel <jirik@users.noreply.github.com>
2023-07-26 11:19:07 +0200

    Remove authn.txt files

--- src/layman/upgrade/__init__.py
+++ src/layman/upgrade/__init__.py
@@ -61,6 +61,7 @@ MIGRATIONS = {
         ((1, 22, 0), [
             upgrade_v1_22.ensure_issuer_sub_uniqueness,
             upgrade_v1_22.fix_issuer_id,
+            upgrade_v1_22.remove_authn_txt_files,
         ]),
     ],
 }
