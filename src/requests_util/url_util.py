850fd226730d6d21e1011be7e479019220b111ff
index-git <sileny.index@gmail.com>
2023-12-12 15:51:48 +0100

    Enable redact_uri with remove_user

--- src/requests_util/url_util.py
+++ src/requests_util/url_util.py
@@ -1,12 +1,12 @@
 from urllib import parse
 
 
-def redact_uri(uri):
+def redact_uri(uri, *, remove_username=False):
     url_components = parse.urlparse(uri)
     if url_components.username or url_components.password:
         hostname = f'{url_components.hostname}:{url_components.port}' if url_components.port else url_components.hostname
         url_components = url_components._replace(
-            netloc=f"{url_components.username}@{hostname}",
+            netloc=f"{url_components.username}@{hostname}" if not remove_username else f"{hostname}",
         )
 
     return url_components.geturl()
--- src/requests_util/url_util_test.py
+++ src/requests_util/url_util_test.py
@@ -3,16 +3,24 @@ import pytest
 import requests_util.url_util
 
 
-@pytest.mark.parametrize('uri, exp_redact_uri', [
-    ('mysql://docker:docker@postgresql:5432/external_test_db?table=table_name&geo_column=wkb_geometry',
+@pytest.mark.parametrize('uri, remove_username, exp_redact_uri', [
+    ('mysql://docker:docker@postgresql:5432/external_test_db?table=table_name&geo_column=wkb_geometry', False,
      'mysql://docker@postgresql:5432/external_test_db?table=table_name&geo_column=wkb_geometry'),
-    ('mysql://docker:@postgresql:5432/external_test_db?table=table_name&geo_column=wkb_geometry',
+    ('mysql://docker:@postgresql:5432/external_test_db?table=table_name&geo_column=wkb_geometry', False,
      'mysql://docker@postgresql:5432/external_test_db?table=table_name&geo_column=wkb_geometry'),
-    ('mysql://docker@postgresql:5432/external_test_db?table=table_name&geo_column=wkb_geometry',
+    ('mysql://docker@postgresql:5432/external_test_db?table=table_name&geo_column=wkb_geometry', False,
      'mysql://docker@postgresql:5432/external_test_db?table=table_name&geo_column=wkb_geometry'),
-    ('mysql://postgresql:5432/external_test_db?table=table_name&geo_column=wkb_geometry',
+    ('mysql://postgresql:5432/external_test_db?table=table_name&geo_column=wkb_geometry', False,
+     'mysql://postgresql:5432/external_test_db?table=table_name&geo_column=wkb_geometry'),
+    ('mysql://docker:docker@postgresql:5432/external_test_db?table=table_name&geo_column=wkb_geometry', True,
+     'mysql://postgresql:5432/external_test_db?table=table_name&geo_column=wkb_geometry'),
+    ('mysql://docker:@postgresql:5432/external_test_db?table=table_name&geo_column=wkb_geometry', True,
+     'mysql://postgresql:5432/external_test_db?table=table_name&geo_column=wkb_geometry'),
+    ('mysql://docker@postgresql:5432/external_test_db?table=table_name&geo_column=wkb_geometry', True,
+     'mysql://postgresql:5432/external_test_db?table=table_name&geo_column=wkb_geometry'),
+    ('mysql://postgresql:5432/external_test_db?table=table_name&geo_column=wkb_geometry', True,
      'mysql://postgresql:5432/external_test_db?table=table_name&geo_column=wkb_geometry'),
 ])
-def test_redact_uri(uri, exp_redact_uri):
-    redact_table_uri = requests_util.url_util.redact_uri(uri)
+def test_redact_uri(uri, remove_username, exp_redact_uri):
+    redact_table_uri = requests_util.url_util.redact_uri(uri, remove_username=remove_username)
     assert redact_table_uri == exp_redact_uri
