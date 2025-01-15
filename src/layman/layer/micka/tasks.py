03e90e8670d31b03017e37059f5f634f135064f1
index-git <sileny.index@gmail.com>
2025-01-15 16:04:23 +0100

    Remove unused refresh_csw

--- src/layman/layer/micka/tasks.py
+++ src/layman/layer/micka/tasks.py
@@ -9,27 +9,7 @@ from . import csw, soap
 
 logger = get_task_logger(__name__)
 
-refresh_csw_needed = empty_method_returns_true
 refresh_soap_needed = empty_method_returns_true
-
-
-@celery_app.task(
-    name='layman.layer.micka.csw.refresh',
-    bind=True,
-    base=celery_app.AbortableTask
-)
-def refresh_csw(self, username, layername, http_method=common.REQUEST_METHOD_POST, metadata_properties_to_refresh=None):
-    metadata_properties_to_refresh = metadata_properties_to_refresh or []
-    if self.is_aborted():
-        raise AbortedException
-    if http_method == common.REQUEST_METHOD_POST:
-        csw.csw_insert(username, layername)
-    else:
-        csw.patch_layer(username, layername, metadata_properties_to_refresh)
-
-    if self.is_aborted():
-        csw.delete_layer(username, layername)
-        raise AbortedException
 
 
 @celery_app.task(
