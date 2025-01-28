2cc6380672035c1c7290c0ebbec63167041b7b5b
Jiri Kozel <jirik@users.noreply.github.com>
2025-01-28 14:35:11 +0100

    Encapsulate switch-to-layman-v1-23.sh

--- tests/migration_to_v2_0/test.sh
+++ tests/migration_to_v2_0/test.sh
@@ -6,31 +6,9 @@ set -exu
 ./tests/migration_to_v2_0/prepare-venv.sh
 
 # remember current git branch and switch to Layman 1.23.x
-make stop-and-remove-all-docker-containers
-CURRENT_GIT_BRANCH="$(git rev-parse --abbrev-ref HEAD)"
-echo "CURRENT_GIT_BRANCH = $CURRENT_GIT_BRANCH"
-git stash clear
-git stash
-git fetch
-git checkout 1.23.x
-git pull
-cp .env.dev .env
-sed -i -e "s/OAUTH2_INTROSPECTION_URL=.*/OAUTH2_INTROSPECTION_URL=http:\\/\\/host.docker.internal:8123\\/rest\\/test-oauth2\\/introspection?is_active=true/" .env
-sed -i -e "s/OAUTH2_USER_PROFILE_URL=.*/OAUTH2_USER_PROFILE_URL=http:\\/\\/host.docker.internal:8123\\/rest\\/test-oauth2\\/user-profile/" .env
-sed -i -e "s/FLASK_SECRET_KEY=.*/FLASK_SECRET_KEY=fb8727f383cacdbdcbf74d2f878b4141b15109f02cbe6117bb7d95605aa1f46f/" .env
-sed -i -e '/   layman_dev/a\' -e '      extra_hosts:\n         - "host.docker.internal:host-gateway"' docker-compose.dev.yml
-sed -i -e '/   celery_worker_dev/a\' -e '      extra_hosts:\n         - "host.docker.internal:host-gateway"' docker-compose.dev.yml
-docker pull layermanager/layman:dev-1-23
-docker tag layermanager/layman:dev-1-23 layman_dev
-docker pull layermanager/layman:client-1-23
-docker tag layermanager/layman:client-1-23 layman_client
-docker tag layermanager/layman:client-1-23 layman_client_test
-docker pull layermanager/layman:timgen-1-23
-docker tag layermanager/layman:timgen-1-23 timgen
-make micka-build && make qgis-build
+source ./tmp/migration_to_v2_0/switch-to-layman-v1-23.sh
 
 # start empty Layman 1.23.x
-make reset-data-directories
 ./tmp/migration_to_v2_0/start-dev-without-wagtail.sh
 
 # activate python virtual environment, set environment variables
