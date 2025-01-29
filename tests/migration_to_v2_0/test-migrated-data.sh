78837b25706a6f6087fd40ee771c311188abc7e9
Jiri Kozel <jirik@users.noreply.github.com>
2025-01-29 19:34:27 +0100

    Rewrite description migration test to new framework

---
+++ tests/migration_to_v2_0/test-migrated-data.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+set -exu
+
+# activate python virtual environment, set environment variables
+source .venv/bin/activate
+set -o allexport && source .env && set +o allexport
+
+# run migrated data tests
+PYTHONPATH=".:tmp/migration_to_v2_0:src" python3 -m pytest -W ignore::DeprecationWarning -xvv tmp/migration_to_v2_0/tests/
+
+deactivate
