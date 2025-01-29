19d2aed74bab8424bad28ff4a973f6affc0b5a7b
Jiri Kozel <jirik@users.noreply.github.com>
2025-01-29 11:36:22 +0100

    Encapsulate switch-to-layman-v2-0.sh

--- tests/migration_to_v2_0/test.sh
+++ tests/migration_to_v2_0/test.sh
@@ -14,8 +14,8 @@ set -exu
 # publish test data on Layman 1.23.x
 ./tmp/migration_to_v2_0/publish-data-on-v1-23.sh
 
-# undo changes in files tracked by git
-git checkout -- docker-compose.dev.yml
+# switch to current branch of Layman 2.0
+./tmp/migration_to_v2_0/switch-to-layman-v2-0.sh
 
-# switch back to current v2 branch
-make stop-and-remove-all-docker-containers || true && git checkout -- docker-compose.dev.yml && git checkout "$(<tmp/migration_to_v2_0/current-v2-git-branch.txt)" && cp .env.dev .env && (git stash pop || true) && make pull-dev-images && make micka-build && make qgis-build && make reset-data-directories
+## switch back to current v2 branch
+#make stop-and-remove-all-docker-containers || true && git checkout -- docker-compose.dev.yml && git checkout "$(<tmp/migration_to_v2_0/current-v2-git-branch.txt)" && cp .env.dev .env && (git stash pop || true) && make pull-dev-images && make micka-build && make qgis-build && make reset-data-directories
