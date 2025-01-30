6f86963e1ca7a51ac84c76196dfe735246c14111
Jiri Kozel <jirik@users.noreply.github.com>
2025-01-30 10:59:30 +0100

    Rename publish-data-on-v1-23.sh to prepare-data-on-v1-23.sh

--- tests/migration_to_v2_0/prepare-data-on-v1-23.sh
+++ tests/migration_to_v2_0/prepare-data-on-v1-23.sh
@@ -6,7 +6,7 @@ set -exu
 source .venv/bin/activate
 set -o allexport && source .env && set +o allexport
 
-# publish some layers on Layman 1.23.x
+# prepare some data on Layman 1.23.x
 PYTHONPATH=".:tmp/migration_to_v2_0:src" python tmp/migration_to_v2_0/prepare_data_on_v1_23.py
 
 deactivate
