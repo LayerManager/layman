8bae2365d241fe6489595e8e3792c11da1d85374
Jiri Kozel <jirik@users.noreply.github.com>
2025-01-30 14:11:39 +0100

    Test complete status

--- migration_to_v2_0_tests/tests/migrated_data_test.py
+++ migration_to_v2_0_tests/tests/migrated_data_test.py
@@ -2,7 +2,7 @@ import pytest
 
 from db import util as db_util
 import layman_settings as settings
-from tools.client import RestClient
+from tools.client import RestClient, LAYER_TYPE, MAP_TYPE
 from tools.oauth2_provider_mock import OAuth2ProviderMock
 from tools.test_data import import_publication_uuids, PUBLICATIONS, Publication
 
@@ -44,3 +44,16 @@ def test_migrated_description(client, publication):
                              data=(publication.uuid,), uri_str=DB_URI)
     assert len(rows) == 1
     assert rows[0][0] == publication.rest_args['description']
+
+
+@pytest.mark.usefixtures("import_publication_uuids_fixture", "oauth2_provider_mock_fixture")
+@pytest.mark.parametrize("publication", [
+    pytest.param(publ, marks=pytest.mark.xfail(reason="Geoserver provider is not yet migrated"))
+    for publ in PUBLICATIONS if publ.type == LAYER_TYPE
+] + [
+    publ for publ in PUBLICATIONS if publ.type == MAP_TYPE
+], ids=ids_fn)
+def test_complete_status(client, publication):
+    publ_detail = client.get_workspace_publication(publication.type, publication.workspace, publication.name,
+                                                   actor_name=publication.owner)
+    assert publ_detail['layman_metadata']['publication_status'] == 'COMPLETE', f'rest_publication_detail={publ_detail}'
