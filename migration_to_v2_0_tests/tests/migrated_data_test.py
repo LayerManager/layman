2619973e645823e4b03996dc42a649d59ec98a1f
Jiri Kozel <jirik@users.noreply.github.com>
2025-01-30 13:59:41 +0100

    Check description in DB prime_schema

--- migration_to_v2_0_tests/tests/migrated_data_test.py
+++ migration_to_v2_0_tests/tests/migrated_data_test.py
@@ -1,8 +1,13 @@
 import pytest
 
+from db import util as db_util
+import layman_settings as settings
 from tools.client import RestClient
 from tools.oauth2_provider_mock import OAuth2ProviderMock
 from tools.test_data import import_publication_uuids, PUBLICATIONS, Publication
+
+
+DB_URI = f"postgresql://{settings.LAYMAN_PG_USER}:{settings.LAYMAN_PG_PASSWORD}@localhost:25433/{settings.LAYMAN_PG_DBNAME}"
 
 
 @pytest.fixture(scope="session")
@@ -34,3 +39,8 @@ def test_migrated_description(client, publication):
     publ_detail = client.get_workspace_publication(publication.type, publication.workspace, publication.name,
                                                    actor_name=publication.owner)
     assert publ_detail['description'] == publication.rest_args['description']
+
+    rows = db_util.run_query(f"select description from {settings.LAYMAN_PRIME_SCHEMA}.publications where uuid = %s",
+                             data=(publication.uuid,), uri_str=DB_URI)
+    assert len(rows) == 1
+    assert rows[0][0] == publication.rest_args['description']
