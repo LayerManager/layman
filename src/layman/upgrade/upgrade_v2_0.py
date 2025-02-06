1c6281785e22953b9edcd6939fa078014a189206
Jiri Kozel <jirik@users.noreply.github.com>
2025-02-06 18:11:39 +0100

    Delete layers without WFS/WMS available during migration

--- src/layman/upgrade/upgrade_v2_0.py
+++ src/layman/upgrade/upgrade_v2_0.py
@@ -1,13 +1,17 @@
 import logging
+import shutil
 from urllib.parse import urljoin
 import requests
+from psycopg2 import sql
 
 from db import util as db_util
 from geoserver import util as gs_util
 from layman import settings, names
+from layman.common.micka import util as micka_util, requests as micka_requests
 from layman.layer import LAYER_TYPE
 from layman.map import MAP_TYPE
 from layman.map.filesystem import input_file
+from layman.util import get_publication_info
 
 logger = logging.getLogger(__name__)
 DB_SCHEMA = settings.LAYMAN_PRIME_SCHEMA
@@ -69,3 +73,100 @@ def ensure_gs_workspaces():
     assert names.GEOSERVER_WMS_WORKSPACE not in all_gs_workspaces
     gs_util.ensure_workspace(names.GEOSERVER_WFS_WORKSPACE, auth=settings.LAYMAN_GS_AUTH)
     gs_util.ensure_workspace(names.GEOSERVER_WMS_WORKSPACE, auth=settings.LAYMAN_GS_AUTH)
+
+
+def delete_layers_without_wfs_wms_available():
+    logger.info(f'    Delete layers that do not have WFS/WMS available')
+
+    query = f'''select w.name, p.name, p.uuid::varchar as uuid, p.id
+    from {DB_SCHEMA}.publications p inner join
+         {DB_SCHEMA}.workspaces w on w.id = p.id_workspace
+    where p.type = %s and p.wfs_wms_status != %s
+    ;'''
+    layers = db_util.run_query(query, (LAYER_TYPE, settings.EnumWfsWmsStatus.AVAILABLE.value, ))
+
+    for workspace, layername, layer_uuid, layer_id in layers:
+        logger.info(f'    Delete layer {workspace}.{layername}')
+
+        # micka
+        muuid = micka_util.get_metadata_uuid(layer_uuid)
+        micka_requests.csw_delete(muuid)
+
+        # geoserver sld
+        gs_wms_workspace = f"{workspace}_wms"
+        gs_util.delete_workspace_style(gs_wms_workspace, layername, auth=settings.LAYMAN_GS_AUTH)
+
+        # geoserver wms
+        gs_util.delete_feature_type(gs_wms_workspace, layername, settings.LAYMAN_GS_AUTH, store='postgresql')
+        gs_util.delete_feature_type(gs_wms_workspace, layername, settings.LAYMAN_GS_AUTH, store=f'external_db_{layername}')
+        gs_util.delete_wms_layer(gs_wms_workspace, layername, settings.LAYMAN_GS_AUTH)
+        gs_util.delete_wms_store(gs_wms_workspace, settings.LAYMAN_GS_AUTH, f"qgis_{layername}")
+        gs_util.delete_coverage_store(gs_wms_workspace, settings.LAYMAN_GS_AUTH, f"geotiff_{layername}")
+        gs_util.delete_coverage_store(gs_wms_workspace, settings.LAYMAN_GS_AUTH, f"image_mosaic_{layername}")
+        gs_util.delete_db_store(gs_wms_workspace, settings.LAYMAN_GS_AUTH, store_name=f"external_db_{layername}")
+        gs_util.delete_security_roles(f"{gs_wms_workspace}.{layername}.r", settings.LAYMAN_GS_AUTH)
+        gs_util.delete_security_roles(f"{gs_wms_workspace}.{layername}.w", settings.LAYMAN_GS_AUTH)
+
+        # geoserver wfs
+        gs_util.delete_feature_type(workspace, layername, settings.LAYMAN_GS_AUTH, store='postgresql')
+        gs_util.delete_feature_type(workspace, layername, settings.LAYMAN_GS_AUTH, store=f'external_db_{layername}')
+        gs_util.delete_db_store(workspace, settings.LAYMAN_GS_AUTH, store_name=f'external_db_{layername}')
+        gs_util.delete_security_roles(f"{workspace}.{layername}.r", settings.LAYMAN_GS_AUTH)
+        gs_util.delete_security_roles(f"{workspace}.{layername}.w", settings.LAYMAN_GS_AUTH)
+
+        # qgis
+        try:
+            shutil.rmtree(f"{settings.LAYMAN_QGIS_DATA_DIR}/workspaces/{workspace}/layers/{layername}")
+        except FileNotFoundError:
+            pass
+
+        # db
+        table_name = f"layer_{layer_uuid.replace('-', '_')}"
+        query = sql.SQL("""
+        DROP TABLE IF EXISTS {table} CASCADE
+        """).format(
+            table=sql.Identifier(workspace, table_name),
+        )
+        db_util.run_statement(query)
+
+        # primedbschema
+        query = f"""with const as (select %s as name)
+        select w.id,
+               w.name
+        from {DB_SCHEMA}.workspaces w inner join
+             const c on (   c.name = w.name
+                         or c.name is null)
+        order by w.name asc
+        ;"""
+        values = db_util.run_query(query, (workspace,))
+        if len(values) > 0:
+            assert len(values) == 1
+            workspace_id = values[0][0]
+
+            query = f'''delete from {DB_SCHEMA}.rights where id_publication = %s;'''
+            db_util.run_statement(query, (layer_id,))
+
+            query = f"""delete from {DB_SCHEMA}.publications p where p.id_workspace = %s and p.name = %s and p.type = %s;"""
+            db_util.run_statement(query, (workspace_id, layername, LAYER_TYPE,))
+
+        # filesystem.uuid (Redis)
+        # In upgrade process, UUIDs are not imported to Redis, so no need to delete it. Plus Redis is not persistent.
+
+        # filesystem
+        try:
+            shutil.rmtree(f"{settings.LAYMAN_DATA_DIR}/workspaces/{workspace}/layers/{layername}")
+        except FileNotFoundError:
+            pass
+        try:
+            shutil.rmtree(f"{settings.LAYMAN_NORMALIZED_RASTER_DATA_DIR}/workspaces/{workspace}/layers/{layername}")
+        except FileNotFoundError:
+            pass
+
+        # layman.celery.delete_publication (Redis)
+        # In upgrade process, there is probably no information about async tasks. Plus Redis is not persistent.
+
+        # far from perfect, but at least some assert that layer was deleted
+        publ_info = get_publication_info(workspace, LAYER_TYPE, layername)
+        assert not publ_info, f"{publ_info}"
+
+        logger.info(f'    Deleting layer {workspace}.{layername} DONE!')
