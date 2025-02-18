57578e429719d4b88dfc828936e07d1acc6c37af
Jiri Kozel <jirik@users.noreply.github.com>
2025-02-18 16:10:24 +0100

    Files are named by uuid in layer.qgis

--- src/layman/layer/qgis/__init__.py
+++ src/layman/layer/qgis/__init__.py
@@ -11,39 +11,10 @@ from layman.layer import LAYER_TYPE
 PUBL_TYPE_DEF_KEY = __name__
 
 check_workspace_name = empty_method
-
-
-def get_workspaces_dir():
-    workspace_sdir = os.path.join(settings.LAYMAN_QGIS_DATA_DIR, 'workspaces')
-    return workspace_sdir
-
-
-def get_workspace_dir(workspace):
-    workspace_dir = os.path.join(get_workspaces_dir(), workspace)
-    return workspace_dir
-
-
-def ensure_whole_user(username):
-    ensure_workspace(username)
-
-
-def delete_whole_user(username):
-    delete_workspace(username)
-
-
-def ensure_workspace(workspace):
-    workspace_dir = get_workspace_dir(workspace)
-    pathlib.Path(workspace_dir).mkdir(exist_ok=True, parents=True)
-    return workspace_dir
-
-
-def delete_workspace(workspace):
-    workspace_dir = get_workspace_dir(workspace)
-    try:
-        shutil.rmtree(workspace_dir)
-        os.rmdir(workspace_dir)
-    except FileNotFoundError:
-        pass
+ensure_whole_user = empty_method
+delete_whole_user = empty_method
+ensure_workspace = empty_method
+delete_workspace = empty_method
 
 
 def get_usernames():
@@ -51,39 +22,32 @@ def get_usernames():
 
 
 def get_workspaces():
-    workspaces_dir = get_workspaces_dir()
-    if not os.path.exists(workspaces_dir):
-        return []
-    workspace = [
-        subfile for subfile in os.listdir(workspaces_dir)
-        if os.path.isdir(os.path.join(workspaces_dir, subfile))
-    ]
-    return workspace
+    return []
 
 
-def get_publications_dir(publ_type, workspace):
+def get_publications_dir(publ_type):
     publ_types = get_publication_types()
     dirname = publ_types[publ_type][PUBL_TYPE_DEF_KEY]['publications_dir']
-    layersdir = os.path.join(get_workspace_dir(workspace), dirname)
+    layersdir = os.path.join(settings.LAYMAN_QGIS_DATA_DIR, dirname)
     return layersdir
 
 
-def get_publication_dir(publ_type, workspace, publ_name):
+def get_publication_dir(publ_type, publ_uuid):
     publ_dir = os.path.join(
-        get_publications_dir(publ_type, workspace),
-        publ_name
+        get_publications_dir(publ_type),
+        publ_uuid
     )
     return publ_dir
 
 
-def ensure_publication_dir(publ_type, workspace, publ_name):
-    publ_dir = get_publication_dir(publ_type, workspace, publ_name)
+def ensure_publication_dir(publ_type, publ_uuid):
+    publ_dir = get_publication_dir(publ_type, publ_uuid)
     pathlib.Path(publ_dir).mkdir(exist_ok=True, parents=True)
     return publ_dir
 
 
-def delete_publication_dir(publ_type, workspace, publ_name):
-    publ_dir = get_publication_dir(publ_type, workspace, publ_name)
+def delete_publication_dir(publ_type, publ_uuid):
+    publ_dir = get_publication_dir(publ_type, publ_uuid)
     try:
         shutil.rmtree(publ_dir)
     except FileNotFoundError:
@@ -92,8 +56,6 @@ def delete_publication_dir(publ_type, workspace, publ_name):
         os.rmdir(publ_dir)
     return {}
 
-
-get_layers_dir = partial(get_publications_dir, LAYER_TYPE)
 
 get_layer_dir = partial(get_publication_dir, LAYER_TYPE)
 
