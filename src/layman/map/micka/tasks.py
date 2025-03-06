2ec2d2a8e366839e70ceffbbea153e11988cacef
Jiri Kozel <jirik@users.noreply.github.com>
2025-03-06 10:51:44 +0100

    Remove unused task map.micka.csw

--- src/layman/map/micka/tasks.py
+++ src/layman/map/micka/tasks.py
@@ -6,26 +6,7 @@ from layman import celery_app, common
 from . import csw, soap
 
 logger = get_task_logger(__name__)
-refresh_csw_needed = empty_method_returns_true
 refresh_soap_needed = empty_method_returns_true
-
-
-@celery_app.task(
-    name='layman.map.micka.csw.refresh',
-    bind=True,
-    base=celery_app.AbortableTask
-)
-def refresh_csw(self, workspace, mapname, http_method=common.REQUEST_METHOD_POST, metadata_properties_to_refresh=None, actor_name=None):
-    if self.is_aborted():
-        raise AbortedException
-    if http_method == common.REQUEST_METHOD_POST:
-        csw.csw_insert(workspace, mapname, actor_name=actor_name)
-    else:
-        csw.patch_map(workspace, mapname, metadata_properties_to_refresh=metadata_properties_to_refresh, actor_name=actor_name)
-
-    if self.is_aborted():
-        csw.delete_map(workspace, mapname)
-        raise AbortedException
 
 
 @celery_app.task(
