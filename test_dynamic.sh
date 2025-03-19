16c35466293ca3457925ab51679b0d64d8d588e7
index-git <sileny.index@gmail.com>
2025-03-19 10:48:42 +0100

    Revert unintentional changes in test_*.sh files

--- test_dynamic.sh
+++ test_dynamic.sh
@@ -10,5 +10,5 @@ rm -rf tmp/artifacts/*
 max_fail="$1"
 if [ -z "$max_fail" ]; then max_fail=1; fi
 
-python3 src/wait_for_deps.py && python3 src/clear_layman_data.py && python3 -m pytest -W ignore::DeprecationWarning --maxfail="$max_fail" -vv --ignore=tests/static_data/ tests/dynamic_data/users_roles/delete_user_test.py
+python3 src/wait_for_deps.py && python3 src/clear_layman_data.py && python3 -m pytest -W ignore::DeprecationWarning --maxfail="$max_fail" -vv --ignore=tests/static_data/ tests
 #python3 src/wait_for_deps.py && python3 src/clear_layman_data.py && python3 -m TEST_TYPE=optional pytest -W ignore::DeprecationWarning -sxvv --capture=tee-sys --nocleanup --ignore=tests/static_data/ tests
