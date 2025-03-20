d66ca16f5ee96c1f5b8237e952f700bf7893028a
Jiri Kozel <jirik@users.noreply.github.com>
2025-03-20 14:51:15 +0100

    Remove check_schema_name from v1.8 migration

--- src/layman/common/prime_db_schema/schema_initialization.py
+++ src/layman/common/prime_db_schema/schema_initialization.py
@@ -2,8 +2,6 @@ import logging
 
 from db import util as db_util
 from layman import settings
-from layman.http import LaymanError
-from layman.util import get_workspaces as global_get_workspaces
 from . import model
 
 DB_SCHEMA = settings.LAYMAN_PRIME_SCHEMA
@@ -26,9 +24,3 @@ def ensure_schema(db_schema):
             raise exc
     else:
         logger.info(f"Layman DB schema already exists, schema_name={db_schema}")
-
-
-def check_schema_name(db_schema):
-    usernames = global_get_workspaces(use_cache=False, skip_modules=('layman.map.prime_db_schema', 'layman.layer.prime_db_schema',))
-    if db_schema in usernames:
-        raise LaymanError(42, {'workspace': db_schema})
